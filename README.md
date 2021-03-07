f05f0840062efc531f3eb5830bbefd06e1571f98
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-07 14:49:56 -0500

    better readme and publish.py

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 # isaac-typescript-definitions
 
-These are TypeScript definitions for [The Binding of Isaac: Afterbirth+](https://store.steampowered.com/app/570660/The_Binding_of_Isaac_Afterbirth/). They are meant to be used in mods utilizing the [TypeScriptToLua](https://typescripttolua.github.io/) tool.
+These are TypeScript definitions for [The Binding of Isaac: Afterbirth+](https://store.steampowered.com/app/570660/The_Binding_of_Isaac_Afterbirth/).
 
-<br />
-
-## TODO (SupremeElf has volunteered)
-
-* convert int --> EntityType, CollectibleType, etc.
-* add JSDoc comments for everything from the official docs
-* add unofficial BossID enum
+The [IsaacScript](https://isaacscript.github.io/) framework automatically uses these definitions.
--- publish.py
+++ publish.py
@@ -1,21 +1,27 @@
-#!/c/Python38/python.exe
+#!/usr/bin/env python
 
-# Standard imports
-import sys
-import subprocess
+import argparse
 import os
 import re
+import subprocess
+import sys
 
 if sys.version_info < (3, 0):
-    print("This script requires Python 3.")
+    print("Error: This script requires Python 3.")
     sys.exit(1)
 
 # Constants
 DIR = os.path.dirname(os.path.realpath(__file__))
 PROJECT_NAME = os.path.basename(DIR)
+PACKAGE_JSON_PATH = os.path.join(DIR, "package.json")
+VERSION_PREFIX = "1.0."
+
+# Variables
+version = ""
 
 # Subroutines
 def error(message, exception=None):
+    message = "Error: {}".format(message)
     if exception is None:
         print(message)
     else:
@@ -23,58 +29,67 @@ def error(message, exception=None):
     sys.exit(1)
 
 
+# Parse command-line arguments
+parser = argparse.ArgumentParser(
+    description="Publish a new version of this package to NPM."
+)
+parser.add_argument(
+    "-s",
+    "--skip-increment",
+    action="store_true",
+    help='do not increment the version number in the "package.json" file',
+)
+args = parser.parse_args()
+
 # Check to make sure that the project compiles
-'''
 os.chdir(DIR)
-RETURN_CODE = subprocess.call(["build.sh"])
-if RETURN_CODE != 0:
+completed_process = subprocess.run(["npx", "tsc"], shell=True)
+if completed_process.returncode != 0:
     error("Failed to build the project.")
-'''
 
-# Increment the version in the "package.json" file
-# http://stackoverflow.com/questions/17140886/how-to-search-and-replace-text-in-a-file-using-python
-PACKAGE_JSON_PATH = os.path.join(DIR, "package.json")
-with open(PACKAGE_JSON_PATH, "r") as file_handle:
-    FILE_DATA = file_handle.read()
-NEW_FILE = ""
-VERSION_PREFIX = "1.0."
-VERSION = ""
-for line in iter(FILE_DATA.splitlines()):
-    match = re.search(r'"version": "' + VERSION_PREFIX + r'(.+)",', line)
-    if match:
-        FINAL_DIGIT = str(int(match.group(1)) + 1)
-        VERSION = VERSION_PREFIX + FINAL_DIGIT
-        NEW_FILE += '  "version": "' + VERSION + '",\n'
-    else:
-        NEW_FILE += line + "\n"
-if VERSION == "":
-    print("Failed to parse the version.")
-    sys.exit(1)
-with open(PACKAGE_JSON_PATH, "w", newline="\n") as file:
-    file.write(NEW_FILE)
+if not args.skip_increment:
+    # Increment the version in the "package.json" file
+    # http://stackoverflow.com/questions/17140886/how-to-search-and-replace-text-in-a-file-using-python
+    with open(PACKAGE_JSON_PATH, "r") as file_handle:
+        file_data = file_handle.read()
+    new_file = ""
+    for line in iter(file_data.splitlines()):
+        match = re.search(r'"version": "' + VERSION_PREFIX + r'(.+)",', line)
+        if match:
+            final_digit = str(int(match.group(1)) + 1)
+            version = VERSION_PREFIX + final_digit
+            new_file += '  "version": "' + version + '",\n'
+        else:
+            new_file += line + "\n"
+    if version == "":
+        print("Failed to parse the version.")
+        sys.exit(1)
+    with open(PACKAGE_JSON_PATH, "w", newline="\n") as file:
+        file.write(new_file)
 
-# Commit to the repository
-RETURN_CODE = subprocess.call(["git", "add", "-A"])
-if RETURN_CODE != 0:
-    error("Failed to git add.")
-RETURN_CODE = subprocess.call(["git", "commit", "-m", VERSION])
-if RETURN_CODE != 0:
-    error("Failed to git commit.")
-RETURN_CODE = subprocess.call(["git", "push"])
-if RETURN_CODE != 0:
-    error("Failed to git push.")
+    # Commit to the repository
+    completed_process = subprocess.run(["git", "add", "-A"])
+    if completed_process.returncode != 0:
+        error("Failed to git add.")
+    completed_process = subprocess.run(["git", "commit", "-m", version])
+    if completed_process.returncode != 0:
+        error("Failed to git commit.")
+    completed_process = subprocess.run(["git", "push"])
+    if completed_process.returncode != 0:
+        error("Failed to git push.")
 
 # Publish
-RETURN_CODE = subprocess.call(
+completed_process = subprocess.run(
     [
-        "C:\\Program Files\\nodejs\\npm.cmd",
+        "npm",
         "publish",
         "--access",
         "public",
-    ]
+    ],
+    shell=True,
 )
-if RETURN_CODE != 0:
+if completed_process.returncode != 0:
     error("Failed to npm publish.")
 
 # Done
-print("Released", PROJECT_NAME, "version", VERSION, "successfully.")
+print("Published {} version {} successfully.".format(PROJECT_NAME, version))
