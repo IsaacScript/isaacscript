2b4089a2a9b153346554b4605253e827bc62990d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-07 17:24:36 -0500

    1.0.3

---
+++ index.js
@@ -0,0 +1,112 @@
+// This is a shared configuration file for ESLint
+// https://eslint.org/docs/user-guide/configuring
+module.exports = {
+  extends: [
+    // The linter base is the Airbnb style guide,
+    // which is the most popular JavaScript style guide in the world:
+    // https://github.com/airbnb/javascript
+    // The actual ESLint config is located here:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules
+    // The TypeScript config extends it:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+    "airbnb-typescript/base",
+
+    // We extend the Airbnb rules with the "recommended" and "recommended-requiring-type-checking"
+    // rules from the "typescript-eslint" plugin, which is also recommended by Matt Turnbull,
+    // the author of "airbnb-typescript/base"
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/README.md#recommended
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended.ts
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+
+    // We use Prettier to automatically format TypeScript files
+    // Disable any ESLint rules that conflict with Prettier
+    // https://github.com/prettier/eslint-config-prettier
+    "prettier",
+  ],
+
+  // We modify the linting rules from the base for some specific things
+  // (listed in alphabetical order)
+  rules: {
+    // Documentation:
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/array-type.md
+    // Not defined in parent configs
+    // Prefer the "[]string" syntax over "Array<string>"
+    "@typescript-eslint/array-type": ["error", { default: "array-simple" }],
+
+    // Documentation:
+    // https://eslint.org/docs/rules/lines-between-class-members
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/lines-between-class-members.md
+    // Defined at:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+    // Airbnb has "exceptAfterSingleLine" turned off by default
+    // A list of single-line variable declarations at the top of a class is common in TypeScript
+    "@typescript-eslint/lines-between-class-members": [
+      "error",
+      "always",
+      { exceptAfterSingleLine: true },
+    ],
+
+    // Documentation:
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-unused-vars.md
+    // https://eslint.org/docs/rules/no-unused-vars
+    // Defined at:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/variables.js
+    // TypeScript itself does not allow unused variables when used with the "noUnusedLocals"
+    // compiler flag, which is what this project uses, so this is a duplicate warning
+    "@typescript-eslint/no-unused-vars": "off",
+
+    // Documentation:
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
+    // https://eslint.org/docs/rules/no-use-before-define
+    // Defined at:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/variables.js
+    // This allows code to be structured in a more logical order
+    "@typescript-eslint/no-use-before-define": "off",
+
+    // Documentation:
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/quotes.md
+    // Defined at:
+    // https://github.com/prettier/eslint-config-prettier/blob/master/%40typescript-eslint.js
+    // In order to forbid unnecessary backticks, we must re-enable the "@typescript-eslint/quotes"
+    // rule as specified in the eslint-config-prettier documentation:
+    // https://github.com/prettier/eslint-config-prettier#enforce-backticks
+    "@typescript-eslint/quotes": [
+      "error",
+      "double",
+      { avoidEscape: true, allowTemplateLiterals: false },
+    ],
+
+    // Documentation:
+    // https://eslint.org/docs/rules/no-console
+    // Defined at:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js
+    // As a command-line program, it is normal to write to standard out and standard error
+    "no-console": "off",
+
+    // Documentation:
+    // https://eslint.org/docs/rules/no-plusplus
+    // Defined at:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+    // Airbnb disallows these because it can lead to errors with minified code;
+    // we don't have to worry about this in for loops though
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+
+    // Documentation:
+    // https://eslint.org/docs/rules/no-restricted-syntax
+    // Defined at:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+    // "for..of" loops are necessary to write efficient code in some situations
+    "no-restricted-syntax": "off",
+
+    // Documentation:
+    // https://eslint.org/docs/rules/prefer-destructuring
+    // Defined at:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/es6.js
+    // Array destructuring can result in non-intuitive code
+    // Object destructuring is disgustingly verbose in TypeScript
+    // e.g. "const foo: string = bar.foo;" vs "const { foo }: { foo: string } = bar;"
+    "prefer-destructuring": "off",
+  },
+};
