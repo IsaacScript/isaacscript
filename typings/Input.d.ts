410aab3e3665d2f68bbf5658fa7b4deddc3816d6
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-03-11 15:57:00 -0500

    bugfix for input not being global

--- typings/Input.d.ts
+++ typings/Input.d.ts
@@ -1,10 +1,18 @@
-declare namespace Input {
-  function IsButtonTriggered(button: Keyboard, controllerID: int): boolean;
-  function IsButtonPressed(button: Keyboard, controllerID: int): boolean;
-  function GetButtonValue(button: Keyboard, controllerID: int): float;
-  function IsActionTriggered(action: ButtonAction, controllerID: int): boolean;
-  function IsActionPressed(action: ButtonAction, controllerID: int): boolean;
-  function GetActionValue(action: ButtonAction, controllerID: int): float;
-  function IsMouseBtnPressed(button: Mouse): boolean;
-  function GetMousePosition(gameCoords: boolean): Vector;
+export {};
+
+declare global {
+  /** @noSelf */
+  namespace Input {
+    function IsButtonTriggered(button: Keyboard, controllerID: int): boolean;
+    function IsButtonPressed(button: Keyboard, controllerID: int): boolean;
+    function GetButtonValue(button: Keyboard, controllerID: int): float;
+    function IsActionTriggered(
+      action: ButtonAction,
+      controllerID: int,
+    ): boolean;
+    function IsActionPressed(action: ButtonAction, controllerID: int): boolean;
+    function GetActionValue(action: ButtonAction, controllerID: int): float;
+    function IsMouseBtnPressed(button: Mouse): boolean;
+    function GetMousePosition(gameCoords: boolean): Vector;
+  }
 }
