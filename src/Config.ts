7cdeda0c0edf2a4cb3354ddc84f4d0251f9ef5e8
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-05-17 05:22:42 -0400

    1.0.61

--- src/Config.ts
+++ src/Config.ts
@@ -1,5 +1,5 @@
 export interface Config {
-  projectName: string;
-  modsDirectory: string; // The "mods" directory that lives next to the "isaac-ng.exe" programs
-  saveSlot: number;
+  projectName: string; // The name of your project directory
+  modsDirectory: string; // The "mods" directory that lives next to the "isaac-ng.exe" program
+  saveSlot: number; // The save slot that you test your mod on
 }
