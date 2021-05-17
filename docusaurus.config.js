a18ae1f9bb14c4c440e72741c893f667e265825f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-05-16 21:49:56 -0400

    changing discord link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ module.exports = {
             logo: { src: "images/isaactearsiconfull.gif" },
             items: [
                 { to: "docs/features", label: "Docs", position: "left" },
-                { href: "https://discord.gg/isaac", label: "Discord", position: "right" },
+                { href: "https://discord.gg/vu3pFATz", label: "Discord", position: "right" },
                 { href: "https://github.com/IsaacScript/isaacscript", label: "GitHub", position: "right" },
             ],
         },
