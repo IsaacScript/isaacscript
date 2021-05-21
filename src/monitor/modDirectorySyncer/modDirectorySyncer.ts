5f22c302331d2d8c88a09d42124e9a682a671094
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-05-21 18:52:11 -0400

    1.0.84

--- src/monitor/modDirectorySyncer/modDirectorySyncer.ts
+++ src/monitor/modDirectorySyncer/modDirectorySyncer.ts
@@ -1,8 +1,4 @@
-import * as chokidar from "chokidar";
-import fs from "fs";
-import path from "path";
-import { MAIN_LUA_SOURCE_PATH } from "../../constants";
-import * as file from "../../file";
+import syncDirectory from "sync-directory";
 
 let modSourcePath: string;
 let modTargetPath: string;
@@ -20,89 +16,20 @@ function init() {
   modSourcePath = process.argv[2];
   modTargetPath = process.argv[3];
 
-  // First, copy the existing directory fresh
-  if (!file.exists(modSourcePath)) {
-    file.makeDir(modSourcePath);
-  }
-  if (file.exists(modTargetPath)) {
-    file.deleteDir(modTargetPath);
-  }
-  file.copy(modSourcePath, modTargetPath);
-
-  // Second, watch for changes
-  const watcher = chokidar.watch(modSourcePath, {
-    persistent: true,
-    ignoreInitial: true,
+  syncDirectory(modSourcePath, modTargetPath, {
+    watch: true,
+    type: "copy",
+    afterSync,
+    onError,
   });
-
-  watcher
-    .on("add", (filePath: string, stats: fs.Stats) => {
-      onAddOrChange(filePath, stats, "Added new");
-    })
-    .on("addDir", addDir)
-    .on("change", (filePath: string, stats: fs.Stats) => {
-      onAddOrChange(filePath, stats, "Changed");
-    })
-    .on("unlink", unlink)
-    .on("unlinkDir", unlinkDir)
-    .on("error", error);
 }
 
-function onAddOrChange(
-  filePath: string,
-  stats: fs.Stats,
-  verb: string,
-  checkNewStats = false,
-) {
-  if (checkNewStats) {
-    stats = fs.statSync(filePath);
-  }
-
-  if (stats.size === 0 && !checkNewStats) {
-    // Sometimes chokidar receives an event when the file is in the process of being written to
-    // If this is the case, the size of the file will be 0
-    // Try to copy the file again after a short delay
-    setTimeout(() => {
-      onAddOrChange(filePath, stats, verb, true);
-    }, 100);
-  } else {
-    addOrChange(filePath, verb);
-  }
+function afterSync(filePath: string) {
+  send(`File synced: ${filePath}`);
 }
 
-function addOrChange(filePath: string, verb: string) {
-  file.copy(filePath, getTargetPath(filePath));
-
-  if (filePath !== MAIN_LUA_SOURCE_PATH) {
-    // We don't need to report if the "main.lua" file changes,
-    // since we have a dedicated message for that
-    send(`${verb} file: ${filePath}`);
-  }
-}
-
-function addDir(dirPath: string) {
-  file.copy(dirPath, getTargetPath(dirPath));
-  send(`Copied new directory: ${dirPath}`);
-}
-
-function unlink(filePath: string) {
-  file.deleteFile(getTargetPath(filePath));
-  send(`Deleted file: ${filePath}`);
-}
-
-function unlinkDir(dirPath: string) {
-  file.deleteDir(getTargetPath(dirPath));
-  send(`Deleted directory: ${dirPath}`);
-}
-
-function error(err: Error) {
-  console.error("modDirectorySyncer - Error:", err);
-  send(`Error: ${err.message}`);
-}
-
-function getTargetPath(filePath: string): string {
-  const basePath = path.relative(modSourcePath, filePath);
-  return path.join(modTargetPath, basePath);
+function onError(err: Error) {
+  console.error("The directory syncer encountered an error:", err);
 }
 
 function send(msg: string) {
