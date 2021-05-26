937e1aab442276e041b462a9979010d5812c7287
Somdudewillson <somdudewilson@gmail.com>
2021-05-26 10:32:07 -0700

    Updated GridEntityPit.MakeBridge() (#11)
    * Updated variant enums
    
    * Updated subtype enums
    
    * Tweaked Prettier config to accept crlf.
    
    * Attempted to fill out BitSet128
    
    * Updated flag enums
    
    * Updated general enums
    
    * The latest enums.lua from Repentance
    
    * Workspace spelling dictionary additions
    
    * Incremented package-lock
    
    * Update .prettierrc.js
    
    * Removed unused/unimplemented constants
    
    * Update Level.d.ts
    
    * Update Level.d.ts
    
    * Updated ItemConfig.Item typing
    
    * Added enum for fireplace variants
    
    * Updated GridEntityPit.d.ts for new method signature
    
    * MakeBridge can take a null argument.
    
    * Fixed incorrect character
    
    * Fixed another incorrect character
    
    Co-authored-by: James <5511220+Zamiell@users.noreply.github.com>

--- typings/GridEntityPit.d.ts
+++ typings/GridEntityPit.d.ts
@@ -1,5 +1,5 @@
 declare class GridEntityPit extends GridEntity {
-  MakeBridge(): void;
+  MakeBridge(bridgeSource: GridEntity | null): void;
   SetLadder(value: boolean): void;
   UpdateCollision(): void;
 
