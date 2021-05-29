7c510af7427b6fe66167c4c63fe957f047289c6e
James <5511220+Zamiell@users.noreply.github.com>
2021-05-28 23:40:36 -0400

    fix for CheckLine

--- typings/Room.d.ts
+++ typings/Room.d.ts
@@ -17,7 +17,7 @@ declare class Room {
     gridPathThreshold?: int,
     ignoreWalls?: boolean,
     ignoreCrushable?: boolean,
-  ): [boolean, Vector];
+  ): [boolean, Vector | null];
   DamageGrid(index: int, damage: int): boolean;
   DestroyGrid(index: int, immediate: boolean): boolean;
   EmitBloodFromWalls(duration: int, count: int): void;
