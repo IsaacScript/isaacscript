aeeff705d1e878ba7a5890f6da5cc20260152f0e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-06-17 10:34:50 -0400

    bugfix for json resolution in the newest version of tstl

--- typings/json.d.ts
+++ typings/json.d.ts
@@ -1,3 +1,11 @@
+// The "json.lua" module exists at:
+// C:\Program Files (x86)\Steam\steamapps\common\The Binding of Isaac Rebirth\resources\scripts\json.lua
+// It is intended to be consumed by mods via:
+// local json = require("json")
+// We need to specify the "@noResolution" tstl compiler annotation,
+// since the "json.lua" file is not supposed to exist inside of end-user mods
+
+/** @noResolution */
 declare module "json" {
   function encode(this: void, data: unknown): string;
   function decode(this: void, data: string): unknown;
