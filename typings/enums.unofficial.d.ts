951b0c6bd420abe0fcfef35407944600f899a9a8
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-06-26 09:08:44 -0400

    adding the KnifeVariant enum

--- typings/enums.unofficial.d.ts
+++ typings/enums.unofficial.d.ts
@@ -50,6 +50,19 @@ declare const enum LaserVariant {
   BRIMTECH = 9,
 }
 
+/** For EntityType.ENTITY_KNIFE (8) */
+declare const enum KnifeVariant {
+  MOMS_KNIFE = 8,
+  BONE_CLUB = 1,
+  BONE_SCYTHE = 2,
+  DONKEY_JAWBONE = 3,
+  BAG_OF_CRAFTING = 4,
+  SUMPTORIUM = 5,
+  NOTCHED_AXE = 9,
+  SPIRIT_SWORD = 10,
+  TECH_SWORD = 11,
+}
+
 /** For EntityType.ENTITY_FIREPLACE (33) */
 declare const enum FireplaceVariant {
   NORMAL = 0,
