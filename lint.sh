2249cf2da75ebc9ec285edeb055cf4847ca4a9a6
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-20 17:46:41 -0400

    adding linter

---
+++ lint.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e # Exit on any errors
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# The latest version of some ESLint plugins require Node.js v16
+NODE_VERSION=$(node --version | cut -c 2-3)
+if (($NODE_VERSION < 16)); then
+  echo "error: requires Node.js version 16"
+  exit 1
+fi
+
+cd "$DIR"
+npx eslint --max-warnings 0 base.js
+npx eslint --max-warnings 0 mod.js
+echo "Success!"
