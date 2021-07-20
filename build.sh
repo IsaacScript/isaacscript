9ab4cc99ea5135c3a85710f91e89134d9232b33e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-20 18:55:05 -0400

    adding CI

--- build.sh
+++ build.sh
@@ -6,16 +6,19 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Get the name of the repository
+# https://stackoverflow.com/questions/23162299/how-to-get-the-last-part-of-dirname-in-bash/23162553
+REPO="$(basename "$DIR")"
+
 cd "$DIR"
 
 # Cleanup
-PROJECT_NAME=isaacscript
 rm -rf "$DIR/mod" # There is not supposed to be a mod directory here
 rm -rf "$DIR/dist" # Remove any previous build artifacts
-rm -rf "$HOME/AppData/Roaming/npm/$PROJECT_NAME" # Previous file from "npm i . -g"
-rm -rf "$HOME/AppData/Roaming/npm/$PROJECT_NAME.cmd" # Previous file from "npm i . -g"
-rm -rf "$HOME/AppData/Roaming/npm/$PROJECT_NAME.ps1" # Previous file from "npm i . -g"
-rm -rf "$HOME/AppData/Roaming/npm/node_modules/$PROJECT_NAME" # Previous link from "npm i . -g"
+rm -rf "$HOME/AppData/Roaming/npm/$REPO" # Previous file from "npm i . -g"
+rm -rf "$HOME/AppData/Roaming/npm/$REPO.cmd" # Previous file from "npm i . -g"
+rm -rf "$HOME/AppData/Roaming/npm/$REPO.ps1" # Previous file from "npm i . -g"
+rm -rf "$HOME/AppData/Roaming/npm/node_modules/$REPO" # Previous link from "npm i . -g"
 
 # Compile
 npx tsc
