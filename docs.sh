8e647e0ec34c9554c1d63118d1ffe98ec63d0ca3
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-29 16:05:23 -0400

    test

---
+++ docs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e # Exit on any errors
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+npx typedoc --out "$DIR/docs" "$DIR/src/index.ts"
+
+# TEST
+ls -l "$DIR/docs"
