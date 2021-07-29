8050202287afd27681310805d9908b70750de72d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-29 17:37:33 -0400

    adding getHUDOffsetVector()

--- src/util.ts
+++ src/util.ts
@@ -53,6 +53,50 @@ export function arrayEquals<T>(array1: T[], array2: T[]): boolean {
 export const ensureAllCases = (obj: never): never => obj;
 
 /**
+ * In the options menu, players have the ability to set a HUD offset. However, mods do not have
+ * access to this value. To get around this, Mod Config Menu provides a separate HUD offset setting
+ * on the first page of the menu. This is intended to be set by end-users to match their vanilla HUD
+ * offset setting so that mods can render UI elements to the screen in the correct position.
+ *
+ * @returns If the user does not have Mod Config Menu enabled, or does not have this option set,
+ * then this function will return `Vector.Zero.` Otherwise, it will return a Vector that represents
+ * a HUD offset that should be added to the position of a UI element.
+ */
+export function getHUDOffsetVector(): Vector {
+  const defaultVector = Vector.Zero;
+
+  // In Mod Config Menu, players can set a Hud Offset
+  if (
+    ModConfigMenu === undefined ||
+    ModConfigMenu.Config === undefined ||
+    ModConfigMenu.Config.General === undefined
+  ) {
+    return defaultVector;
+  }
+
+  const hudOffset = ModConfigMenu.Config.General.HudOffset;
+  if (hudOffset === undefined) {
+    return defaultVector;
+  }
+
+  // Expected values are integers between 1 and 10
+  if (type(hudOffset) !== "number" || hudOffset < 1 || hudOffset > 10) {
+    return defaultVector;
+  }
+
+  const x = hudOffset * 2;
+  let y = hudOffset;
+  if (y >= 4) {
+    y += 1;
+  }
+  if (y >= 9) {
+    y += 1;
+  }
+
+  return Vector(x, y);
+}
+
+/**
  * Helper function to initialize an RNG object.
  *
  * Example:
