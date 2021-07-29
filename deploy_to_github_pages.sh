d558b6fe882dcd43421392359940c26d320c4a1d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-29 11:48:30 -0400

    deploy script

--- deploy_to_github_pages.sh
+++ deploy_to_github_pages.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+set -e # Exit on any errors
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Get the name of the repository
+# https://stackoverflow.com/questions/23162299/how-to-get-the-last-part-of-dirname-in-bash/23162553
+REPO="$(basename "$DIR")"
+
+cd "$DIR"
+
+# Generate the docs
+git checkout main
+npx typedoc "$DIR/src/index.ts"
+TMP_DIR="/tmp/$REPO"
+rm -rf "$TMP_DIR"
+cp -R "$DIR/docs" "$TMP_DIR"
+git checkout gh-pages
+rm -rf *
+cp -R "$TMP_DIR/*" .
+git add -A
+git commit -m "updating docs"
+git push
+git checkout main
