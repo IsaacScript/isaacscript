fe1629a5719c11994747c82bae755195cdd26a34
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-29 11:09:30 -0400

    removing extra flag functions

--- src/flags.ts
+++ src/flags.ts
@@ -1,22 +1,5 @@
 export function addFlag(flags: int, flag: int): int {
-  validateFlags(flags, true);
   return flags | flag; // eslint-disable-line no-bitwise
-}
-
-export function addTearFlag(flags: int, tearFlag: TearFlags): int {
-  // TearFlags are really represented as BitSet128 objects in Lua,
-  // but we describe them as integers in the TypeScript definitions for simplicity
-  // Thus, we first have to cast the flags to a BitSet128 before we can do transformations on them
-  const oldBitSet128 = flags as unknown as BitSet128;
-
-  // We can't use operator overloading in TypeScript,
-  // so we use the convenience functions of the BitSet128 object instead
-  const newBitSet128 = oldBitSet128.bor(tearFlag);
-
-  // Cast it back to the original type
-  const newFlags = newBitSet128 as unknown as int;
-
-  return newFlags;
 }
 
 export function hasFlag(flags: int, flag: int): boolean {
@@ -24,55 +7,5 @@ export function hasFlag(flags: int, flag: int): boolean {
 }
 
 export function removeFlag(flags: int, flag: int): int {
-  validateFlags(flags, true);
   return flags & ~flag; // eslint-disable-line no-bitwise
-}
-
-export function removeTearFlag(flags: int, tearFlag: TearFlags): int {
-  // TearFlags are really represented as BitSet128 objects in Lua,
-  // but we describe them as integers in the TypeScript definitions for simplicity
-  // Thus, we first have to cast the flags to a BitSet128 before we can do transformations on them
-  const oldBitSet128 = flags as unknown as BitSet128;
-
-  // We can't use operator overloading in TypeScript,
-  // so we use the convenience functions of the BitSet128 object instead
-  const newBitSet128 = oldBitSet128.band(~tearFlag); // eslint-disable-line no-bitwise
-
-  // Cast it back to the original type
-  const newFlags = newBitSet128 as unknown as int;
-
-  return newFlags;
-}
-
-function validateFlags(flags: int, adding: boolean) {
-  const functionPrefix = adding ? "add" : "remove";
-
-  const flagsType = type(flags);
-  if (flagsType === "number") {
-    return;
-  }
-
-  const metaTable = getmetatable(flags) as Record<string, string>;
-  if (metaTable === undefined) {
-    error(
-      `The ${functionPrefix}Flag function does not support the ${flagsType} type.`,
-    );
-  }
-
-  const metaTableType = metaTable.__type; // eslint-disable-line no-underscore-dangle
-  if (metaTableType === undefined) {
-    error(
-      `The ${functionPrefix}Flag function does not support the ${flagsType} type.`,
-    );
-  }
-
-  if (metaTableType === "BitSet128") {
-    error(
-      `The ${functionPrefix}Flag function does not support TearFlags. Use the "${functionPrefix}TearFlags()" convenience function instead.`,
-    );
-  }
-
-  error(
-    `The ${functionPrefix}Flag function does not support the ${metaTableType} type.`,
-  );
 }
