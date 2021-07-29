f6951d671a79089ccf6878660d1674b26a8ef13f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-29 12:47:50 -0400

    changing CI

--- docs.sh
+++ docs.sh
@@ -12,4 +12,5 @@ cd "$DIR"
 npx typedoc "$DIR/src/index.ts"
 
 # Force push the "docs" directory to the "gh-pages" branch of the repository
-git push --set-upstream origin `git subtree split --prefix docs main`:gh-pages --force
+git fetch # Needed to prevent "error: unable to delete 'gh-pages': remote ref does not exist" in CI
+git push origin `git subtree split --prefix docs main`:gh-pages --force
