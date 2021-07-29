2926792bc76f0ffae9420d4625b7c3fa0e81ff54
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-29 17:06:06 -0400

    adding initRNG()

---
+++ src/constants.ts
@@ -0,0 +1,6 @@
+/**
+ * This is the ShiftIdx that Blade recommended after having reviewing the game's internal functions.
+ * Any value between 0 and 80 should work equally well.
+ * https://www.jstatsoft.org/article/view/v008i14/xorshift.pdf
+ */
+export const RECOMMENDED_SHIFT_IDX = 35;
---
+++ src/util.ts
@@ -0,0 +1,17 @@
+import { RECOMMENDED_SHIFT_IDX } from "./constants";
+
+/**
+ * Helper function to initialize an RNG object.
+ *
+ * @param seed The seed to initialize it with.
+ * (If you aren't initializing it with a seed, then don't use this function and instead simply call
+ * the `RNG()` constructor.)
+ */
+export function initRNG(seed: int): RNG {
+  const rng = RNG();
+
+  // The game expects seeds in the range of 0 to 4294967295
+  rng.SetSeed(seed, RECOMMENDED_SHIFT_IDX);
+
+  return rng;
+}
