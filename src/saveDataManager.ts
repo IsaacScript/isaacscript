968d725976f55ad5edb58177608f16f75fa594e9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-30 01:09:48 -0400

    better docs for save data manager

--- src/saveDataManager.ts
+++ src/saveDataManager.ts
@@ -13,6 +13,66 @@ export function init(mod: Mod): void {
   mod.AddCallback(ModCallbacks.MC_POST_NEW_ROOM, postNewRoom); // 19
 }
 
+/**
+ * This is the entry point to the save data manager, a system which provides two major features:
+ *
+ * 1. automatic resetting of data on a new run, on a new level, or on a new room (as desired)
+ * 2. automatic saving and loading of all data to the "save#.dat" file
+ *
+ * The save data manager never exposes any data, ensuring that all of the files in your mod have
+ * variables that are properly scoped.
+ *
+ * To use it, simply define an object that roughly matches the format of `SaveData`, and feed it to
+ * the `saveDataManager.init()` function.
+ *
+ * Example:
+ * ```
+ * // in file: feature1.ts
+ * import { saveDataManager } from "isaacscript-common";
+ *
+ * // Declare local variables to this file or feature
+ * const v = {
+ *   // These are never reset
+ *   persistent: {
+ *     foo1: 0,
+ *   },
+ *
+ *   // These are reset at the beginning of every run
+ *   run: {
+ *     foo2: 0,
+ *   },
+ *
+ *   // These are reset at the beginning of every level
+ *   level: {
+ *     foo3: 0,
+ *   },
+ *
+ *   // These are reset at the beginning of every room
+ *   room: {
+ *     foo2: 0,
+ *   },
+ * };
+ * // (every child object is optional; only create the ones that you need)
+ *
+ * // Register the variables with the save data manager
+ * // We need to provide a string key to the resetter so that it can save the variables properly in
+ * // the "save#.dat" file
+ * saveDataManager.register("feature1", v);
+ *
+ * // Elsewhere in the file...
+ * function feature1Function() {
+ *   if (v.run.foo1 > 0) {
+ *     // Do stuff
+ *   }
+ * }
+ * ```
+ *
+ * @param key The name of the file or feature that is submitting data to be managed by the save data
+ * manager. The save data manager will throw an error if it receives a second registration request
+ * using the same key as a previous subscriber.
+ * @param saveData An object with one or more child-objects of "persistent", "run", "level", or
+ * "room".
+ */
 export function register(key: string, saveData: SaveData): void {
   if (saveDataMap.has(key)) {
     error(
