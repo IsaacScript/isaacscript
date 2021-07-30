8c69633dd963f82b73c1b8040d36e7b34815c38e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-30 01:52:52 -0400

    debug

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,11 @@ export * from "./functions/player";
 export * from "./functions/tears";
 export * from "./functions/ui";
 export * from "./functions/util";
+
+/*
 export { saveDataManager } from "./saveDataManager";
 export { default as CallbackParametersCustom } from "./types/CallbackParametersCustom";
 export { default as ModCallbacksCustom } from "./types/ModCallbacksCustom";
 export { default as PickingUpItem } from "./types/PickingUpItem";
 export * from "./upgradeMod";
+*/
