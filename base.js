84ee37da5b2746b88fa4a7931c9f79eff9777335
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-31 20:16:49 -0400

    adding new rule

--- base.js
+++ base.js
@@ -33,6 +33,10 @@ module.exports = {
     // https://github.com/Zamiell/eslint-plugin-eqeqeq-fix
     "plugin:eqeqeq-fix/recommended",
 
+    // This provides a version of the "no-template-curly-in-string" that the "--fix" flag can fix
+    // https://github.com/Zamiell/eslint-plugin-no-template-curly-in-string-fix
+    "plugin:no-template-curly-in-string-fix/recommended",
+
     // We use Prettier to automatically format TypeScript files
     // We want to run Prettier as an ESLint rule so that we can detect non-formatted files in CI
     // https://github.com/prettier/eslint-plugin-prettier
