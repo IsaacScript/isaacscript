e70f5c1f0b3dc3417af4107f937dbf696ef17c5a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-07-31 22:57:14 -0400

    better rng comments

--- typings/RNG.d.ts
+++ typings/RNG.d.ts
@@ -3,7 +3,17 @@ declare function RNG(this: void): RNG;
 declare class RNG {
   GetSeed(): int;
   Next(): int;
+  /**
+   * Generates a random float between 0 and 1.
+   * It is inclusive on the bottom and exclusive on the top.
+   */
   RandomFloat(): float;
+  /**
+   * Generates a random integer between 0 and max - 1. For example, `rng.RandomInt(4)` will return
+   * either 0, 1, 2, or 3.
+   *
+   * @param max
+   */
   RandomInt(max: int): int;
   SetSeed(seed: int, shiftIdx: int): void;
 }
