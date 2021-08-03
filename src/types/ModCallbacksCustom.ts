3028be2c73f523060800b5bac4f78eae6623ad0c
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-03 19:22:20 -0400

    better comment

--- src/types/ModCallbacksCustom.ts
+++ src/types/ModCallbacksCustom.ts
@@ -2,6 +2,10 @@
  * These are the custom callbacks available for use once the mod object has been upgraded.
  * Also see the [[`upgradeMod`]] function and [[`CallbackParametersCustom`]].
  *
+ * For a better listing of all possible custom callbacks, check out the official
+ * [Function Signatures](https://isaacscript.github.io/docs/function-signatures#custom-callbacks)
+ * docs instead.
+ *
  * @category Custom Callbacks
  */
 enum ModCallbacksCustom {
