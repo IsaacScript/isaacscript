cbd03c971edcfe5b46c5552a923ff34a905e04e4
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-03 20:36:32 -0400

    better comments for Isaac class

--- typings/Isaac.d.ts
+++ typings/Isaac.d.ts
@@ -3,13 +3,34 @@ export {};
 declare global {
   /** @noSelf */
   namespace Isaac {
+    /**
+     * Your mod can't do much of anything unless you attach some callback functions that can run
+     * code when certain things happen. The different types of callbacks are represented in the
+     * ModCallbacks enum.
+     *
+     * Some callbacks take an optional third argument to specify that you only want it the function
+     * to fire on a specific thing. For example:
+     * ```
+     * mod.AddCallback(
+     *   ModCallbacks.MC_POST_EFFECT_UPDATE,
+     *   postEffectUpdatePoof01,
+     *   EffectVariant.POOF01,
+     * )
+     * ```
+     */
     function AddCallback(
       mod: Mod,
       callbackID: ModCallbacks,
       callbackFn: () => void,
       entityID?: int,
     ): void;
+    /**
+     * @returns The pill color.
+     */
     function AddPillEffectToPool(pillEffect: PillEffect | int): PillColor | int;
+    /**
+     * Puts a string into the debug console. (You can open the debug console with the tilde key.)
+     */
     function ConsoleOutput(text: string): void;
     function CountBosses(): int;
     function CountEnemies(): int;
@@ -23,12 +44,18 @@ declare global {
      * @param subType Specifying -1 will return all subtypes. Default is -1.
      */
     function CountEntities(
-      spawner: never, // Entity | null,
+      fakeArg: never,
+      spawner: Entity | null,
       entityType?: EntityType | int,
       variant?: int,
       subType?: int,
     ): int;
+    /**
+     * Prints a string to the "log.txt" file. By default, the file is located at:
+     * `C:\Users\[YourUsername]\Documents\My Games\Binding of Isaac Repentance\log.txt`
+     */
     function DebugString(msg: string): Mod;
+    /** Executes a command on the debug console. */
     function ExecuteCommand(command: string): string;
     function Explode(
       position: Vector,
@@ -65,19 +92,35 @@ declare global {
       radius: float,
       partitions?: int,
     ): Entity[];
+    /** Returns -1 if no card with the specified name was found. */
     function GetCardIdByName(cardName: string): Card | int;
+    /** Returns 0 if the current run is not a challenge. */
     function GetChallenge(): Challenge | int;
-    /** Returns -1 if the challenge does not exist. */
+    /** Returns -1 if the specified challenge name does not exist. */
     function GetChallengeIdByName(challengeName: string): Challenge | int;
+    /** Returns -1 if no costume with the specified name was found. */
     function GetCostumeIdByPath(path: string): int;
+    /** Returns -1 if no curse with the specified name was found. */
     function GetCurseIdByName(curseName: string): LevelCurse | int;
+    /** Returns 0 if no entity with the specified name was found. */
     function GetEntityTypeByName(entityName: string): EntityType | int;
+    /** Returns -1 if no entity with the specified name was found. */
     function GetEntityVariantByName(entityName: string): int;
+    /**
+     * Returns the amount of render frames that have passed since the game was open.
+     * The counter for this increases even when the game is paused or when you are in the main menu.
+     * Since Isaac frames are equal to render frames, 60 frames equals 1 second.
+     * Note that these frames are completely different from the frames returned from
+     * `Game().GetFrameCount()` and other functions.
+     */
     function GetFrameCount(): int;
     function GetFreeNearPosition(position: Vector, step: float): Vector;
     function GetItemConfig(): ItemConfig;
+    /** Returns -1 if no collectible with the specified name was found. */
     function GetItemIdByName(entityName: string): CollectibleType | int;
+    /** Returns -1 if no music with the specified name was found. */
     function GetMusicIdByName(musicName: string): Music | int;
+    /** Returns -1 if no pill with the specified name was found. */
     function GetPillEffectByName(pillName: string): PillEffect | int;
     /**
      * With no argument, it returns the 0th player.
@@ -96,7 +139,7 @@ declare global {
      */
     function GetPlayer(playerID: int): EntityPlayer | null;
     /**
-     * If the specified character does not exist, it returns -1.
+     * Returns -1 if the specified character does not exist.
      *
      * @param playerName
      * @param tainted Default is false.
@@ -105,23 +148,51 @@ declare global {
       playerName: string,
       tainted?: boolean,
     ): PlayerVariant | int;
+    /**
+     * Returns a random position in the current room in world coordinates (not render coordinates).
+     */
     function GetRandomPosition(): Vector;
-    function GetRoomEntities(): Entity[];
+    /** This should never be used over Isaac.GetRoomEntities() since it is known to cause bugs. */
+    function GetRoomEntities(fakeArg: never): Entity[];
+    /** Returns -1 if no sound with the specified name was found. */
     function GetSoundIdByName(soundName: string): SoundEffect | int;
+    /**
+     * Returns the width of the given string in pixels based on the "terminus8" font.
+     * (This is the same font used in the `Isaac.RenderText()` function.)
+     */
     function GetTextWidth(str: string): int;
     /**
      * Returns the current time in milliseconds since the program was launched.
      * (This is simply a mapping to "os.clock()".)
      */
     function GetTime(): int;
+    /** Returns -1 if the specified trinket was not found. */
     function GetTrinketIdByName(trinketName: string): TrinketType | int;
+    /**
+     * @param gridEntityType
+     * @param variant
+     * @param position
+     * @param forced Forced has no effect and will not override a grid entity on the given location.
+     * Remove any existing grid entities first.
+     */
     function GridSpawn(
       gridEntityType: GridEntityType | int,
       variant: int,
       position: Vector,
       forced: boolean,
     ): GridEntity;
+    /**
+     * Returns true if your mod has data stored from the `Isaac.SaveModData()` function.
+     * (This corresponds to "save#.dat" files existing in the mod's save data folder.)
+     */
     function HasModData(mod: Mod): boolean;
+    /**
+     * Returns a string that was stored in a "save#.dat" file from the `Isaac.SaveModData()`
+     * function. If there is no "save#.dat" file for your mod, this function will return an empty
+     * string. There are 3 "save#.dat" files, one per save slot. The number will be determined
+     * automatically by the game. In Repentance, these files are located in the "data" folder next
+     * to the "mods" folder.
+     */
     function LoadModData(mod: Mod): string;
     // function RegisterMod(mod: Mod, modName: string, APIVersion: int): void; // Should use the global RegisterMod() instead
     function RemoveCallback(
@@ -129,6 +200,10 @@ declare global {
       callbackID: ModCallbacks,
       callbackFn: () => void,
     ): void;
+    /**
+     * Will delete a "save#.dat" file, if it exists. For more information, see the
+     * `Isaac.SaveModData()` function.
+     */
     function RemoveModData(mod: Mod): void;
     function RenderScaledText(
       str: string,
@@ -150,6 +225,12 @@ declare global {
       b: float,
       a: float,
     ): void;
+    /**
+     * Stores a string in a "save#.dat" file for persistent storage across game invocations.
+     * There are 3 "save#.dat" files, one per save slot. The number will be determined automatically
+     * by the game. In Repentance, these files are located in the "data" folder next to the "mods"
+     * folder.
+     */
     function SaveModData(mod: Mod, data: string): void;
     function ScreenToWorld(position: Vector): Vector;
     function ScreenToWorldDistance(position: Vector): Vector;
@@ -161,7 +242,15 @@ declare global {
       velocity: Vector,
       spawner: Entity | null,
     ): Entity;
+    /**
+     * Converts a game Vector (i.e. `entity.Position`) to a render Vector used for drawing sprites
+     * and text to the screen at fixed positions.
+     */
     function WorldToRenderPosition(position: Vector): Vector;
+    /**
+     * Converts a game Vector (i.e. `entity.Position`) to a screen Vector used for drawing sprites
+     * and text next to an entity.
+     */
     function WorldToScreen(position: Vector): Vector;
     function WorldToScreenDistance(position: Vector): Vector;
   }
--- typings/ItemConfigList.d.ts
+++ typings/ItemConfigList.d.ts
@@ -3,7 +3,7 @@ declare class ItemConfigList {
    * ItemConfigList is bugged such that using the "Get()" method returns Lua userdata.
    * This userdata cannot be used for anything. Instead, use "ItemConfig.GetCollectible()".
    */
-  Get(idx: never): ItemConfigItem;
+  Get(fakeArg: never, idx: int): ItemConfigItem;
 
   Size: int;
 }
