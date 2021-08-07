d48267a196055e481bb32283cdcad4cdbd1af359
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-06 21:35:02 -0400

    bugfix for touching wrong file

--- src/commands/monitor/touchSaveDatFiles.ts
+++ src/commands/monitor/touchSaveDatFiles.ts
@@ -1,16 +1,16 @@
 import path from "path";
-import { CURRENT_DIRECTORY_NAME } from "../../constants";
+import { WATCHER_MOD_NAME } from "../../constants";
 import * as file from "../../file";
 import { Config } from "../../types/Config";
 
 export default function touchSaveDatFiles(config: Config): void {
   const modsDataPath = path.join(config.modsDirectory, "..", "data");
-  const modDataPath = path.join(modsDataPath, CURRENT_DIRECTORY_NAME);
-  if (!file.exists(modDataPath)) {
-    file.makeDir(modDataPath);
+  const watcherModDataPath = path.join(modsDataPath, WATCHER_MOD_NAME);
+  if (!file.exists(watcherModDataPath)) {
+    file.makeDir(watcherModDataPath);
   }
   const saveDatFileName = `save${config.saveSlot}.dat`;
-  const saveDatPath = path.join(modDataPath, saveDatFileName);
+  const saveDatPath = path.join(watcherModDataPath, saveDatFileName);
   if (!file.exists(saveDatPath)) {
     file.touch(saveDatPath);
   }
