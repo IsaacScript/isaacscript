c6607a2f80645e20c7badb1314c6799aa207e2fe
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-09 09:47:20 -0400

    more descriptive log functions

--- src/functions/log.ts
+++ src/functions/log.ts
@@ -17,21 +17,24 @@ export function log(msg: string): void {
  * Helper function for printing out every damage flag that is turned on. Helpful when debugging.
  */
 export function logAllDamageFlags(flags: int): void {
-  logAllFlags(flags, MAX_DAMAGE_FLAG_SHIFT);
+  logAllFlags(flags, MAX_DAMAGE_FLAG_SHIFT, "damage");
 }
 
 /**
  * Helper function for printing out every entity flag that is turned on. Helpful when debugging.
  */
 export function logAllEntityFlags(flags: int): void {
-  logAllFlags(flags, MAX_ENTITY_FLAG_SHIFT);
+  logAllFlags(flags, MAX_ENTITY_FLAG_SHIFT, "entity");
 }
 
 /**
  * Helper function for printing out every flag that is turned on. Helpful when debugging.
  */
-export function logAllFlags(flags: int, maxShift: int): void {
-  log("Logging all flags:");
+export function logAllFlags(flags: int, maxShift: int, description = ""): void {
+  if (description !== "") {
+    description += " ";
+  }
+  log(`Logging all ${description}flags:`);
   for (let i = 0; i <= maxShift; i++) {
     if (hasFlag(flags, 1 << i)) {
       log(`- Has flag: ${i}`);
@@ -43,12 +46,12 @@ export function logAllFlags(flags: int, maxShift: int): void {
  * Helper function for printing out every projectile flag that is turned on. Helpful when debugging.
  */
 export function logAllProjectileFlags(flags: int): void {
-  logAllFlags(flags, MAX_PROJECTILE_FLAG_SHIFT);
+  logAllFlags(flags, MAX_PROJECTILE_FLAG_SHIFT, "projectile");
 }
 
 /**
  * Helper function for printing out every use flag that is turned on. Helpful when debugging.
  */
 export function logAllUseFlags(flags: int): void {
-  logAllFlags(flags, MAX_USE_FLAG_SHIFT);
+  logAllFlags(flags, MAX_USE_FLAG_SHIFT, "use");
 }
