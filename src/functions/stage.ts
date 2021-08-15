cf9aaedac43ac38a6b5e16b7b8c2bd7c19412a4a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-15 09:48:19 -0400

    adding onFinalFloor helper function

--- src/functions/stage.ts
+++ src/functions/stage.ts
@@ -1,14 +1,3 @@
-export function onRepentanceStage(): boolean {
-  const game = Game();
-  const level = game.GetLevel();
-  const stageType = level.GetStageType();
-
-  return (
-    stageType === StageType.STAGETYPE_REPENTANCE ||
-    stageType === StageType.STAGETYPE_REPENTANCE_B
-  );
-}
-
 export function onCathedral(): boolean {
   const game = Game();
   const level = game.GetLevel();
@@ -34,6 +23,38 @@ export function onDarkRoom(): boolean {
   const stageType = level.GetStageType();
 
   return stage === 11 && stageType === 0;
+}
+
+/**
+ * Returns whether or not the player is on the "final floor" of the particular run. The final floor
+ * is defined as one that prevents the player from entering the I AM ERROR room on.
+ *
+ * For example, when using Undefined on The Chest, it has a 50% chance of teleporting the player to
+ * the Secret Room and a 50% chance of teleporting the player to the Super Secret Room, because the
+ * I AM ERROR room is never entered into the list of possibilities.
+ */
+export function onFinalFloor(): boolean {
+  const game = Game();
+  const level = game.GetLevel();
+  const stage = level.GetStage();
+
+  return (
+    stage === 11 || // The Chest / Dark Room
+    stage === 12 || // The Void
+    stage === 13 || // Home
+    (stage === 8 && onRepentanceStage()) // Corpse 2
+  );
+}
+
+export function onRepentanceStage(): boolean {
+  const game = Game();
+  const level = game.GetLevel();
+  const stageType = level.GetStageType();
+
+  return (
+    stageType === StageType.STAGETYPE_REPENTANCE ||
+    stageType === StageType.STAGETYPE_REPENTANCE_B
+  );
 }
 
 export function onSheol(): boolean {
