279bace925daed7c7a4a70a347c69407a0b9dbef
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-20 19:09:34 -0400

    GetPtrHash now accepts RoomDescriptor or RoomDescriptorReadOnly

--- typings/functions.d.ts
+++ typings/functions.d.ts
@@ -1,4 +1,7 @@
 declare function Game(this: void): Game;
-declare function GetPtrHash(this: void, entity: Entity): int;
+declare function GetPtrHash(
+  this: void,
+  pointer: Entity | RoomDescriptor | RoomDescriptorReadOnly,
+): int;
 declare function Random(this: void): int;
 declare function RandomVector(this: void): Vector;
