2547ceb23cfae995c8ce07b80b387a621d72541c
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-19 21:07:23 -0400

    Revert "trying out a brand"
    This reverts commit ae5abf83eca4a75b354640236093b2a9f7ad5de6.

--- typings/primitives.d.ts
+++ typings/primitives.d.ts
@@ -6,10 +6,10 @@
  * https://spin.atomicobject.com/2018/11/05/using-an-int-type-in-typescript/
  * To avoid verbose casting everywhere, we just make int an alias for number.
  */
-type int = number & { __intBrand: any }; // eslint-disable-line
+type int = number; // eslint-disable-line @typescript-eslint/naming-convention
 
 /**
  * In TypeScript, a "number" is the same thing as a float. We make an alias here so that the API
  * definitions better correspond to the official API documentation.
  */
-type float = number & { __floatBrand: any }; // eslint-disable-line
+type float = number; // eslint-disable-line @typescript-eslint/naming-convention
