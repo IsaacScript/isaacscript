64fc9e02faa7c6a5953ccd83938687cc8a0925f9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-22 13:54:08 -0400

    GetPtrHash now returns a PtrHash instead of an int

--- typings/functions.d.ts
+++ typings/functions.d.ts
@@ -1,7 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+declare type PtrHash = number & { __ptrHashBrand: any };
+
 declare function Game(this: void): Game;
+/**
+ * Comparing two API objects directly in mod code will not work, even if the Lua metadata points to
+ * the same pointer in memory. As a workaround, use this function to get a numerical hash of the
+ * object.
+ *
+ * A `PtrHash` object is simply a branded number for better type safety.
+ */
 declare function GetPtrHash(
   this: void,
   pointer: Entity | RoomDescriptor | RoomDescriptorReadOnly,
-): int;
+): PtrHash;
 declare function Random(this: void): int;
 declare function RandomVector(this: void): Vector;
