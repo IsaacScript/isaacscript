a860a1a1e79608e1c7b5625a531a0d6fca97220e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-28 00:26:15 -0400

    better comment for RandomVector

--- typings/functions.d.ts
+++ typings/functions.d.ts
@@ -14,4 +14,5 @@ declare function GetPtrHash(
   pointer: Entity | RoomDescriptor | RoomDescriptorReadOnly,
 ): PtrHash;
 declare function Random(this: void): int;
+/** Returns a random vector between (-1, -1) and (1, 1). */
 declare function RandomVector(this: void): Vector;
