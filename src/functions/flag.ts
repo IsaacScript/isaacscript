825814be17b2b3aacb3caad04f33a5266ac69bf1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-08-30 23:44:53 -0400

    making flag functions variadic

--- src/functions/flag.ts
+++ src/functions/flag.ts
@@ -1,24 +1,37 @@
 /**
  * Helper function to add a bit flag to an existing set of bit flags.
+ * This is a variadic function, so pass as many flags as you want to add.
  *
- * Example:
+ * Example 1:
  * ```
  * // Give the player spectral tears
  * const player = Isaac.GetPlayer();
  * player.TearFlags = addFlag(player.TearFlags, TearFlags.TEAR_SPECTRAL);
  * ```
  *
+ * Example 2:
+ * ```
+ * // Give the player spectral and homing tears
+ * const player = Isaac.GetPlayer();
+ * player.TearFlags = addFlag(player.TearFlags, TearFlags.TEAR_SPECTRAL, TearFlags.TEAR_HOMING);
+ * ```
+ *
  * @param flags The existing set of bit flags.
- * @param flag The bit flag to add.
+ * @param flag One or more bit flags to add, each as a separate argument.
  * @returns The combined bit flags.
  */
-export function addFlag(flags: int, flag: int): int {
-  return flags | flag;
+export function addFlag(flags: int, ...flag: int[]): int {
+  for (const f of flag) {
+    flags |= f;
+  }
+
+  return flags;
 }
 
 /**
- * Helper function to check to see if a particular bit flag is set to true on an existing set of bit
- * flags.
+ * Helper function to check to see if a particular bit flag is set to true.
+ * This is a variadic function, so pass as many flags as you want to check for.
+ * If passed multiple flags, it will only return true if all of the flags are set.
  *
  * Example:
  * ```
@@ -29,14 +42,21 @@ export function addFlag(flags: int, flag: int): int {
  * ```
  *
  * @param flags The existing set of bit flags.
- * @param flag The bit flag to check for.
+ * @param flag One or more bit flags to check for, each as a separate argument.
  */
-export function hasFlag(flags: int, flag: int): boolean {
-  return (flags & flag) === flag;
+export function hasFlag(flags: int, ...flag: int[]): boolean {
+  for (const f of flag) {
+    if (!((flags & f) === f)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
  * Helper function to remove a bit flag from an existing set of bit flags.
+ * This is a variadic function, so pass as many flags as you want to remove.
  *
  * Example:
  * ```
@@ -46,9 +66,13 @@ export function hasFlag(flags: int, flag: int): boolean {
  * ```
  *
  * @param flags The existing set of bit flags.
- * @param flag The bit flag to remove.
+ * @param flag One or more bit flags to remove, each as a separate argument.
  * @returns The combined bit flags.
  */
-export function removeFlag(flags: int, flag: int): int {
-  return flags & ~flag;
+export function removeFlag(flags: int, ...flag: int[]): int {
+  for (const f of flag) {
+    flags &= ~f;
+  }
+
+  return flags;
 }
