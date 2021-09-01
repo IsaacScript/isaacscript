332a79f6f3785a4fd8168f32293664a4a409f078
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-09-01 16:06:42 -0400

    better stat comments

--- src/functions/tears.ts
+++ src/functions/tears.ts
@@ -1,6 +1,7 @@
 /**
- * Converts the specified amount of tears stat into MaxFireDelay and adds it to the player. This
- * function should only be used inside the EvaluateCache callback.
+ * - Converts the specified amount of tears stat into MaxFireDelay and adds it to the player.
+ * - This function should only be used inside the EvaluateCache callback.
+ * - In this context, the "tears stat" represents what is shown on the in-game stat UI.
  */
 export function addTearsStat(player: EntityPlayer, tearsStat: float): void {
   const existingTearsStat = getTearsStat(player.MaxFireDelay);
@@ -10,18 +11,21 @@ export function addTearsStat(player: EntityPlayer, tearsStat: float): void {
 }
 
 /**
- * The `EntityPlayer` object stores a player's tear rate in the `MaxFireDelay` attribute. This is
- * equivalent to how many tears the player can shoot per frame. If you already have a "tears" stat
- * and you want to convert it back to MaxFireDelay, then use this function.
+ * - The `EntityPlayer` object stores a player's tear rate in the `MaxFireDelay` attribute. This is
+ * equivalent to how many tears the player can shoot per frame.
+ * - If you already have a "tears" stat and you want to convert it back to MaxFireDelay, then use
+ * this function.
+ * - In this context, the "tears stat" represents what is shown on the in-game stat UI.
  */
 export function getFireDelay(tearsStat: float): float {
   return math.max(30 / tearsStat - 1, -0.9999);
 }
 
 /**
- * The `EntityPlayer` object stores a player's tear rate in the `MaxFireDelay` attribute. This is
- * equivalent to how many tears the player can shoot per frame. If you want to convert this to the
- * "tears" stat that is referred to on the wiki and so forth, then use this function.
+ * - The `EntityPlayer` object stores a player's tear rate in the `MaxFireDelay` attribute. This is
+ * equivalent to how many tears the player can shoot per frame.
+ * - If you want to convert this to the "tears" stat that is shown on the in-game stat UI, then use
+ * this function.
  */
 export function getTearsStat(fireDelay: float): float {
   return 30 / (fireDelay + 1);
