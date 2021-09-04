339e9cacd763e89675de047312a9755e1c6f119b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-09-03 22:51:07 -0400

    bugfix for PostPurchase

--- src/callbacks/postPurchase.ts
+++ src/callbacks/postPurchase.ts
@@ -12,7 +12,7 @@ const v = {
   room: {
     /** Indexed by EntityPickup.Index. Only tracks pickups with a price. */
     pickupMap: new Map<int, PickupDescription>(),
-    playerHoldingItemMap: new Map<PlayerIndex, boolean>(),
+    playerHoldingItemLastFrameMap: new Map<PlayerIndex, boolean>(),
   },
 };
 
@@ -34,12 +34,12 @@ function postUpdate() {
 
   const pickups = Isaac.FindByType(EntityType.ENTITY_PICKUP);
   const players = getPlayers();
-  checkPickupsGone(pickups, players);
+  checkPickupsPurchased(pickups, players);
   storePickupsInMap(pickups);
   storePlayersInMap(players);
 }
 
-function checkPickupsGone(pickups: Entity[], players: EntityPlayer[]) {
+function checkPickupsPurchased(pickups: Entity[], players: EntityPlayer[]) {
   for (const [index, pickupDescription] of v.room.pickupMap) {
     // First, see if a pickup that existed on the last frame is now gone
     if (pickupIndexExists(index, pickups)) {
@@ -54,12 +54,13 @@ function checkPickupsGone(pickups: Entity[], players: EntityPlayer[]) {
     for (const player of players) {
       const playerHoldingItem = player.IsHoldingItem();
       const playerIndex = getPlayerIndex(player);
-      const playerHoldingItemOnLastFrame =
-        v.room.playerHoldingItemMap.get(playerIndex);
-      if (playerHoldingItemOnLastFrame === undefined) {
+      const playerHoldingItemLastFrame =
+        v.room.playerHoldingItemLastFrameMap.get(playerIndex);
+      if (playerHoldingItemLastFrame === undefined) {
         continue;
       }
-      if (!playerHoldingItemOnLastFrame && playerHoldingItem) {
+
+      if (!playerHoldingItemLastFrame && playerHoldingItem) {
         // Assume that this is the player that purchased the pickup
         postPurchase.fire(
           player,
@@ -93,13 +94,13 @@ function storePlayersInMap(players: EntityPlayer[]) {
   for (const player of players) {
     const playerIndex = getPlayerIndex(player);
     const holdingItem = player.IsHoldingItem();
-    v.room.playerHoldingItemMap.set(playerIndex, holdingItem);
+    v.room.playerHoldingItemLastFrameMap.set(playerIndex, holdingItem);
   }
 }
 
 function pickupIndexExists(index: int, pickups: Entity[]) {
   for (const entity of pickups) {
-    if (entity.Index === index) {
+    if (entity.Index === index && entity.Exists()) {
       return true;
     }
   }
