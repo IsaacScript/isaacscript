7086fab5511eb3f48f5f72a66c308e8adb3db321
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-09-04 23:05:54 -0400

    better controller enum labels

--- typings/unofficial/enums.d.ts
+++ typings/unofficial/enums.d.ts
@@ -182,29 +182,25 @@ declare const enum Controller {
   DPAD_UP = 2,
   DPAD_DOWN = 3,
   /** A, X and B on Xbox, Playstation and Nintendo respectively. */
-  CPAD_DOWN = 4,
+  BUTTON_A = 4,
   /** B, O and A on Xbox, Playstation and Nintendo respectively. */
-  CPAD_RIGHT = 5,
+  BUTTON_B = 5,
   /** X, □ and Y on Xbox, Playstation and Nintendo respectively. */
-  CPAD_LEFT = 6,
+  BUTTON_X = 6,
   /** Y, Δ and X on Xbox, Playstation and Nintendo respectively. */
-  CPAD_UP = 7,
-  /** Left shoulder */
-  BUTTON_LB = 8,
-  /** Left trigger */
-  BUTTON_LT = 9,
-  /** Left stick */
-  BUTTON_LS = 10,
-  /** Right shoulder */
-  BUTTON_RB = 11,
-  /** Right trigger */
-  BUTTON_RT = 12,
-  /** Right stick */
-  BUTTON_RS = 13,
+  BUTTON_Y = 7,
+  /** Left shoulder button. */
+  BUMPER_LEFT = 8,
+  TRIGGER_LEFT = 9,
+  STICK_LEFT = 10,
+  /** Right shoulder button. */
+  BUMPER_RIGHT = 11,
+  TRIGGER_RIGHT = 12,
+  STICK_RIGHT = 13,
   /** Select, Share and - on Xbox, Playstation and Nintendo respectively. */
-  SELECT = 14,
+  BUTTON_BACK = 14,
   /** Start, Options and + on Xbox, Playstation and Nintendo respectively. */
-  START = 15,
+  BUTTON_START = 15,
 }
 
 declare const enum LineCheckMode {
