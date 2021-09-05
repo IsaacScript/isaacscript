603550eade8f21ec9c6e0ab0953105fcb5ff58f7
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-09-04 22:30:27 -0400

    refactoring StageAPI into an interface

--- typings/mods/StageAPI.d.ts
+++ typings/mods/StageAPI.d.ts
@@ -1,4 +1,4 @@
-export {};
+declare const StageAPI: StageAPIInterface | undefined;
 
 interface StageAPICallbackParameters {
   [StageCallback.PRE_ROOM_LAYOUT_CHOOSE]: [
@@ -26,76 +26,74 @@ interface StageAPICallbackParameters {
   ];
 }
 
-declare global {
-  /** @noSelf */
-  namespace StageAPI {
-    const StageOverride: {
-      CatacombsOne: 1;
-      CatacombsTwo: 2;
-    };
+/** @noSelf */
+interface StageAPIInterface {
+  StageOverride: {
+    CatacombsOne: 1;
+    CatacombsTwo: 2;
+  };
 
-    /**
-     * Creates a new custom stage.
-     *
-     * @param name MUST BE UNIQUE. USED TO IDENTIFY STAGE AND FOR SAVING CURRENT STAGE.
-     * @param noSetReplaces Replaces defaults to catacombs one if noSetReplaces is not set.
-     */
-    function CustomStage(
-      name: string,
-      StageOverrideStage?: StageOverrideStage,
-      noSetReplaces?: boolean,
-    ): CustomStage;
+  /**
+   * Creates a new custom stage.
+   *
+   * @param name MUST BE UNIQUE. USED TO IDENTIFY STAGE AND FOR SAVING CURRENT STAGE.
+   * @param noSetReplaces Replaces defaults to catacombs one if noSetReplaces is not set.
+   */
+  CustomStage(
+    name: string,
+    StageOverrideStage?: StageOverrideStage,
+    noSetReplaces?: boolean,
+  ): CustomStage;
 
-    function RoomGfx(
-      Backdrop: Backdrop,
-      GridGfx: GridGfx | undefined,
-      shadingName: string,
-      shadingPrefix: string,
-    ): RoomGfx;
+  RoomGfx(
+    Backdrop: Backdrop,
+    GridGfx: GridGfx | undefined,
+    shadingName: string,
+    shadingPrefix: string,
+  ): RoomGfx;
 
-    function RoomsList(name: string, ...layouts: unknown[]): RoomsList;
-    function GridGfx(): GridGfx;
+  RoomsList(name: string, ...layouts: unknown[]): RoomsList;
+  GridGfx(): GridGfx;
 
-    /**
-     * Stores a function and its params in a table indexed by `ID` and sorted by `priority`,
-     * where low priority is at the start.
-     */
-    function AddCallback<T extends keyof StageAPICallbackParameters>(
-      modID: string,
-      id: T,
-      priority: int,
-      ...args: StageAPICallbackParameters[T]
-    ): void;
+  /**
+   * Stores a function and its params in a table indexed by `ID` and sorted by `priority`,
+   * where low priority is at the start.
+   */
+  AddCallback<T extends keyof StageAPICallbackParameters>(
+    modID: string,
+    id: T,
+    priority: int,
+    ...args: StageAPICallbackParameters[T]
+  ): void;
 
-    /** Unregisters all mod callbacks, should be used when a mod loads, useful for `luamod`. */
-    function UnregisterCallbacks(modID: Mod): void;
+  /** Unregisters all mod callbacks, should be used when a mod loads, useful for `luamod`. */
+  UnregisterCallbacks(modID: Mod): void;
 
-    /** Teleports the player(s) to a specified stage */
-    function GotoCustomStage(
-      CustomStage: CustomStage,
-      playTransition?: boolean,
-      noForgetSeed?: boolean,
-    ): void;
+  /** Teleports the player(s) to a specified stage */
+  GotoCustomStage(
+    CustomStage: CustomStage,
+    playTransition?: boolean,
+    noForgetSeed?: boolean,
+  ): void;
 
-    /**
-     * Convenience function that assembles filenames and packages them in a {@link Backdrop} for
-     * you.
-     *
-     * @param prefix the path to the directory containing the backdrop spritesheets,
-     * as well as any shared prefix.
-     *
-     *  Ex: "gfx/backdrop/revel1/glacier/main_"
-     * @param suffix Generally the file extension, i.e. `".png"`.
-     */
-    function BackdropHelper(
-      backdrop: {
-        Walls: string[];
-        NFloors: string[];
-        LFloors: string[];
-        Corners: string[];
-      },
-      prefix: string,
-      suffix: string,
-    ): Backdrop;
-  }
+  /**
+   * Convenience function that assembles filenames and packages them in a {@link Backdrop} for
+   * you.
+   *
+   * @param prefix the path to the directory containing the backdrop spritesheets,
+   * as well as any shared prefix.
+   *
+   *  Ex: "gfx/backdrop/revel1/glacier/main_"
+   * @param suffix Generally the file extension, i.e. `".png"`.
+   */
+  BackdropHelper(
+    backdrop: {
+      Walls: string[];
+      NFloors: string[];
+      LFloors: string[];
+      Corners: string[];
+    },
+    prefix: string,
+    suffix: string,
+  ): Backdrop;
 }
