965c49c9218f3c80e53e2f7c141f08609e67c679
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-09-12 11:58:42 -0400

    adding logMap and logSet helper functions

--- src/functions/log.ts
+++ src/functions/log.ts
@@ -5,6 +5,9 @@ declare global {
   function getParentFunctionDescription(this: void, levels: int): string;
 }
 
+/**
+ * Helper function to prefix the name of the function and the line number before a debug message.
+ */
 export function getDebugPrependString(
   msg: string,
   // We use 3 as a default because:
@@ -101,4 +104,20 @@ export function logAllUseFlags(flags: int): void {
 
 export function logEntity(entity: Entity): void {
   log(`Entity: ${entity.Type}.${entity.Variant}.${entity.SubType}`);
+}
+
+export function logMap(map: Map<AnyNotNil, unknown>): void {
+  log("Printing out map:");
+  for (const [key, value] of map.entries()) {
+    log(`  Key: ${key}, Value: ${value}`);
+  }
+  log(`The size of the map was: ${map.size}`);
+}
+
+export function logSet(set: Set<AnyNotNil>): void {
+  log("Printing out set:");
+  for (const value of set.values()) {
+    log(`  Value: ${value}`);
+  }
+  log(`The size of the set was: ${set.size}`);
 }
