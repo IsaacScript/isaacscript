cb758de5366c21bea107c50669666811bb04cf8f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-10-09 02:25:34 -0400

    adding socket definitions

---
+++ typings/socket.d.ts
@@ -0,0 +1,21 @@
+// The "socket.lua" module exists at:
+// C:\Program Files (x86)\Steam\steamapps\common\The Binding of Isaac Rebirth\resources\scripts\socket.lua
+// It is intended to be consumed by mods via:
+// local socket = require("socket")
+// (but it requires that the "--luadebug" flag is enabled)
+// We need to specify the "@noResolution" tstl compiler annotation,
+// since the "json.lua" file is not supposed to exist inside of end-user mods
+
+/** @noResolution */
+declare module "socket" {
+  function tcp(this: void): SocketClient;
+  function udp(this: void): SocketClient;
+}
+
+interface SocketClient {
+  receive(): LuaMultiReturn<[data: string | undefined, errMsg: string]>;
+  send(
+    msg: string,
+  ): LuaMultiReturn<[sentBytes: int | undefined, errMsg: string]>;
+  settimeout(timeout: int): void;
+}
