89a5e5189e6dfb279509e07ceee645688d93afec
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-10-09 02:34:42 -0400

    changing Socket from a module to an interface

--- typings/socket.d.ts
+++ typings/socket.d.ts
@@ -3,13 +3,10 @@
 // It is intended to be consumed by mods via:
 // local socket = require("socket")
 // (but it requires that the "--luadebug" flag is enabled)
-// We need to specify the "@noResolution" tstl compiler annotation,
-// since the "json.lua" file is not supposed to exist inside of end-user mods
 
-/** @noResolution */
-declare module "socket" {
-  function tcp(this: void): SocketClient;
-  function udp(this: void): SocketClient;
+declare interface Socket {
+  tcp(this: void): SocketClient;
+  udp(this: void): SocketClient;
 }
 
 declare interface SocketClient {
