e015096380f1a65214c9eee7f5e2da5a890ce320
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-03 19:29:49 -0400

    adding getMaxTrinketID and getTrinketSet helper functions

--- src/functions/trinkets.ts
+++ src/functions/trinkets.ts
@@ -1,4 +1,5 @@
 import { GOLDEN_TRINKET_SHIFT } from "../constants";
+import { copySet } from "./util";
 
 export interface TrinketSituation {
   trinketTypeRemoved: TrinketType | int;
@@ -96,4 +97,32 @@ export function giveTrinketsBack(
   if (trinketSituation.trinket2 !== TrinketType.TRINKET_NULL) {
     player.AddTrinket(trinketSituation.trinket2, false);
   }
+}
+
+export function getMaxTrinketID(): int {
+  const itemConfig = Isaac.GetItemConfig();
+  return itemConfig.GetTrinkets().Size - 1;
+}
+
+const TRINKET_SET = new Set<TrinketType | int>();
+
+function initSet() {
+  const itemConfig = Isaac.GetItemConfig();
+
+  for (let trinketType = 1; trinketType <= getMaxTrinketID(); trinketType++) {
+    const itemConfigTrinket = itemConfig.GetTrinket(trinketType);
+    if (itemConfigTrinket !== undefined) {
+      TRINKET_SET.add(trinketType);
+    }
+  }
+}
+
+/** Returns a set containing every valid trinket type in the game, including modded items. */
+export function getTrinketSet(): Set<TrinketType | int> {
+  // Lazy initialize the set
+  if (TRINKET_SET.size === 0) {
+    initSet();
+  }
+
+  return copySet(TRINKET_SET);
 }
