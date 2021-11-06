3094f6ce0feceb28533e7c71ff15503caf1d9db8
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-06 10:51:29 -0400

    adding many entity variant enums

--- typings/unofficial/enumsVariants.d.ts
+++ typings/unofficial/enumsVariants.d.ts
@@ -59,12 +59,134 @@ declare const enum KnifeVariant {
   TECH_SWORD = 11,
 }
 
+/** For EntityType.ENTITY_GAPER (10) */
+declare const enum GaperVariant {
+  FROWNING_GAPER = 0,
+  GAPER = 1,
+  FLAMING_GAPER = 2,
+  ROTTEN_GAPER = 3,
+}
+
+/** For EntityType.ENTITY_GUSHER (11) */
+declare const enum GusherVariant {
+  GUSHER = 0,
+  PACER = 1,
+}
+
+/** For EntityType.ENTITY_POOTER (14) */
+declare const enum PooterVariant {
+  POOTER = 0,
+  SUPER_POOTER = 1,
+  TAINTED_POOTER = 2,
+}
+
+/** For EntityType.ENTITY_CLOTTY (15) */
+declare const enum ClottyVariant {
+  CLOTTY = 0,
+  CLOT = 1,
+  BLOB = 2,
+  GRILLED_CLOTTY = 3,
+}
+
+/** For EntityType.ENTITY_MULLIGAN (16) */
+declare const enum MulliganVariant {
+  MULLIGAN = 16,
+  MULLIGOON = 1,
+  MULLIBOOM = 2,
+}
+
+/** For EntityType.ENTITY_SHOPKEEPER (17) */
+declare const enum ShopkeeperVariant {
+  SHOPKEEPER = 0,
+  SECRET_ROOM_KEEPER = 1,
+  ERROR_ROOM_KEEPER = 2,
+  SPECIAL_SHOPKEEPER = 3,
+  SPECIAL_SECRET_ROOM_KEEPER = 4,
+}
+
+/** For EntityType.ENTITY_LARRYJR (19) */
+declare const enum LarryJrVariant {
+  LARRY_JR = 0,
+  THE_HOLLOW = 1,
+  TUFF_TWIN = 2,
+  THE_SHELL = 3,
+}
+
+/** For EntityType.ENTITY_HIVE (22) */
+declare const enum HiveVariant {
+  HIVE = 0,
+  DROWNED_HIVE = 1,
+  HOLY_MULLIGAN = 2,
+  TAINTED_MULLIGAN = 3,
+}
+
 /** For EntityType.ENTITY_CHARGER (23) */
 declare const enum ChargerVariant {
   CHARGER = 0,
   DROWNED_CHARGER = 1,
   DANK_CHARGER = 2,
   CARRION_PRINCESS = 3,
+}
+
+/** For EntityType.ENTITY_GLOBIN (24) */
+declare const enum GlobinVariant {
+  GLOBIN = 0,
+  GAZING_GLOBIN = 1,
+  DANK_GLOBIN = 2,
+  CURSED_GLOBIN = 3,
+}
+
+/** For EntityType.ENTITY_BOOMFLY (25) */
+declare const enum BoomFlyVariant {
+  BOOM_FLY = 0,
+  RED_BOOM_FLY = 1,
+  DROWNED_BOOM_FLY = 2,
+  DRAGON_FLY = 3,
+  BONE_FLY = 4,
+  SICK_BOOM_FLY = 5,
+  TAINTED_BOOM_FLY = 6,
+}
+
+/** For EntityType.ENTITY_MAW (26) */
+declare const enum MawVariant {
+  MAW = 0,
+  RED_MAW = 1,
+  PSYCHIC_MAW = 2,
+}
+
+/** For EntityType.ENTITY_HOST (27) */
+declare const enum HostVariant {
+  HOST = 0,
+  RED_HOST = 1,
+  HARD_HOST = 2,
+}
+
+/** For EntityType.ENTITY_CHUB (28) */
+declare const enum ChubVariant {
+  CHUB = 0,
+  CHAD = 1,
+  CARRION_QUEEN = 2,
+}
+
+/** For EntityType.ENTITY_HOPPER (29) */
+declare const enum HopperVariant {
+  HOPPER = 0,
+  TRITE = 1,
+  EGGY = 2,
+  TAINTED_HOPPER = 3,
+}
+
+/** For EntityType.ENTITY_BOIL (30) */
+declare const enum BoilVariant {
+  BOIL = 0,
+  GUT = 1,
+  SACK = 2,
+}
+
+/** For EntityType.ENTITY_SPITY (31) */
+declare const enum SpittyVariant {
+  SPITTY = 0,
+  TAINTED_SPITTY = 1,
 }
 
 /** For EntityType.ENTITY_FIREPLACE (33) */
@@ -80,6 +202,29 @@ declare const enum FireplaceVariant {
   MOVEABLE_PURPLE = 13,
 }
 
+/** For EntityType.ENTITY_LEAPER (34) */
+declare const enum LeaperVariant {
+  LEAPER = 0,
+  STICKY_LEAPER = 1,
+}
+
+/** For EntityType.ENTITY_MRMAW (35) */
+declare const enum MrMawVariant {
+  MR_MAW = 0,
+  MR_MAW_HEAD = 1,
+  MR_RED_MAW = 2,
+  MR_RED_MAW_HEAD = 3,
+  MR_MAW_NECK = 10,
+}
+
+/** For EntityType.ENTITY_BABY (38) */
+declare const enum BabyVariant {
+  BABY = 0,
+  ANGELIC_BABY = 1,
+  ULTRA_PRIDE_BABY = 2,
+  WRINKLY_BABY = 3,
+}
+
 /** For EntityType.ENTITY_VIS (39) */
 declare const enum VisVariant {
   VIS = 0,
@@ -89,10 +234,139 @@ declare const enum VisVariant {
   CHUBBER_PROJECTILE = 22,
 }
 
+/** For EntityType.ENTITY_GUTS (40) */
+declare const enum GutsVariant {
+  GUTS = 0,
+  SCARRED_GUTS = 1,
+  SLOG = 2,
+}
+
+/** For EntityType.ENTITY_KNIGHT (41) */
+declare const enum KnightVariant {
+  KNIGHT = 0,
+  SELFLESS_KNIGHT = 1,
+  LOOSE_KNIGHT = 2,
+  BRAINLESS_KNIGHT = 3,
+  BLACK_KNIGHT = 4,
+}
+
+/** For EntityType.ENTITY_STONEHEAD (42) */
+declare const enum StoneHeadVariant {
+  STONE_GRIMACE = 0,
+  VOMIT_GRIMACE = 1,
+  TRIPLE_GRIMACE = 2,
+}
+
+/** For EntityType.ENTITY_MONSTRO2 (43) */
+declare const enum Monstro2Variant {
+  MONSTRO_2 = 0,
+  GISH = 1,
+}
+
+/** For EntityType.ENTITY_POKY (44) */
+declare const enum PokyVariant {
+  POKY = 0,
+  SLIDE = 1,
+}
+
 /** For EntityType.ENTITY_MOM (45) */
 declare const enum MomVariant {
   MOM = 0,
   STOMP = 10,
+}
+
+/** For EntityType.ENTITY_SLOTH (46) */
+declare const enum SlothVariant {
+  SLOTH = 0,
+  SUPER_SLOTH = 1,
+  ULTRA_PRIDE = 2,
+}
+
+/** For EntityType.ENTITY_LUST (47) */
+declare const enum LustVariant {
+  LUST = 0,
+  SUPER_LUST = 1,
+}
+
+/** For EntityType.ENTITY_WRATH (48) */
+declare const enum WrathVariant {
+  WRATH = 0,
+  SUPER_WRATH = 1,
+}
+
+/** For EntityType.ENTITY_GLUTTONY (49) */
+declare const enum GluttonyVariant {
+  GLUTTONY = 0,
+  SUPER_GLUTTONY = 1,
+}
+
+/** For EntityType.ENTITY_GREED (50) */
+declare const enum GreedVariant {
+  GREED = 0,
+  SUPER_GREED = 1,
+}
+
+/** For EntityType.ENTITY_ENVY (51) */
+declare const enum EnvyVariant {
+  ENVY = 0,
+  SUPER_ENVY = 1,
+  ENVY_BIG = 10,
+  SUPER_ENVY_BIG = 11,
+  ENVY_MEDIUM = 20,
+  SUPER_ENVY_MEDIUM = 21,
+  ENVY_SMALL = 30,
+  SUPER_ENVY_SMALL = 31,
+}
+
+/** For EntityType.ENTITY_PRIDE (52) */
+declare const enum PrideVariant {
+  PRIDE = 0,
+  SUPER_PRIDE = 1,
+}
+
+/** For EntityType.ENTITY_DOPLE (53) */
+declare const enum DopleVariant {
+  DOPLE = 0,
+  EVIL_TWIN = 1,
+}
+
+/** For EntityType.ENTITY_LEECH (55) */
+declare const enum LeechVariant {
+  LEECH = 0,
+  KAMIKAZE_LEECH = 1,
+  HOLY_LEECH = 2,
+}
+
+/** For EntityType.ENTITY_MEMBRAIN (57) */
+declare const enum MemBrainVariant {
+  MEMBRAIN = 0,
+  MAMA_GUTS = 1,
+  DEAD_MEAT = 2,
+}
+
+/** For EntityType.ENTITY_PARA_BITE (58) */
+declare const enum ParaBiteVariant {
+  PARA_BITE = 0,
+  SCARRED_PARA_BITE = 1,
+}
+
+/** For EntityType.ENTITY_EYE (60) */
+declare const enum EyeVariant {
+  EYE = 0,
+  BLOODSHOT_EYE = 1,
+  HOLY_EYE = 2,
+}
+
+/** For EntityType.ENTITY_SUCKER (61) */
+declare const enum SuckerVariant {
+  SUCKER = 0,
+  SPIT = 1,
+  SOUL_SUCKER = 2,
+  INK = 3,
+  MAMA_FLY = 4,
+  BULB = 5,
+  BLOOD_FLY = 6,
+  TAINTED_SUCKER = 7,
 }
 
 /** For EntityType.ENTITY_PIN (62) */
@@ -103,6 +377,13 @@ declare const enum PinVariant {
   WORMWOOD = 3,
 }
 
+/** For EntityType.ENTITY_WAR (65) */
+declare const enum WarVariant {
+  WAR = 0,
+  CONQUEST = 1,
+  WAR_WITHOUT_HORSE = 2,
+}
+
 /** For EntityType.ENTITY_DEATH (66) */
 declare const enum DeathVariant {
   DEATH = 0,
@@ -111,12 +392,40 @@ declare const enum DeathVariant {
   DEATH_WITHOUT_HORSE = 30,
 }
 
+/** For EntityType.ENTITY_DUKE (67) */
+declare const enum DukeVariant {
+  DUKE_OF_FLIES = 0,
+  THE_HUSK = 1,
+}
+
 /** For EntityType.ENTITY_PEEP (68) */
 declare const enum PeepVariant {
   PEEP = 0,
   BLOAT = 1,
   PEEP_EYE = 10,
   BLOAT_EYE = 11,
+}
+
+/** For EntityType.ENTITY_LOKI (69) */
+declare const enum LokiVariant {
+  LOKI = 0,
+  LOKII = 1,
+}
+
+/**
+ * For EntityType.ENTITY_FISTULA_BIG (71), EntityType.ENTITY_FISTULA_MEDIUM (72),
+ * and EntityType.ENTITY_FISTULA_SMALL (73)
+ */
+declare const enum FistulaVariant {
+  FISTULA = 0,
+  TERATOMA = 1,
+}
+
+/** For EntityType.ENTITY_MOMS_HEART (78) */
+declare const enum MomsHeartVariant {
+  MOMS_HEART = 0,
+  IT_LIVES = 1,
+  MOMS_GUTS = 2,
 }
 
 /** For EntityType.ENTITY_GEMINI (79) */
@@ -142,11 +451,73 @@ declare const enum SatanVariant {
   STOMP = 10,
 }
 
+/** For EntityType.ENTITY_GURGLE (87) */
+declare const enum GurgleVariant {
+  GURGLE = 0,
+  CRACKLE = 1,
+}
+
+/** For EntityType.ENTITY_WALKINGBOIL (88) */
+declare const enum WalkingBoilVariant {
+  WALKING_BOIL = 0,
+  WALKING_GUT = 1,
+  WALKING_SACK = 2,
+}
+
+/** For EntityType.ENTITY_HEART (92) */
+declare const enum HeartVariant {
+  HEART = 0,
+  HALF_HEART = 1,
+}
+
+/** For EntityType.ENTITY_MASK (93) */
+declare const enum MaskVariant {
+  MASK = 0,
+  MASK_2 = 1,
+}
+
+/** For EntityType.ENTITY_WIDOW (100) */
+declare const enum WidowVariant {
+  WIDOW = 0,
+  THE_WRETCHED = 1,
+}
+
+/** For EntityType.ENTITY_DADDYLONGLEGS (101) */
+declare const enum DaddyLongLegsVariant {
+  DADDY_LONG_LEGS = 0,
+  TRIACHNID = 1,
+}
 /** For EntityType.ENTITY_ISAAC (102) */
 declare const enum IsaacVariant {
   ISAAC = 0,
   BLUE_BABY = 1,
   BLUE_BABY_HUSH = 2,
+}
+
+/** For EntityType.ENTITY_CONSTANT_STONE_SHOOTER (202) */
+declare const enum ConstantStoneShooterVariant {
+  CONSTANT_STONE_SHOOTER = 0,
+  CROSS_STONE_SHOOTER = 10,
+  CROSS_STONE_SHOOTER_ALWAYS_ON = 11,
+}
+
+/** For EntityType.ENTITY_BABY_LONG_LEGS (206) */
+declare const enum BabyLongLegsVariant {
+  BABY_LONG_LEGS = 0,
+  SMALL_BABY_LONG_LEGS = 1,
+}
+
+/** For EntityType.ENTITY_CRAZY_LONG_LEGS (207) */
+declare const enum CrazyLongLegsVariant {
+  CRAZY_LONG_LEGS = 0,
+  SMALL_CRAZY_LONG_LEGS = 1,
+}
+
+/** For EntityType.ENTITY_FATTY (208) */
+declare const enum FattyVariant {
+  FATTY = 0,
+  PALE_FATTY = 1,
+  FLAMING_FATTY = 2,
 }
 
 /** For EntityType.ENTITY_DEATHS_HEAD (212) */
@@ -162,6 +533,94 @@ declare const enum DeathsHeadVariant {
   REDSKULL = 4,
 }
 
+/** For EntityType.ENTITY_SWINGER (216) */
+declare const enum SwingerVariant {
+  SWINGER = 0,
+  SWINGER_HEAD = 1,
+  SWINGER_NECK = 2,
+}
+
+/** For EntityType.ENTITY_DIP (217) */
+declare const enum DipVariant {
+  DIP = 0,
+  CORN = 1,
+  BROWNIE_CORN = 2,
+  BIG_CORN = 3,
+}
+
+/** For EntityType.ENTITY_SQUIRT (220) */
+declare const enum SquirtVariant {
+  SQUIRT = 0,
+  DANK_SQUIRT = 1,
+}
+
+/** For EntityType.ENTITY_SKINNY (226) */
+declare const enum SkinnyVariant {
+  SKINNY = 0,
+  ROTTY = 1,
+  CRISPY = 2,
+}
+
+/** For EntityType.ENTITY_BONY (227) */
+declare const enum BonyVariant {
+  BONY = 0,
+  HOLY_BONY = 1,
+}
+
+/** For EntityType.ENTITY_HOMUNCULUS (228) */
+declare const enum HomunculusVariant {
+  HOMUNCULUS = 0,
+  HOMUNCULUS_CORD = 10,
+}
+
+/** For EntityType.ENTITY_TUMOR (229) */
+declare const enum TumorVariant {
+  TUMOR = 0,
+  PLANETOID = 1,
+}
+
+/** For EntityType.ENTITY_NERVE_ENDING (231) */
+declare const enum NerveEndingVariant {
+  NERVE_ENDING = 0,
+  NERVE_ENDING_2 = 1,
+}
+
+/** For EntityType.ENTITY_GURGLING (237) */
+declare const enum GurglingVariant {
+  GURGLING = 0,
+  GURGLING_BOSS = 1,
+  TURDLING = 2,
+}
+
+/** For EntityType.ENTITY_GRUB (239) */
+declare const enum GrubVariant {
+  GRUB = 0,
+  CORPSE_EATER = 1,
+  CARRION_RIDER = 2,
+}
+
+/** For EntityType.ENTITY_WALL_CREEP (240) */
+declare const enum WallCreepVariant {
+  WALL_CREEP = 0,
+  SOY_CREEP = 1,
+  RAG_CREEP = 2,
+  TAINTED_SOY_CREEP = 3,
+}
+
+/** For EntityType.ENTITY_RAGE_CREEP (241) */
+declare const enum RageCreepVariant {
+  RAGE_CREEP = 0,
+  SPLIT_RAGE_CREEP = 1,
+}
+
+/** For EntityType.ENTITY_ROUND_WORM (244) */
+declare const enum RoundWormVariant {
+  ROUND_WORM = 0,
+  TUBE_WORM = 1,
+  TAINTED_ROUND_WORM = 2,
+  TAINTED_TUBE_WORM = 3,
+}
+
 /** For EntityType.ENTITY_RAGLING (246) */
 declare const enum RaglingVariant {
   RAGLING = 0,
@@ -172,6 +631,12 @@ declare const enum RaglingVariant {
 declare const enum BegottenVariant {
   BEGOTTEN = 0,
   BEGOTTEN_CHAIN = 10,
+}
+
+/** For EntityType.ENTITY_CONJOINED_FATTY (257) */
+declare const enum ConjoinedFattyVariant {
+  CONJOINED_FATTY = 0,
+  BLUE_CONJOINED_FATTY = 1,
 }
 
 /** For EntityType.ENTITY_THE_HAUNT (260) */
@@ -193,6 +658,12 @@ declare const enum MamaGurdyVariant {
   RIGHT_HAND = 2,
 }
 
+/** For EntityType.ENTITY_POLYCEPHALUS (269) */
+declare const enum PolycephalusVariant {
+  POLYCEPHALUS = 0,
+  THE_PILE = 1,
+}
+
 /** For EntityType.ENTITY_URIEL (271) and EntityType.ENTITY_GABRIEL (272) */
 declare const enum AngelVariant {
   NORMAL = 0,
@@ -205,6 +676,13 @@ declare const enum LambVariant {
   BODY = 10,
 }
 
+/** For EntityType.ENTITY_MEGA_SATAN (274) and EntityType.ENTITY_MEGA_SATAN_2 (275) */
+declare const enum MegaSatanVariant {
+  MEGA_SATAN = 0,
+  MEGA_SATAN_RIGHT_HAND = 1,
+  MEGA_SATAN_LEFT_HAND = 2,
+}
+
 /** EntityType.ENTITY_PITFALL (291) */
 declare const enum PitfallVariant {
   PITFALL = 0,
@@ -212,23 +690,263 @@ declare const enum PitfallVariant {
   TELEPORT_PITFALL = 2,
 }
 
-/** EntityType.ENTITY_LITTLE_HORN (404) */
+/** For EntityType.ENTITY_MOVABLE_TNT (292) */
+declare const enum MoveableTNTVariant {
+  MOVEABLE_TNT = 0,
+  MINE_CRAFTER = 1,
+}
+
+/** For EntityType.ENTITY_ULTRA_COIN (293) */
+declare const enum UltraCoinVariant {
+  SPINNER = 0,
+  KEY = 1,
+  BOMB = 2,
+  HEART = 3,
+}
+
+/** For EntityType.ENTITY_STONEY (302) */
+declare const enum StoneyVariant {
+  STONEY = 0,
+  CROSS_STONEY = 10,
+}
+
+/** For EntityType.ENTITY_PORTAL (306) */
+declare const enum PortalVariant {
+  PORTAL = 0,
+  LIL_PORTAL = 1,
+}
+
+/** For EntityType.ENTITY_LEPER (310) */
+declare const enum LeperVariant {
+  LEPER = 0,
+  LEPER_FLESH = 1,
+}
+
+/** For EntityType.ENTITY_MR_MINE (311) */
+declare const enum MrMineVariant {
+  MR_MINE = 0,
+  MR_MINE_NECK = 10,
+}
+
+/** For EntityType.ENTITY_LITTLE_HORN (404) */
 declare const enum LittleHornVariant {
   LITTLE_HORN = 0,
   DARK_BALL = 1,
 }
 
-/** EntityType.ENTITY_BIG_HORN (411) */
+/** For EntityType.ENTITY_RAG_MAN (405) */
+declare const enum RagManVariant {
+  RAG_MAN = 0,
+  RAG_MAN_HEAD = 1,
+}
+
+/** For EntityType.ENTITY_ULTRA_GREED (406) */
+declare const enum UltraGreedVariant {
+  ULTRA_GREED = 0,
+  ULTRA_GREEDIER = 1,
+}
+
+/** For EntityType.ENTITY_RAG_MEGA (409) */
+declare const enum RagMegaVariant {
+  RAG_MEGA = 0,
+  PURPLE_BALL = 1,
+  REBIRTH_PILLAR = 2,
+}
+
+/** For EntityType.ENTITY_BIG_HORN (411) */
 declare const enum BigHornVariant {
   BIG_HORN = 0,
   SMALL_HOLE = 1,
   BIG_HOLE = 2,
 }
 
+/** For EntityType.ENTITY_BLOOD_PUPPY (802) */
+declare const enum BloodPuppyVariant {
+  SMALL = 0,
+  LARGE = 1,
+}
+
+/** For EntityType.ENTITY_SUB_HORF (812) */
+declare const enum SubHorfVariant {
+  SUB_HORF = 0,
+  TAINTED_SUB_HORF = 1,
+}
+
+/** For EntityType.ENTITY_POLTY (816) */
+declare const enum PoltyVariant {
+  POLTY = 0,
+  KINETI = 1,
+}
+
+/** For EntityType.ENTITY_PREY (817) */
+declare const enum PreyVariant {
+  PREY = 0,
+  MULLIGHOUL = 1,
+}
+
+/** For EntityType.ENTITY_ROCK_SPIDER (818) */
+declare const enum RockSpiderVariant {
+  ROCK_SPIDER = 0,
+  TINTED_ROCK_SPIDER = 1,
+  COAL_SPIDER = 2,
+}
+
+/** For EntityType.ENTITY_FLY_BOMB (819) */
+declare const enum FlyBombVariant {
+  FLY_BOMB = 0,
+  ETERNAL_FLY_BOMB = 1,
+}
+
+/** For EntityType.ENTITY_DANNY (820) */
+declare const enum DannyVariant {
+  DANNY = 0,
+  COAL_BOY = 1,
+}
+
+/** For EntityType.ENTITY_GYRO (824) */
+declare const enum GyroVariant {
+  GYRO = 0,
+  GRILLED_GYRO = 1,
+}
+
+/** For EntityType.ENTITY_FACELESS (827) */
+declare const enum FacelessVariant {
+  FACELESS = 0,
+  TAINTED_FACELESS = 1,
+}
+
+/** For EntityType.ENTITY_MOLE (829) */
+declare const enum MoleVariant {
+  MOLE = 0,
+  TAINTED_MOLE = 1,
+}
+
+/** For EntityType.ENTITY_BIG_BONY (830) */
+declare const enum BigBonyVariant {
+  BIG_BONY = 0,
+  BIG_BONE = 10,
+}
+
+/** For EntityType.ENTITY_GUTTED_FATTY (831) */
+declare const enum GuttyFattyVariant {
+  GUTTED_FATTY = 0,
+  GUTTY_FATTY_EYE = 10,
+  FESTERING_GUTS = 20,
+}
+
+/** For EntityType.ENTITY_EXORCIST (832) */
+declare const enum ExorcistVariant {
+  EXORCIST = 0,
+  FANATIC = 1,
+}
+
+/** For EntityType.ENTITY_WHIPPER (834) */
+declare const enum WhipperVariant {
+  WHIPPER = 0,
+  SNAPPER = 1,
+  FLAGELLANT = 2,
+}
+
+/** For EntityType.ENTITY_PEEPER_FATTY (835) */
+declare const enum PeeperFattyVariant {
+  PEEPING_FATTY = 0,
+  PEEPING_FATTY_EYE = 10,
+}
+
+/** For EntityType.ENTITY_REVENANT (841) */
+declare const enum RevenantVariant {
+  REVENANT = 0,
+  QUAD_REVENANT = 1,
+}
+
+/** For EntityType.ENTITY_CANARY (843) */
+declare const enum CanaryVariant {
+  CANARY = 0,
+  FOREIGNER = 1,
+}
+
+/** For EntityType.ENTITY_GAPER_L2 (850) */
+declare const enum Gaper2Variant {
+  GAPER = 0,
+  HORF = 1,
+  GUSHER = 2,
+}
+
+/** For EntityType.ENTITY_CHARGER_L2 (855) */
+declare const enum Charger2Variant {
+  CHARGER = 0,
+  ELLEECH = 1,
+}
+
+/** For EntityType.ENTITY_EVIS (865) */
+declare const enum EvisVariant {
+  EVIS = 0,
+  EVIS_GUTS = 10,
+}
+
 /** For EntityType.ENTITY_DARK_ESAU (866) */
 declare const enum DarkEsauVariant {
   DARK_ESAU = 0,
   PIT = 1,
+}
+
+/** For EntityType.ENTITY_DUMP (876) */
+declare const enum DumpVariant {
+  DUMP = 0,
+  DUMP_HEAD = 1,
+}
+
+/** For EntityType.ENTITY_NEEDLE (881) */
+declare const enum NeedleVariant {
+  NEEDLE = 0,
+  PASTY = 1,
+}
+
+/** For EntityType.ENTITY_CULTIST (885) */
+declare const enum CultistVariant {
+  CULTIST = 0,
+  BLOOD_CULTIST = 1,
+  BONE_TRAP = 10,
+}
+
+/** For EntityType.ENTITY_VIS_FATTY (886) */
+declare const enum VisFattyVariant {
+  VIS_FATTY = 0,
+  FETAL_DEMON = 1,
+}
+
+/** For EntityType.ENTITY_GOAT (891) */
+declare const enum GoatVariant {
+  GOAT = 0,
+  BLACK_GOAT = 1,
+}
+
+/** For EntityType.ENTITY_VISAGE (903) */
+declare const enum VisageVariant {
+  VISAGE = 0,
+  VISAGE_MASK = 1,
+  VISAGE_CHAIN = 10,
+  VISAGE_PLASMA = 20,
+}
+
+/** For EntityType.ENTITY_SIREN (904) */
+declare const enum SirenVariant {
+  SIREN = 0,
+  SIREN_SKULL = 1,
+  SIREN_HELPER_PROJECTILE = 10,
+}
+
+/** For EntityType.ENTITY_SCOURGE (909) */
+declare const enum ScourgeVariant {
+  SCOURGE = 0,
+  SCOURGE_CHAIN = 10,
+}
+
+/** For EntityType.ENTITY_CHIMERA (910) */
+declare const enum ChimeraVariant {
+  CHIMERA = 0,
+  CHIMERA_BODY = 1,
+  CHIMERA_HEAD = 2,
 }
 
 /** For EntityType.ENTITY_ROTGUT (911) */
@@ -243,6 +961,24 @@ declare const enum MotherVariant {
   PHASE_1 = 0,
   PHASE_2 = 10,
   BALL = 100,
+}
+
+/** For EntityType.ENTITY_SINGE (915) */
+declare const enum SingeVariant {
+  SINGE = 0,
+  SINGE_BALL = 1,
+}
+
+/** For EntityType.ENTITY_RAGLICH (919) */
+declare const enum RaglichVariant {
+  RAGLICH = 0,
+  RAGLICH_ARM = 1,
+}
+
+/** For EntityType.ENTITY_CLUTCH (921) */
+declare const enum ClutchVariant {
+  CLUTCH = 0,
+  CLUTCH_ORBITAL = 1,
 }
 
 /** For EntityType.ENTITY_DOGMA (950) */
@@ -275,4 +1011,13 @@ declare const enum BeastVariant {
   BACKGROUND_PESTILENCE = 102,
   BACKGROUND_WAR = 103,
   BACKGROUND_DEATH = 104,
+}
+
+/** For EntityType.ENTITY_GENERIC_PROP (960) */
+declare const enum GenericPropVariant {
+  GENERIC_PROP = 0,
+  MOMS_DRESSER = 1,
+  MOMS_VANITY = 2,
+  COUCH = 3,
+  TV = 4,
 }
