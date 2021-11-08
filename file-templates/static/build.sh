465b1f90cc68e1d1091dd223aca7e1179254f30a
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-08 15:40:27 -0500

    adding seconds counter to build script

--- file-templates/static/build.sh
+++ file-templates/static/build.sh
@@ -6,8 +6,10 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+SECONDS=0
+
 # Convert the TypeScript to a single Lua file
 cd "$DIR"
 npx tstl
 
-echo "Success!"
+echo "Successfully built in $SECONDS seconds."
