f489506e3dc7c3fe908f5150b0887ce8ec88e1d2
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-11 17:04:12 -0500

    bugfix for playerHealth

--- src/functions/playerHealth.ts
+++ src/functions/playerHealth.ts
@@ -163,14 +163,15 @@ export function setPlayerHealth(
 }
 
 export function removeAllPlayerHealth(player: EntityPlayer): void {
-  const brokenHearts = player.GetBrokenHearts();
   const goldenHearts = player.GetGoldenHearts();
+  const boneHearts = player.GetBoneHearts();
+  const brokenHearts = player.GetBrokenHearts();
 
-  // We have to remove the exact amount of Golden Hearts or else it will bug out
+  // To avoid bugs, we have to remove the exact amount of certain types of hearts
   // We remove Golden Hearts first so that they don't break
   player.AddGoldenHearts(goldenHearts * -1);
   player.AddBrokenHearts(brokenHearts * -1);
   player.AddMaxHearts(MAX_PLAYER_HEART_CONTAINERS * -2, true);
   player.AddSoulHearts(MAX_PLAYER_HEART_CONTAINERS * -2);
-  player.AddBoneHearts(MAX_PLAYER_HEART_CONTAINERS);
+  player.AddBoneHearts(boneHearts * -1);
 }
