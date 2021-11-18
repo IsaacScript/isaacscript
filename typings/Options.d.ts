e32fdbe432fb07d3fee1a78c552ac7029c539c89
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-18 11:47:17 -0500

    adding LanguageAbbreviation enum

--- typings/Options.d.ts
+++ typings/Options.d.ts
@@ -1,38 +1,78 @@
 declare namespace Options {
   /**
    * Whether the announcer voice should play when using items, pills, cards, and runes.
-   * 0: random, 1: off, 2: always on.
+   * This cannot be changed in-game and is only exposed in the "options.ini" file.
+   * 0: random, 1: always off, 2: always on.
    */
   let AnnouncerVoiceMode: 0 | 1 | 2;
 
+  /** This can be changed from the in-game options menu. */
   let BulletVisibility: boolean;
 
-  /** Toggles the active camera. 1: on, 2: off. */
+  /**
+   * Toggles the active camera.
+   * This can be changed from the in-game options menu.
+   * 1: on, 2: off.
+   */
   let CameraStyle: 1 | 2;
 
+  /** This can be changed from the in-game options menu. */
   let ChargeBars: boolean;
 
-  /** How big the text in the debug console should be. 0: default, 1: small, 2: tiny. */
+  /**
+   * How big the text in the debug console should be.
+   * This cannot be changed in-game and is only exposed in the "options.ini" file.
+   * 0: default, 1: small, 2: tiny.
+   */
   let ConsoleFont: 0 | 1 | 2;
 
+  /** This cannot be changed in-game and is only exposed in the "options.ini" file. */
   let DebugConsoleEnabled: boolean;
+
+  /** This can be changed from the in-game options menu. */
   let DisplayPopups: boolean;
 
-  /** Sets the style of the Extra HUD. 0: off, 1: on, 2: mini. */
+  /**
+   * Sets the style of the Extra HUD.
+   * This can be changed from the in-game options menu.
+   * 0: off, 1: on, 2: mini.
+   */
   let ExtraHUDStyle: 0 | 1 | 2;
 
+  /** This cannot be changed in-game and is only exposed in the "options.ini" file. */
   let FadedConsoleDisplay: boolean;
+
+  /**
+   * The filter is a setting that makes the game look more like the Flash version (i.e. Wrath of the
+   * Lamb).
+   * This can be changed from the in-game options menu.
+   */
   let Filter: boolean;
+
+  /**
+   * The Found HUD is the in-game stat menu. The name is a reference to MissingHUD, which was a stat
+   * HUD added by a mod.
+   * This can be changed from the in-game options menu.
+   */
   let FoundHUD: boolean;
+
+  /** This can be changed from the in-game options menu. */
   let Fullscreen: boolean;
 
-  /** Valid values are from 0.5-1.5. */
+  /**
+   * This can be changed from the in-game options menu.
+   * Valid values are from 0.5-1.5.
+   */
   let Gamma: float;
 
-  /** Valid values are from 0-1. */
+  /**
+   * This can be changed from the in-game options menu.
+   * Valid values are from 0-1.
+   */
   let HUDOffset: float;
 
-  const Language: string;
+  /** This can be changed from the in-game options menu. */
+  const Language: LanguageAbbreviation;
 
   /** Valid values are from 0-1. */
   let MapOpacity: float;
--- typings/unofficial/enums.d.ts
+++ typings/unofficial/enums.d.ts
@@ -319,3 +319,14 @@ declare const enum RoomDescriptorFlag {
    */
   BLUE_REDIRECT = 1 << 18,
 }
+
+/** Listed in order of how they cycle through the options menu. */
+declare const enum LanguageAbbreviation {
+  ENGLISH = "en",
+  JAPANESE = "jp",
+  SPANISH = "es",
+  GERMAN = "de",
+  RUSSIAN = "ru",
+  KOREAN = "kr",
+  CHINESE_SIMPLE = "zh",
+}
