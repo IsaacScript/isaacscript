0aafc325a00658059fa3433c14ee5c618d29c70d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-19 00:31:17 -0500

    updating Racing+ globals

--- typings/mods/RacingPlus.d.ts
+++ typings/mods/RacingPlus.d.ts
@@ -2,7 +2,7 @@
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
  * it will be undefined.
  */
-declare const traceback: (() => void) | undefined;
+declare const sandboxTraceback: (() => void) | undefined;
 
 /**
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
