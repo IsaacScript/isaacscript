0855b73a90b5f1a41a213d7960be771ff568913d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-19 18:36:24 -0500

    bugfix for Racing+ definitions

--- typings/mods/RacingPlus.d.ts
+++ typings/mods/RacingPlus.d.ts
@@ -2,7 +2,7 @@
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
  * it will be undefined.
  */
-declare const sandboxTraceback: (() => void) | undefined;
+declare const sandboxTraceback: ((this: void) => void) | undefined;
 
 /**
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
@@ -10,4 +10,6 @@ declare const sandboxTraceback: (() => void) | undefined;
  *
  * @param levels The amount of levels to look backwards in the call stack.
  */
-declare const getParentFunctionDescription: ((levels: int) => void) | undefined;
+declare const getParentFunctionDescription:
+  | ((this: void, levels: int) => void)
+  | undefined;
