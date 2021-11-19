5e07b8637ba2f192603e69e2c56e63f8b315fc65
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-19 14:55:13 -0500

    adding getTrinkets helper function

--- src/functions/trinkets.ts
+++ src/functions/trinkets.ts
@@ -105,6 +105,25 @@ export function getMaxTrinketID(): int {
   return itemConfig.GetTrinkets().Size - 1;
 }
 
+/** Helper function to get all of the collectible entities in the room. */
+export function getTrinkets(matchingSubType = -1): EntityPickup[] {
+  const entities = Isaac.FindByType(
+    EntityType.ENTITY_PICKUP,
+    PickupVariant.PICKUP_TRINKET,
+    matchingSubType,
+  );
+
+  const collectibles: EntityPickup[] = [];
+  for (const entity of entities) {
+    const pickup = entity.ToPickup();
+    if (pickup !== undefined) {
+      collectibles.push(pickup);
+    }
+  }
+
+  return collectibles;
+}
+
 /**
  * This is a helper function to get a trinket name from a TrinketType.
  *
