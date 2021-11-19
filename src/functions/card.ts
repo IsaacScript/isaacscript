963b468c9f5226bfb87c1ffcf2885153e18780d9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-19 03:27:56 -0500

    adding getRandomRune helper function

--- src/functions/card.ts
+++ src/functions/card.ts
@@ -1,8 +1,22 @@
-import { getRandomInt } from "./random";
+import { getRandomInt, nextSeed } from "./random";
 
 /** Has an equal chance of returning any value between 1 and `Card.NUM_CARDS - 1`. */
 export function getRandomCard(seed = Random()): Card {
   return getRandomInt(1, Card.NUM_CARDS - 1, seed);
+}
+
+/**
+ * Has an equal chance of returning any rune. (e.g. Rune of Hagalaz, Blank Rune, Black Rune, Rune
+ * Shard, Soul of Isaac, etc.)
+ */
+export function getRandomRune(seed = Random()): Card {
+  let card: Card;
+  do {
+    seed = nextSeed(seed);
+    card = getRandomCard(seed);
+  } while (!isRune(card));
+
+  return card;
 }
 
 /** Returns true for entries in the Card enum that are not a rune or an object. */
@@ -10,7 +24,9 @@ export function isCard(card: Card): boolean {
   return isNotCardOrRune(card) && !isRune(card);
 }
 
-/** Returns true for entries in the Card enum that are not a card or a rune. (e.g. Dice Shard) */
+/**
+ * Returns true for entries in the Card enum that are not a card and not a rune (like Dice Shard).
+ */
 export function isNotCardOrRune(card: Card): boolean {
   return (
     card === Card.CARD_DICE_SHARD ||
@@ -21,7 +37,7 @@ export function isNotCardOrRune(card: Card): boolean {
 
 /**
  * Returns true for entries in the Card enum that are a rune. (e.g. Rune of Hagalaz, Blank Rune,
- * Black Rune, Rune Shard, Soul of Isaac)
+ * Black Rune, Rune Shard, Soul of Isaac, etc.)
  */
 export function isRune(card: Card): boolean {
   return (
