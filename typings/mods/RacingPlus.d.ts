88849798e74921b63fd49cc6289d953f11ee577f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-19 00:20:18 -0500

    better comments for racing+ functions

--- typings/mods/RacingPlus.d.ts
+++ typings/mods/RacingPlus.d.ts
@@ -1,5 +1,11 @@
-// These are global functions provided by the Racing+ client sandbox
-// If the sandbox is not present, they will be undefined
-
+/**
+ * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
+ * it will be undefined.
+ */
 declare const traceback: (() => void) | undefined;
+
+/**
+ * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
+ * it will be undefined.
+ */
 declare const getParentFunctionDescription: (() => void) | undefined;
