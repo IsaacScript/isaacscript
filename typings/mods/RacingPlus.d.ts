c9d7101c10d8694a09aebe43f3a9ee1ede3d2177
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-19 00:22:59 -0500

    bugfix for getParentFunctionDescription

--- typings/mods/RacingPlus.d.ts
+++ typings/mods/RacingPlus.d.ts
@@ -7,5 +7,7 @@ declare const traceback: (() => void) | undefined;
 /**
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
  * it will be undefined.
+ *
+ * @param levels The amount of levels to look backwards in the call stack.
  */
-declare const getParentFunctionDescription: (() => void) | undefined;
+declare const getParentFunctionDescription: ((levels: int) => void) | undefined;
