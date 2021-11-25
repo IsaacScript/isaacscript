3eb2b5cc9840fa88caacccf67362ef76af3eed75
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-25 01:54:45 -0500

    documentation for spawnCollectible helper function

--- src/functions/spawnCollectible.ts
+++ src/functions/spawnCollectible.ts
@@ -3,6 +3,19 @@ import { areFeaturesInitialized } from "../initialized";
 import { isQuestCollectible } from "./collectibles";
 import { anyPlayerIs } from "./player";
 
+/**
+ * Helper function to spawn a collectible. Use this instead of `Game.Spawn()` because it handles the
+ * cases of Tainted Keeper collectibles costing coins and preventing quest items from being rotated
+ * by Tainted Isaac's rotation mechanic.
+ *
+ * @param collectibleType The collectible type to spawn.
+ * @param position The position to spawn the collectible at.
+ * @param seed Optional. Random() by default.
+ * @param options Optional. Set to true to make the collectible a "There's Options" style
+ * collectible. False by default.
+ * @param forceFreeItem Optional. Set to true to disable the logic that gives the item a price for
+ * Tainted Keeper. False by default.
+ */
 export function spawnCollectible(
   collectibleType: CollectibleType | int,
   position: Vector,
