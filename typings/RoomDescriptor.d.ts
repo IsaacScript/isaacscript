ea65caedc31148b2c6508f12d61380ed6dd5217b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-11-29 10:14:58 -0500

    better documentation for SafeGridIndex

--- typings/RoomDescriptor.d.ts
+++ typings/RoomDescriptor.d.ts
@@ -27,7 +27,16 @@ declare interface RoomDescriptor {
   PoopCount: int;
   PressurePlatesTriggered: boolean;
   SacrificeDone: boolean;
+
+  /**
+   * `GridIndex` is equal to the the specific 1x1 quadrant that the player entered the room at.
+   * Thus, it will return different values for the same big room. `SafeGridIndex` is always equal to
+   * the the top-left index of the room. Thus, it will always return consistent values. Use
+   * `SafeGridIndex` over `GridIndex` as the key for data structures that use the room index as an
+   * index.
+   */
   SafeGridIndex: int;
+
   ShopItemDiscountIdx: int;
   ShopItemIdx: int;
   SpawnSeed: int;
--- typings/RoomDescriptorReadOnly.d.ts
+++ typings/RoomDescriptorReadOnly.d.ts
@@ -117,6 +117,12 @@ declare interface RoomDescriptorReadOnly {
   SacrificeDone: Readonly<boolean>;
 
   /**
+   * `GridIndex` is equal to the the specific 1x1 quadrant that the player entered the room at.
+   * Thus, it will return different values for the same big room. `SafeGridIndex` is always equal to
+   * the the top-left index of the room. Thus, it will always return consistent values. Use
+   * `SafeGridIndex` over `GridIndex` as the key for data structures that use the room index as an
+   * index.
+   *
    * To set this property, get a writable copy of the RoomDescriptor by using the
    * `Level.GetRoomByIdx()` method.
    */
