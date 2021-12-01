2f736634b2ffab3092327ac1fa003b19b7385d1b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-01 16:19:33 -0500

    updating Racing+ definitions

--- typings/mods/RacingPlus.d.ts
+++ typings/mods/RacingPlus.d.ts
@@ -2,6 +2,12 @@
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
  * it will be undefined.
  */
+declare const sandboxGetTraceback: ((this: void) => string) | undefined;
+
+/**
+ * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
+ * it will be undefined.
+ */
 declare const sandboxTraceback: ((this: void) => void) | undefined;
 
 /**
