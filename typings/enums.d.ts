e61b281ce472007fa6a0d92bddfdf9d389b6a36b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-10 15:31:40 -0500

    adding comments for PlayerForm enum

--- typings/enums.d.ts
+++ typings/enums.d.ts
@@ -923,19 +923,33 @@ declare enum LevelCurse {
 }
 
 declare enum PlayerForm {
+  /** Guppy */
   PLAYERFORM_GUPPY = 0,
+  /** Beelzebub */
   PLAYERFORM_LORD_OF_THE_FLIES = 1,
+  /** Fun Guy */
   PLAYERFORM_MUSHROOM = 2,
+  /** Seraphim */
   PLAYERFORM_ANGEL = 3,
+  /** Bob */
   PLAYERFORM_BOB = 4,
+  /** Spun */
   PLAYERFORM_DRUGS = 5,
+  /** Yes Mother? */
   PLAYERFORM_MOM = 6,
+  /** Conjoined */
   PLAYERFORM_BABY = 7,
+  /** Leviathan */
   PLAYERFORM_EVIL_ANGEL = 8,
+  /** Oh Crap */
   PLAYERFORM_POOP = 9,
+  /** Bookworm */
   PLAYERFORM_BOOK_WORM = 10,
+  /** Adult */
   PLAYERFORM_ADULTHOOD = 11,
+  /** Spider Baby */
   PLAYERFORM_SPIDERBABY = 12,
+  /** Stompy */
   PLAYERFORM_STOMPY = 13,
   /** This transformation is unused by the game. */
   PLAYERFORM_FLIGHT = 14,
