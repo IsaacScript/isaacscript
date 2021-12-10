013610aef301065e5919a9d1764ae9d9b86f05ce
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-10 15:35:29 -0500

    upgrading prettier

--- lint.sh
+++ lint.sh
@@ -15,12 +15,15 @@ fi
 
 cd "$DIR"
 
-# Step 1 - Use ESLint to lint the TypeScript
+# Step 1 - Use Prettier to check formatting
+npx prettier --check "src/**/*.ts"
+
+# Step 2 - Use ESLint to lint the TypeScript
 # Since all ESLint errors are set to warnings,
 # we set max warnings to 0 so that warnings will fail in CI
 npx eslint --max-warnings 0 typings
 
-# Step 2 - Spell check every file using cspell
+# Step 3 - Spell check every file using cspell
 # We use no-progress and no-summary because we want to only output errors
 npx cspell --no-progress --no-summary "typings/**/*.ts"
 npx cspell --no-progress --no-summary "scripts/**/*.py"
