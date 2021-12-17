37941d23678100974f3049beaa06f6307afe963d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-17 16:31:14 -0500

    adding comments for RoomType enum

--- typings/enums.d.ts
+++ typings/enums.d.ts
@@ -833,11 +833,16 @@ declare enum RoomType {
   ROOM_SUPERSECRET = 8,
   ROOM_ARCADE = 9,
   ROOM_CURSE = 10,
+
+  /** This includes both normal Challenge Rooms and Boss Challenge Rooms. */
   ROOM_CHALLENGE = 11,
+
   ROOM_LIBRARY = 12,
   ROOM_SACRIFICE = 13,
   ROOM_DEVIL = 14,
   ROOM_ANGEL = 15,
+
+  /** This is the room type of a crawlspace. */
   ROOM_DUNGEON = 16,
   ROOM_BOSSRUSH = 17,
   ROOM_ISAACS = 18,
@@ -849,10 +854,22 @@ declare enum RoomType {
 
   // Repentance
   ROOM_PLANETARIUM = 24,
+
+  /** The Mausoleum teleporter entrance. Currently unused. */
   ROOM_TELEPORTER = 25,
+
+  /** The Mausoleum teleporter exit. Currently unused. */
   ROOM_TELEPORTER_EXIT = 26,
+
+  /**
+   * The room with a trapdoor to the Repentance floors. This also includes the "strange room" on
+   * Depths 2.
+   */
   ROOM_SECRET_EXIT = 27,
+
+  /** The Blue Womb rooms spawned by the Blue Key. */
   ROOM_BLUE = 28,
+
   ROOM_ULTRASECRET = 29,
 
   NUM_ROOMTYPES = 30,
