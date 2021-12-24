d472056cc9868ccd4731a9f2fe62cf2d31c438cf
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-24 09:36:04 -0500

    updating deps

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ cd "$DIR"
 
 PACKAGE_JSON="$DIR/package.json"
 OLD_HASH=$(md5sum "$PACKAGE_JSON")
-npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON" --reject typescript,typescript-to-lua
+npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON"
 NEW_HASH=$(md5sum "$PACKAGE_JSON")
 if [[ $OLD_HASH != $NEW_HASH ]]; then
   npm install
