ae54912ce1c7f549865245d25e92a3e8689849ae
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-23 23:28:01 -0500

    adding function to sandbox definition

--- typings/mods/Sandbox.d.ts
+++ typings/mods/Sandbox.d.ts
@@ -4,6 +4,7 @@
 declare interface Sandbox {
   connect(hostname: string, port: int, useTCP: boolean): SocketClient;
   connectLocalhost(port: int, useTCP: boolean): SocketClient;
+  getDate(format?: string): string;
   getParentFunctionDescription(levels: int): string;
   getTraceback(): string;
   init(): void;
