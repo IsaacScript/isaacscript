beda1ec5571080b0f7bde25496fb0609656339e7
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-23 22:56:40 -0500

    rename 2/2

---
+++ typings/mods/Sandbox.d.ts
@@ -0,0 +1,12 @@
+// This is the Racing+ sandbox provided by the Electron client
+
+/** @noSelf */
+declare interface Sandbox {
+  connect(hostname: string, port: int, useTCP: boolean): SocketClient;
+  connectLocalhost(port: int, useTCP: boolean): SocketClient;
+  getParentFunctionDescription(levels: int): string;
+  getTraceback(): string;
+  init(): void;
+  isSocketInitialized(): boolean;
+  traceback(): void;
+}
