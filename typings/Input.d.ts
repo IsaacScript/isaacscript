c6f60bd152f10cb2539b50f123753c0be6fa2014
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-02-16 11:14:54 -0500

    updating parameter names

--- typings/Input.d.ts
+++ typings/Input.d.ts
@@ -1,11 +1,20 @@
 /** @noSelf */
 declare namespace Input {
-  function GetActionValue(action: ButtonAction, controllerID: int): float;
-  function GetButtonValue(button: Keyboard, controllerID: int): float;
+  function GetActionValue(buttonAction: ButtonAction, controllerID: int): float;
+  function GetButtonValue(keyboard: Keyboard, controllerID: int): float;
   function GetMousePosition(gameCoords: boolean): Vector;
-  function IsActionPressed(action: ButtonAction, controllerID: int): boolean;
-  function IsActionTriggered(action: ButtonAction, controllerID: int): boolean;
-  function IsButtonPressed(button: Keyboard, controllerID: int): boolean;
-  function IsButtonTriggered(button: Keyboard, controllerID: int): boolean;
-  function IsMouseBtnPressed(button: Mouse): boolean;
+
+  function IsActionPressed(
+    buttonAction: ButtonAction,
+    controllerID: int,
+  ): boolean;
+
+  function IsActionTriggered(
+    buttonAction: ButtonAction,
+    controllerID: int,
+  ): boolean;
+
+  function IsButtonPressed(keyboard: Keyboard, controllerID: int): boolean;
+  function IsButtonTriggered(keyboard: Keyboard, controllerID: int): boolean;
+  function IsMouseBtnPressed(mouse: Mouse): boolean;
 }
