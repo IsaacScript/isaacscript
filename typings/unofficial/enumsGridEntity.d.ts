af77fda2cf12c7191532937cda09b9ffd093406e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-02-26 05:42:41 -0500

    better commenting

--- typings/unofficial/enumsGridEntity.d.ts
+++ typings/unofficial/enumsGridEntity.d.ts
@@ -6,22 +6,28 @@
 declare const enum GridEntityXMLType {
   ROCK = 1000,
   ROCK_BOMB = 1001,
+
   /** A pot, mushroom, or skull, depending on the stage and room type. */
   ROCK_ALT = 1002,
+
   /** A tinted rock. */
   ROCKT = 1003,
+
   /** A marked skull that will drop a Fool card. */
   ROCK_ALT2 = 1008,
+
   /**
    * Rocks that will be destroyed when other nearby rocks are. In-game, it will be a
    * GridEntityType.GRID_ROCK with a VarData of 1.
    */
   ROCK_EVENT = 1009,
+
   ROCK_SPIKED = 1010,
   ROCK_GOLD = 1011,
   TNT = 1300,
   FIREPLACE = 1400,
   RED_FIREPLACE = 1410,
+
   POOP_RED = 1490,
   POOP_RAINBOW = 1494,
   POOP_CORN = 1495,
@@ -31,7 +37,10 @@ declare const enum GridEntityXMLType {
   POOP_GIGA = 1499,
   POOP = 1500,
   POOP_CHARMING = 1501,
+
+  /** A block. */
   ROCKB = 1900,
+
   PILLAR = 1901,
   SPIKES = 1930,
   SPIKES_ONOFF = 1931,
