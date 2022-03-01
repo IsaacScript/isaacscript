149cd911fde88fd91565f2f9be8d388b79ed2a2d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-01 17:14:57 -0500

    moving pickup functions

--- src/functions/entity.ts
+++ src/functions/entity.ts
@@ -108,38 +108,6 @@ export function getEntities(
 }
 
 /**
- * Helper function to get all of the pickups in the room.
- *
- * Example:
- * ```
- * // Make all of the pickups in the room invisible
- * for (const pickup of getPickups()) {
- *   pickup.Visible = false;
- * }
- * ```
- */
-export function getPickups(
-  matchingVariant: PickupVariant | int = -1,
-  matchingSubType = -1,
-): EntityPickup[] {
-  const entities = getEntities(
-    EntityType.ENTITY_PICKUP,
-    matchingVariant,
-    matchingSubType,
-  );
-
-  const pickups: EntityPickup[] = [];
-  for (const entity of entities) {
-    const pickup = entity.ToPickup();
-    if (pickup !== undefined) {
-      pickups.push(pickup);
-    }
-  }
-
-  return pickups;
-}
-
-/**
  * Helper function to measure an entity's velocity to see if it is moving.
  *
  * @param entity The entity whose velocity to measure.
@@ -202,23 +170,6 @@ export function removeAllMatchingEntities(
 ): boolean {
   const entities = getEntities(entityType, entityVariant, entitySubType);
   return removeEntities(entities, cap);
-}
-
-/**
- * Helper function to remove all of the pickups in the room.
- *
- * @param variant Optional. If specified, will only remove pickups that match this variant.
- * @param subType Optional. If specified, will only remove pickups that match this sub-type.
- * @param cap Optional. If specified, will only remove the given amount of pickups.
- * @returns True if one or more pickups was removed, false otherwise.
- */
-export function removeAllPickups(
-  variant?: PickupVariant | int,
-  subType?: int,
-  cap?: int,
-): boolean {
-  const pickups = getPickups(variant, subType);
-  return removeEntities(pickups, cap);
 }
 
 /**
--- src/functions/pickups.ts
+++ src/functions/pickups.ts
@@ -1,6 +1,39 @@
 import { CHEST_PICKUP_VARIANTS } from "../constants";
 import { getRandomArrayElement } from "./array";
+import { getEntities, removeEntities } from "./entity";
 import { getEnumValues } from "./utils";
+
+/**
+ * Helper function to get all of the pickups in the room.
+ *
+ * Example:
+ * ```
+ * // Make all of the pickups in the room invisible
+ * for (const pickup of getPickups()) {
+ *   pickup.Visible = false;
+ * }
+ * ```
+ */
+export function getPickups(
+  matchingVariant: PickupVariant | int = -1,
+  matchingSubType = -1,
+): EntityPickup[] {
+  const entities = getEntities(
+    EntityType.ENTITY_PICKUP,
+    matchingVariant,
+    matchingSubType,
+  );
+
+  const pickups: EntityPickup[] = [];
+  for (const entity of entities) {
+    const pickup = entity.ToPickup();
+    if (pickup !== undefined) {
+      pickups.push(pickup);
+    }
+  }
+
+  return pickups;
+}
 
 /**
  * Has as an equal chance of returning any value in the `HeartSubType` enum. Useful for spawning a random type of heart.
@@ -18,4 +51,21 @@ export function getRandomHeartSubType(
 
 export function isChest(pickup: EntityPickup): boolean {
   return CHEST_PICKUP_VARIANTS.has(pickup.Variant);
+}
+
+/**
+ * Helper function to remove all of the pickups in the room.
+ *
+ * @param variant Optional. If specified, will only remove pickups that match this variant.
+ * @param subType Optional. If specified, will only remove pickups that match this sub-type.
+ * @param cap Optional. If specified, will only remove the given amount of pickups.
+ * @returns True if one or more pickups was removed, false otherwise.
+ */
+export function removeAllPickups(
+  variant?: PickupVariant | int,
+  subType?: int,
+  cap?: int,
+): boolean {
+  const pickups = getPickups(variant, subType);
+  return removeEntities(pickups, cap);
 }
