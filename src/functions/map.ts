fd76a9c1e335f192620a49f41f6c05a9752221f4
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-09 18:30:03 -0500

    adding mapGetNextSeed helper function

--- src/functions/map.ts
+++ src/functions/map.ts
@@ -1,3 +1,5 @@
+import { nextSeed } from "./random";
+
 /** Helper function to copy a map. (You can also use a Map constructor to accomplish this task.) */
 export function copyMap<K, V>(oldMap: Map<K, V>): Map<K, V> {
   const newMap = new Map<K, V>();
@@ -6,4 +8,24 @@ export function copyMap<K, V>(oldMap: Map<K, V>): Map<K, V> {
   }
 
   return newMap;
+}
+
+/**
+ * Helper function to make using map with `Seed` values easier. Use this instead of manually getting
+ * the current value, incrementing it, and then re-setting it.
+ */
+export function mapGetNextSeed<K>(map: Map<K, Seed>, key: K): Seed | undefined {
+  const seed = map.get(key);
+  if (seed === undefined) {
+    return undefined;
+  }
+
+  if (seed === 0) {
+    error(`The seed map had a value of 0 for the key of: ${key}`);
+  }
+
+  const newSeed = nextSeed(seed);
+  map.set(key, newSeed);
+
+  return newSeed;
 }
