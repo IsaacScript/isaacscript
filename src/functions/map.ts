4f7ba0b36659e658d5fa2d547943eacf64d11961
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-09 18:34:05 -0500

    adding defaultMapGetNextSeed helper function

--- src/functions/map.ts
+++ src/functions/map.ts
@@ -1,3 +1,4 @@
+import { DefaultMap } from "../types/DefaultMap";
 import { nextSeed } from "./random";
 
 /** Helper function to copy a map. (You can also use a Map constructor to accomplish this task.) */
@@ -11,8 +12,29 @@ export function copyMap<K, V>(oldMap: Map<K, V>): Map<K, V> {
 }
 
 /**
- * Helper function to make using map with `Seed` values easier. Use this instead of manually getting
- * the current value, incrementing it, and then re-setting it.
+ * Helper function to make using default maps with `Seed` values easier. Use this instead of
+ * manually getting the current value, incrementing it, and then setting it.
+ */
+export function defaultMapGetNextSeed<K, A extends unknown[]>(
+  map: DefaultMap<K, Seed, A>,
+  key: K,
+  ...extraArgs: A
+): Seed {
+  const seed = map.getAndSetDefault(key, ...extraArgs);
+
+  if (seed === 0) {
+    error(`The seed map had a value of 0 for the key of: ${key}`);
+  }
+
+  const newSeed = nextSeed(seed);
+  map.set(key, newSeed);
+
+  return newSeed;
+}
+
+/**
+ * Helper function to make using maps with `Seed` values easier. Use this instead of manually
+ * getting the current value, incrementing it, and then setting it.
  */
 export function mapGetNextSeed<K>(map: Map<K, Seed>, key: K): Seed | undefined {
   const seed = map.get(key);
