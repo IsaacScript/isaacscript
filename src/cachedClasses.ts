fc576ba0494f8ba19cb77a2daab5fa7ee69fdc5f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-09 19:33:12 -0500

    adding cached classes

---
+++ src/cachedClasses.ts
@@ -0,0 +1,39 @@
+/**
+ * A cached version of the `Game()` constructor.
+ *
+ * Use this instead of invoking the constructor again for a miniscule performance increase.
+ *
+ * Caching the results of a this constructor is safe, but caching other classes (like `Level` or
+ * `Room`) is not safe and can lead to the game crashing in certain situations.
+ */
+export const game = Game();
+
+/**
+ * A cached version of the `Isaac.GetItemConfig()()` constructor.
+ *
+ * Use this instead of invoking the constructor again for a miniscule performance increase.
+ *
+ * Caching the results of a this constructor is safe, but caching other classes (like `Level` or
+ * `Room`) is not safe and can lead to the game crashing in certain situations.
+ */
+export const itemConfig = Isaac.GetItemConfig();
+
+/**
+ * A cached version of the `MusicManager()` constructor.
+ *
+ * Use this instead of invoking the constructor again for a miniscule performance increase.
+ *
+ * Caching the results of a this constructor is safe, but caching other classes (like `Level` or
+ * `Room`) is not safe and can lead to the game crashing in certain situations.
+ */
+export const musicManager = MusicManager();
+
+/**
+ * A cached version of the `SFXManager()` constructor.
+ *
+ * Use this instead of invoking the constructor again for a miniscule performance increase.
+ *
+ * Caching the results of a this constructor is safe, but caching other classes (like `Level` or
+ * `Room`) is not safe and can lead to the game crashing in certain situations.
+ */
+export const sfxManager = SFXManager();
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export * from "./cachedClasses";
 export {
   forceNewLevelCallback,
   forceNewRoomCallback,
