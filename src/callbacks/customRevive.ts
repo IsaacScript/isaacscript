4b47352150e30da7733b73489e020483bc0f36b3
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-15 02:33:58 -0400

    bugfix for custom revive

--- src/callbacks/customRevive.ts
+++ src/callbacks/customRevive.ts
@@ -3,6 +3,7 @@ import { runNextGameFrame } from "../features/runInNFrames";
 import { saveDataManager } from "../features/saveDataManager/exports";
 import { removeCollectibleFromItemTracker } from "../functions/collectibles";
 import { removeAllFamiliars } from "../functions/familiars";
+import { log } from "../functions/log";
 import { getPlayerFromIndex, getPlayerIndex } from "../functions/player";
 import { ModCallbacksCustom } from "../types/ModCallbacksCustom";
 import { ModUpgraded } from "../types/ModUpgraded";
@@ -16,8 +17,17 @@ import {
   preCustomReviveHasSubscriptions,
 } from "./subscriptions/preCustomRevive";
 
+const DEBUG = false;
+
 enum CustomReviveState {
   DISABLED,
+
+  /**
+   * We can't immediately jump to waiting for an item animation because it is possible for a player
+   * to be holding an item above their head as they are dying (e.g. with Razor Blade).
+   */
+  WAITING_FOR_ROOM_TRANSITION,
+
   WAITING_FOR_ITEM_ANIMATION,
 }
 
@@ -34,6 +44,7 @@ export function customReviveCallbacksInit(mod: ModUpgraded): void {
   saveDataManager("customRevive", v, hasSubscriptions);
 
   mod.AddCallback(ModCallbacks.MC_POST_RENDER, postRender); // 2
+  mod.AddCallback(ModCallbacks.MC_POST_NEW_ROOM, postNewRoom); // 19
   mod.AddCallbackCustom(
     ModCallbacksCustom.MC_POST_PEFFECT_UPDATE_REORDERED,
     postPEffectUpdateReordered,
@@ -54,7 +65,7 @@ function hasSubscriptions() {
   );
 }
 
-// ModCallbacks.MC_POST_RENDER
+// ModCallbacks.MC_POST_RENDER (2)
 function postRender() {
   if (v.run.state !== CustomReviveState.WAITING_FOR_ITEM_ANIMATION) {
     return;
@@ -63,6 +74,16 @@ function postRender() {
   // The 1-up sound will fire before the item holding animation begins,
   // so we mute it on every render frame
   sfxManager.Stop(SoundEffect.SOUND_1UP);
+}
+
+// ModCallbacks.MC_POST_NEW_ROOM (19)
+function postNewRoom() {
+  if (v.run.state !== CustomReviveState.WAITING_FOR_ROOM_TRANSITION) {
+    return;
+  }
+
+  v.run.state = CustomReviveState.WAITING_FOR_ITEM_ANIMATION;
+  logStateChanged();
 }
 
 // ModCallbacksCustom.MC_POST_PEFFECT_UPDATE_REORDERED
@@ -110,6 +131,7 @@ function checkWaitingForItemAnimation(player: EntityPlayer) {
   v.run.state = CustomReviveState.DISABLED;
   v.run.revivalType = null;
   v.run.dyingPlayerIndex = null;
+  logStateChanged();
 }
 
 // ModCallbacksCustom.MC_POST_PLAYER_FATAL_DAMAGE
@@ -140,9 +162,10 @@ function playerIsAboutToDie(player: EntityPlayer) {
     return;
   }
 
-  v.run.state = CustomReviveState.WAITING_FOR_ITEM_ANIMATION;
+  v.run.state = CustomReviveState.WAITING_FOR_ROOM_TRANSITION;
   v.run.revivalType = revivalType;
   v.run.dyingPlayerIndex = getPlayerIndex(player);
+  logStateChanged();
 
   player.AddCollectible(CollectibleType.COLLECTIBLE_1UP, 0, false);
   removeAllFamiliars(FamiliarVariant.ONE_UP);
@@ -179,4 +202,14 @@ function playerIsAboutToDie(player: EntityPlayer) {
     // The familiar will show up again for some reason, so remove it again
     removeAllFamiliars(FamiliarVariant.ONE_UP);
   });
+}
+
+function logStateChanged() {
+  if (DEBUG) {
+    log(
+      `Custom revive state changed: ${CustomReviveState[v.run.state]} (${
+        v.run.state
+      })`,
+    );
+  }
 }
