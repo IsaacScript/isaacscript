e4955795aac1e12944473ea1f2796fe271845231
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-17 12:27:33 -0400

    cleanup dead comment

--- src/constants.ts
+++ src/constants.ts
@@ -15,15 +15,6 @@ export const MOD_UPLOADER_PATH =
   "C:\\Program Files (x86)\\Steam\\steamapps\\common\\The Binding of Isaac Rebirth\\tools\\ModUploader\\ModUploader.exe";
 export const PROJECT_NAME = "IsaacScript";
 
-/**
- * The default Node.js stack size is 984 kilobytes, but this is not large enough to compile the
- * biggest mods, resulting in an error of "RangeError: Maximum call stack size exceeded". Thus,
- * IsaacScript provides an option to specify a custom stack size that is bigger than the default.
- * We choose 1968 because it is double the default.
- */
-// export const CUSTOM_NODE_JS_STACK_SIZE = 1968;
-// (commented out temporarily)
-
 // isaacscript
 const REPO_ROOT = path.join(__dirname, "..", "..");
 
