205bca7cc14278d6cdde50633e6fb7b30027244b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-18 11:56:29 -0400

    fix for linter

--- src/plugins/addCrashDebugStatements.ts
+++ src/plugins/addCrashDebugStatements.ts
@@ -1,7 +1,7 @@
-import * as tstl from "typescript-to-lua";
 import * as crypto from "crypto";
 import { SourceNode } from "source-map";
 import * as ts from "typescript";
+import * as tstl from "typescript-to-lua";
 
 class CustomPrinter extends tstl.LuaPrinter {
   printStatement(statement: tstl.Statement): SourceNode {
