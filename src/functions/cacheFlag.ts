2ffff8bd3e1bc2aa5ebf8447754115d61e4ae6d5
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-21 00:41:22 -0400

    documentation for getDefaultPlayerStat

--- src/functions/cacheFlag.ts
+++ src/functions/cacheFlag.ts
@@ -1,5 +1,10 @@
 import { DEFAULT_PLAYER_STAT_MAP } from "../maps/defaultPlayerStatMap";
 
+/**
+ * Returns the starting stat that Isaac (the default character) starts with.
+ *
+ * For example, if you pass this function `CacheFlag.CACHE_DAMAGE`, it will return 3.5.
+ */
 export function getDefaultPlayerStat(
   cacheFlag: CacheFlag,
 ): float | boolean | undefined {
