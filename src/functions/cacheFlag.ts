7f04b8b30ddf41b8813a89a654a8d8464b7978a1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-21 12:39:13 -0400

    removing flight from the default stat map to simplify the types

--- src/functions/cacheFlag.ts
+++ src/functions/cacheFlag.ts
@@ -5,8 +5,6 @@ import { DEFAULT_PLAYER_STAT_MAP } from "../maps/defaultPlayerStatMap";
  *
  * For example, if you pass this function `CacheFlag.CACHE_DAMAGE`, it will return 3.5.
  */
-export function getDefaultPlayerStat(
-  cacheFlag: CacheFlag,
-): float | boolean | undefined {
+export function getDefaultPlayerStat(cacheFlag: CacheFlag): number | undefined {
   return DEFAULT_PLAYER_STAT_MAP.get(cacheFlag);
 }
--- src/maps/defaultPlayerStatMap.ts
+++ src/maps/defaultPlayerStatMap.ts
@@ -1,10 +1,8 @@
-export const DEFAULT_PLAYER_STAT_MAP: Map<CacheFlag, number | boolean> =
-  new Map<CacheFlag, number | boolean>([
-    [CacheFlag.CACHE_DAMAGE, 3.5], // 1 << 0
-    [CacheFlag.CACHE_FIREDELAY, 10], // 1 << 1
-    [CacheFlag.CACHE_SHOTSPEED, 1.0], // 1 << 2
-    [CacheFlag.CACHE_RANGE, 6.5], // 1 << 3
-    [CacheFlag.CACHE_SPEED, 1], // 1 << 4
-    [CacheFlag.CACHE_FLYING, false], // 1 << 7
-    [CacheFlag.CACHE_LUCK, 0], // 1 << 10
-  ]);
+export const DEFAULT_PLAYER_STAT_MAP: ReadonlyMap<CacheFlag, number> = new Map([
+  [CacheFlag.CACHE_DAMAGE, 3.5], // 1 << 0
+  [CacheFlag.CACHE_FIREDELAY, 10], // 1 << 1
+  [CacheFlag.CACHE_SHOTSPEED, 1.0], // 1 << 2
+  [CacheFlag.CACHE_RANGE, 6.5], // 1 << 3
+  [CacheFlag.CACHE_SPEED, 1], // 1 << 4
+  [CacheFlag.CACHE_LUCK, 0], // 1 << 10
+]);
