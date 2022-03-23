f724c20dd19a74199cb3f3f91193a3b112bf5c7b
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-23 16:42:14 -0400

    better docs for RNG

--- typings/RNG.d.ts
+++ typings/RNG.d.ts
@@ -5,18 +5,22 @@ declare interface RNG {
   Next(): Seed;
 
   /**
-   * Generates a random float between 0 and 1. It is inclusive on the lower end and exclusive on the
+   * Returns a random float between 0 and 1. It is inclusive on the lower end and exclusive on the
    * higher end.
+   *
+   * Note that this will automatically call the `RNG.Next` method before retrieving the random
+   * number. Since this mutates the RNG object, you should use this method with care.
    */
   RandomFloat(): float;
 
   /**
-   * Generates a random integer between 0 and max. It is inclusive on the lower end and exclusive on
+   * Returns a random integer between 0 and max. It is inclusive on the lower end and exclusive on
    * the higher end.
    *
    * For example, `rng.RandomInt(4)` will return either 0, 1, 2, or 3.
    *
-   * @param max
+   * Note that this will automatically call the `RNG.Next` method before retrieving the random
+   * number. Since this mutates the RNG object, you should use this method with care.
    */
   RandomInt(max: int): int;
 
