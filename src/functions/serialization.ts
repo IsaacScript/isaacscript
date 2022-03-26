b0f720f332a093f1437c9df243ba40592873e12e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2022-03-26 11:44:47 -0400

    refactoring/optimizing the new serialization code

--- src/functions/serialization.ts
+++ src/functions/serialization.ts
@@ -1,13 +1,10 @@
 import { SerializableIsaacAPIClassType } from "../enums/private/SerializableIsaacAPIClassType";
 import { SerializationType } from "../enums/SerializationType";
+import { SERIALIZABLE_ISAAC_API_CLASS_TYPE_TO_COPY_FUNCTION } from "../objects/serializableIsaacAPIClassTypeToCopyFunction";
+import { SERIALIZABLE_ISAAC_API_CLASS_TYPE_TO_IDENTITY_FUNCTION } from "../objects/serializableIsaacAPIClassTypeToIdentityFunction";
 import { SerializableIsaacAPIClass } from "../types/private/SerializableIsaacAPIClass";
 import { SerializedIsaacAPIClass } from "../types/private/SerializedIsaacAPIClass";
-import { copyColor, isSerializedColor } from "./color";
 import { getIsaacAPIClassType } from "./isaacAPIClass";
-import { copyKColor, isSerializedKColor } from "./kColor";
-import { copyRNG, isSerializedRNG } from "./rng";
-import { ensureAllCases, getEnumValues } from "./utils";
-import { copyVector, isSerializedVector } from "./vector";
 
 export function copySerializableIsaacAPIClass(
   isaacAPIClassOrSerializedTable:
@@ -19,31 +16,15 @@ export function copySerializableIsaacAPIClass(
     isaacAPIClassOrSerializedTable,
   ) as SerializableIsaacAPIClassType;
 
-  switch (isaacAPIClassType) {
-    case SerializableIsaacAPIClassType.COLOR: {
-      const color = isaacAPIClassOrSerializedTable as unknown as Color;
-      return copyColor(color, serializationType);
-    }
-
-    case SerializableIsaacAPIClassType.KCOLOR: {
-      const kColor = isaacAPIClassOrSerializedTable as unknown as KColor;
-      return copyKColor(kColor, serializationType);
-    }
-
-    case SerializableIsaacAPIClassType.RNG: {
-      const rng = isaacAPIClassOrSerializedTable as unknown as RNG;
-      return copyRNG(rng);
-    }
-
-    case SerializableIsaacAPIClassType.VECTOR: {
-      const vector = isaacAPIClassOrSerializedTable as unknown as Vector;
-      return copyVector(vector);
-    }
-
-    default: {
-      return ensureAllCases(isaacAPIClassType);
-    }
+  const copyFunction =
+    SERIALIZABLE_ISAAC_API_CLASS_TYPE_TO_COPY_FUNCTION[isaacAPIClassType];
+  if (copyFunction === undefined) {
+    error(
+      `Failed to copy Isaac API class "${isaacAPIClassType}" since there is not a defined copy function for this class type.`,
+    );
   }
+
+  return copyFunction(isaacAPIClassOrSerializedTable, serializationType);
 }
 
 export function isSerializedIsaacAPIClass(
@@ -54,48 +35,8 @@ export function isSerializedIsaacAPIClass(
     return false;
   }
 
-  const serializableIsaacAPIClassTypes = getEnumValues(
-    SerializableIsaacAPIClassType,
+  const identityFunctions = Object.values(
+    SERIALIZABLE_ISAAC_API_CLASS_TYPE_TO_IDENTITY_FUNCTION,
   );
-  for (const serializableIsaacAPIClassType of serializableIsaacAPIClassTypes) {
-    switch (serializableIsaacAPIClassType) {
-      case SerializableIsaacAPIClassType.COLOR: {
-        if (isSerializedColor(object)) {
-          return true;
-        }
-
-        break;
-      }
-
-      case SerializableIsaacAPIClassType.KCOLOR: {
-        if (isSerializedKColor(object)) {
-          return true;
-        }
-
-        break;
-      }
-
-      case SerializableIsaacAPIClassType.RNG: {
-        if (isSerializedRNG(object)) {
-          return true;
-        }
-
-        break;
-      }
-
-      case SerializableIsaacAPIClassType.VECTOR: {
-        if (isSerializedVector(object)) {
-          return true;
-        }
-
-        break;
-      }
-
-      default: {
-        ensureAllCases(serializableIsaacAPIClassType);
-      }
-    }
-  }
-
-  return false;
+  return identityFunctions.some((identityFunction) => identityFunction(object));
 }
---
+++ src/objects/serializableIsaacAPIClassTypeToCopyFunction.ts
@@ -0,0 +1,18 @@
+import { SerializableIsaacAPIClassType } from "../enums/private/SerializableIsaacAPIClassType";
+import { SerializationType } from "../enums/SerializationType";
+import { copyColor } from "../functions/color";
+import { copyKColor } from "../functions/kColor";
+import { copyRNG } from "../functions/rng";
+import { copyVector } from "../functions/vector";
+
+export const SERIALIZABLE_ISAAC_API_CLASS_TYPE_TO_COPY_FUNCTION: {
+  readonly [key in SerializableIsaacAPIClassType]: (
+    object: unknown,
+    serializationType: SerializationType,
+  ) => boolean;
+} = {
+  [SerializableIsaacAPIClassType.COLOR]: copyColor,
+  [SerializableIsaacAPIClassType.KCOLOR]: copyKColor,
+  [SerializableIsaacAPIClassType.RNG]: copyRNG,
+  [SerializableIsaacAPIClassType.VECTOR]: copyVector,
+};
