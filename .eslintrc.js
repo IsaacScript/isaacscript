bb6be84e35952a0596868d9f86a428c548653d20
James <5511220+Zamiell@users.noreply.github.com>
2022-05-13 08:19:44 -0400

    updating cspell template

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,22 +2,27 @@
 // https://eslint.org/docs/user-guide/configuring
 module.exports = {
   extends: [
-    // The linter base is the shared IsaacScript config:
-    // https://github.com/IsaacScript/eslint-config-isaacscript/blob/main/base.js
+    /**
+     * The linter base is the shared IsaacScript config:
+     * https://github.com/IsaacScript/eslint-config-isaacscript/blob/main/base.js
+     */
     "eslint-config-isaacscript/base",
   ],
 
-  // Don't bother linting the template files (for inserting into a new IsaacScript project) or the
-  // compiled output.
+  /**
+   * Don't bother linting the template files (for inserting into a new IsaacScript project) or the
+   * compiled output.
+   */
   ignorePatterns: ["./file-templates/**", "./dist/**"],
 
   parserOptions: {
-    // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
-    // things to lint correctly. We do not point this at "./tsconfig.json" because certain files
-    // (such at this file) should be linted but not included in the actual project output.
+    /**
+     * ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
+     * things to lint correctly. We do not point this at "./tsconfig.json" because certain files
+     * (such at this file) should be linted but not included in the actual project output.
+     */
     project: "./tsconfig.eslint.json",
   },
 
-  // We modify the linting rules from the base for some specific things.
   rules: {},
 };
--- file-templates/static/.eslintrc.js
+++ file-templates/static/.eslintrc.js
@@ -2,20 +2,23 @@
 // https://eslint.org/docs/user-guide/configuring
 module.exports = {
   extends: [
-    // The linter base is the IsaacScript mod config:
-    // https://github.com/IsaacScript/eslint-config-isaacscript/blob/main/mod.js
+    /**
+     * The linter base is the IsaacScript mod config:
+     * https://github.com/IsaacScript/eslint-config-isaacscript/blob/main/mod.js
+     */
     "eslint-config-isaacscript/mod",
   ],
 
   parserOptions: {
-    // ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
-    // things to lint correctly. We do not point this at "./tsconfig.json" because certain files
-    // (such at this file) should be linted but not included in the actual project output.
+    /**
+     * ESLint needs to know about the project's TypeScript settings in order for TypeScript-specific
+     * things to lint correctly. We do not point this at "./tsconfig.json" because certain files
+     * (such at this file) should be linted but not included in the actual project output.
+     */
     project: "./tsconfig.eslint.json",
   },
 
-  // We modify the linting rules from the base for some specific things.
   rules: {
-    // Insert changed or disabled rules here, if necessary
+    // Insert changed or disabled rules here, if necessary.
   },
 };
