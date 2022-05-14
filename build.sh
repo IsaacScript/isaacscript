207b5a7df6401b99483c530b9a00a4e0d8a6d60b
James <5511220+Zamiell@users.noreply.github.com>
2022-05-14 00:25:25 -0400

    bugfix for triple slash directive not working

--- build.sh
+++ build.sh
@@ -13,4 +13,11 @@ cd "$DIR"
 # Compile the project, which will result in Lua files & TypeScript definition files.
 npx tstl
 
+# Copy the declarations into place. (The TypeScript compiler does not do this automatically for some
+# reason.)
+cp -R "$DIR/src/types" "$DIR/dist/"
+
+# TypeScript messes up the path inside of the triple slash directive, so we must manually repair it.
+sed --in-place 's/\.\.\/src\///g' "$DIR/dist/index.d.ts"
+
 echo "Successfully built in $SECONDS seconds."
