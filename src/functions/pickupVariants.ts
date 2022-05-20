b549c081b23883f63e14cace55c50f85a40965b9
James <5511220+Zamiell@users.noreply.github.com>
2022-05-20 05:29:31 -0400

    adding more pickup identity functions

--- src/functions/pickupVariants.ts
+++ src/functions/pickupVariants.ts
@@ -22,11 +22,36 @@ export function isBomb(pickup: EntityPickup): pickup is EntityPickupBomb {
   return pickup.Variant === PickupVariant.BOMB;
 }
 
+/** For PickupVariant.POOP (42) */
+export function isPoop(pickup: EntityPickup): pickup is EntityPickupPoop {
+  return pickup.Variant === PickupVariant.POOP;
+}
+
+/** For PickupVariant.POOP (42) */
+export function isSack(pickup: EntityPickup): pickup is EntityPickupSack {
+  return pickup.Variant === PickupVariant.SACK;
+}
+
+/** For PickupVariant.PILL (70) */
+export function isPill(pickup: EntityPickup): pickup is EntityPickupPill {
+  return pickup.Variant === PickupVariant.PILL;
+}
+
+/** For PickupVariant.LIL_BATTERY (90) */
+export function isBattery(pickup: EntityPickup): pickup is EntityPickupBattery {
+  return pickup.Variant === PickupVariant.LIL_BATTERY;
+}
+
 /** For PickupVariant.COLLECTIBLE (100) */
 export function isCollectible(
   pickup: EntityPickup,
 ): pickup is EntityPickupCollectible {
   return pickup.Variant === PickupVariant.COLLECTIBLE;
+}
+
+/** For PickupVariant.TAROT_CARD (300) */
+export function isCardPickup(pickup: EntityPickup): pickup is EntityPickupCard {
+  return pickup.Variant === PickupVariant.TAROT_CARD;
 }
 
 /** For PickupVariant.TRINKET (350) */
