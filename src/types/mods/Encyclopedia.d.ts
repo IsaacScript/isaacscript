7f7aaff7d6b92b30a87f4d9f3298723eda383a1a
Scallium <88222543+Scallium@users.noreply.github.com>
2022-05-22 21:16:16 -0700

    Encyclopedia fixes (#47)
    * Encyclopedia fixes
    
    * Update .cspell.json
    
    * Update Encyclopedia.d.ts
    
    * Update Encyclopedia.d.ts
    
    * Update Encyclopedia.d.ts
    
    * Update Encyclopedia.d.ts

--- src/types/mods/Encyclopedia.d.ts
+++ src/types/mods/Encyclopedia.d.ts
@@ -3,6 +3,7 @@ import { PlayerType } from "../../enums/collections/subTypes";
 declare global {
   const Encyclopedia: EncyclopediaInterface | undefined;
 
+  /** @noSelf */
   interface EncyclopediaInterface {
     AddCharacter(args: {
       CompletionTrackerFuncs: [() => EncyclopediaItemVars[]];
@@ -23,16 +24,16 @@ declare global {
     AddItem(itemTab: {
       ActiveCharge?: number;
       Class?: string;
-      CloseFunc: (vars: EncyclopediaItemVars) => void;
+      CloseFunc?: (vars: EncyclopediaItemVars) => void;
       Desc?: string;
       Hide?: boolean;
       ID: number;
       ModName?: string;
       Name?: string;
-      Pools: EncyclopediaItemPools;
-      Sprite: Sprite;
-      StatusFunc: (vars: EncyclopediaItemVars) => EncyclopediaItemVars;
-      UnlockFunc: (vars: EncyclopediaItemVars) => EncyclopediaItemVars;
+      Pools?: EncyclopediaItemPoolType[];
+      Sprite?: Sprite;
+      StatusFunc?: (vars: EncyclopediaItemVars) => EncyclopediaItemVars;
+      UnlockFunc?: (vars: EncyclopediaItemVars) => EncyclopediaItemVars;
       WikiDesc?: EncyclopediaWikiDescription;
     }): void;
 
@@ -58,15 +59,15 @@ declare global {
     ): void;
 
     AddRune(itemTab: {
-      Class: string;
+      Class?: string;
       Desc?: string;
       ID: number;
       ModName?: string;
       Name?: string;
-      RuneType: number;
+      RuneType?: number;
       Sprite?: Sprite;
-      StatusClose: (vars: EncyclopediaItemVars) => void;
-      StatusFunc: (vars: EncyclopediaItemVars) => EncyclopediaItemVars;
+      StatusClose?: (vars: EncyclopediaItemVars) => void;
+      StatusFunc?: (vars: EncyclopediaItemVars) => EncyclopediaItemVars;
       UnlockFunc?: (vars: EncyclopediaItemVars) => EncyclopediaItemVars | void;
       WikiDesc?: EncyclopediaWikiDescription;
     }): void;
@@ -96,7 +97,7 @@ declare global {
       newSprite: string,
       layer: number,
     ): Sprite;
-    ItemPools: EncyclopediaItemPools;
+    ItemPools: int;
   }
 
   /**
@@ -106,25 +107,57 @@ declare global {
    */
   type EncyclopediaItemPools = Record<string, number>;
 
+  const enum EncyclopediaItemPoolType {
+    POOL_TREASURE = 1,
+    POOL_SHOP = 2,
+    POOL_BOSS = 3,
+    POOL_DEVIL = 4,
+    POOL_ANGEL = 5,
+    POOL_SECRET = 6,
+    POOL_ENCYCLOPEDIARARY = 7,
+    POOL_SHELL_GAME = 8, // unused (for now)
+    POOL_GOLDEN_CHEST = 9,
+    POOL_RED_CHEST = 10,
+    POOL_BEGGAR = 11,
+    POOL_DEMON_BEGGAR = 12,
+    POOL_CURSE = 13,
+    POOL_KEY_MASTER = 14,
+    POOL_BATTERY_BUM = 15,
+    POOL_MOMS_CHEST = 16,
+
+    POOL_GREED_TREASURE = 17,
+    POOL_GREED_BOSS = 18,
+    POOL_GREED_SHOP = 19,
+    POOL_GREED_DEVIL = 20,
+    POOL_GREED_ANGEL = 21,
+    POOL_GREED_CURSE = 22,
+    POOL_GREED_SECRET = 23,
+
+    POOL_CRANE_GAME = 24,
+    POOL_ULTRA_SECRET = 25, // unused
+    POOL_BOMB_BUM = 26,
+    POOL_PLANETARIUM = 27,
+    POOL_OLD_CHEST = 28,
+    POOL_BABY_SHOP = 29,
+    POOL_WOODEN_CHEST = 30,
+    POOL_ROTTEN_BEGGAR = 31,
+
+    NUM_ITEMPOOLS = 31,
+  }
+
   /**
    * A description for an item. Each object in the array is a category, containing a header and as
    * many paragraphs as are needed.
    */
-  type EncyclopediaWikiDescription = [
-    {
-      header: {
-        clr?: number;
-        fSize?: number;
-        hAlign?: number;
-        str: string;
-      };
-      paragraphs: [
-        {
-          str: string;
-        },
-      ];
-    },
-  ];
+
+  type EncyclopediaWikiDescriptionLine = {
+    clr?: number;
+    fsize?: number;
+    halign?: number;
+    str: string;
+  };
+
+  type EncyclopediaWikiDescription = EncyclopediaWikiDescriptionLine[][];
 
   /** Encyclopedia's "General Item Variables" object. Used to store data about an item. */
   interface EncyclopediaItemVars {
