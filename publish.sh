0c0b2acfaf4acdd85be3ff147386392274e89d0b
James <5511220+Zamiell@users.noreply.github.com>
2022-05-23 20:19:57 -0400

    chore: testing version

--- publish.sh
+++ publish.sh
@@ -28,14 +28,12 @@ else
 fi
 
 # Bump the version.
-nx version "$1" --skipProjectChangelog $PRE_ID
+nx version "$1" --allowEmptyRelease --skipProjectChangelog $PRE_ID
 # (add back the "--push" flag when @jscutlery/semver is fixed)
-
-exit 1
 
 # Temporary fix for @jscutlery/semver not appending a newline to the "package.json" files.
 npx prettier "$DIR/packages/$1/package.json" --write
-git commit --amend --no-edit
+git commit -C HEAD --amend # We need to take the metadata from the last commit to avoid blowing away the tag
 git push
 
 # Upload it to NPM.
