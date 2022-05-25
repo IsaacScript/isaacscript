c082eb121df47f6b4fbd403c1e2b6860ab510494
James <5511220+Zamiell@users.noreply.github.com>
2022-05-24 21:10:16 -0400

    chore: deploying docs in cI

---
+++ packages/docs/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e # Exit on any errors
+
+# Get the directory of this script
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+DOCS_REPO_NAME="isaacscript.github.io"
+cd "$DIR"
+rm -rf "$DIR/$DOCS_REPO_NAME"
+git clone --depth 1 --branch gh-pages "git@github.com:IsaacScript/$DOCS_REPO_NAME.git"
+cp -R "$DIR/build/"* "$DIR/$DOCS_REPO_NAME/"
+cd "$DIR/$DOCS_REPO_NAME"
+
+set +e
+if npx git-dirty; then
+  # There are no changes to deploy.
+  exit 0
+fi
+set -e
+
+git add -A
+git commit -m "deploy"
+git push
