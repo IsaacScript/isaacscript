14d09b7a86bc982103fd5561ca5c6785f0988154
James <5511220+Zamiell@users.noreply.github.com>
2022-06-06 11:35:53 -0400

    chore: fix publish.sh script

--- publish.sh
+++ publish.sh
@@ -8,24 +8,32 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd "$DIR"
 
-if [ ! -z "$1" ]; then
+if [ -z "$1" ]; then
   echo "Error: The package name is required."
+  exit 1
 fi
 PACKAGE_NAME=$1
 
 PACKAGE_DIR="$DIR/packages/$PACKAGE_NAME"
-if ! test -f "$PACKAGE_DIR"; then
-  echo "Error: The path of \"$PACKAGE_DIR\" does not exist."
+if ! test -d "$PACKAGE_DIR"; then
+  echo "Error: The directory of \"$PACKAGE_DIR\" does not exist."
+  exit 1
 fi
 
 cd "$PACKAGE_DIR"
 
-if [ ! -z "$2" ]; then
+if [ -z "$2" ]; then
   echo "Error: The version bump description is required."
+  exit 1
 fi
 VERSION_BUMP=$2
 
-yarn version --set-version $VERSION_BUMP
+if [ "$VERSION_BUMP" == "major" ] || [ "$VERSION_BUMP" == "minor" ] || [ "$VERSION_BUMP" == "patch" ]; then
+  yarn version --new-version $VERSION_BUMP
+else
+  yarn version --set-version $VERSION_BUMP
+fi
+
 bash "$PACKAGE_DIR/build.sh"
 cd "$DIR/dist/packages/$PACKAGE_NAME"
 npm publish
