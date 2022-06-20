d6ba4baa9a14aca341bd62995301e516b9b4a5a8
James <5511220+Zamiell@users.noreply.github.com>
2022-06-20 14:01:32 -0400

    chore: update deps

--- update.sh
+++ update.sh
@@ -16,7 +16,7 @@ OLD_HASH=$(md5sum "$PACKAGE_JSON")
 # - @mdx-js/react,react,react-dom - Stuck until Docusaurus upgrades.
 # - unified - Does not work with the current Markdown linting setup because it requires a "module"
 #   type in "package.json", and Docusaurus does not support this yet.
-npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON" --reject "chalk,@mdx-js/react,react,react-dom,unified"
+npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON" --reject "chalk,@mdx-js/react,react,react-dom,unified,sync-directory"
 NEW_HASH=$(md5sum "$PACKAGE_JSON")
 if [[ $OLD_HASH != $NEW_HASH ]]; then
   yarn
