8063418f937d06360e03cdf4cd985f5d328bd297
James <5511220+Zamiell@users.noreply.github.com>
2022-06-22 13:35:15 -0400

    fix(common): angleToDirection has wrong directions

--- packages/isaacscript-common/src/functions/direction.ts
+++ packages/isaacscript-common/src/functions/direction.ts
@@ -3,6 +3,13 @@ import { DIRECTION_NAMES } from "../objects/directionNames";
 import { DIRECTION_TO_DEGREES } from "../objects/directionToDegrees";
 import { DIRECTION_TO_VECTOR } from "../objects/directionToVector";
 
+/**
+ * Helper function to convert the degrees of an angle to the `Direction` enum.
+ *
+ * Note that this function considers 0 degrees to be pointing to the right, which is unusual because
+ * 0 normally corresponds to up. (This corresponds to how the `Vector.GetAngleDegrees` method
+ * works.)
+ */
 export function angleToDirection(angleDegrees: int): Direction {
   let positiveDegrees = angleDegrees;
   while (positiveDegrees < 0) {
@@ -11,22 +18,22 @@ export function angleToDirection(angleDegrees: int): Direction {
   const normalizedDegrees = positiveDegrees % 360;
 
   if (normalizedDegrees < 45) {
-    return Direction.UP;
+    return Direction.RIGHT;
   }
 
   if (normalizedDegrees < 135) {
-    return Direction.RIGHT;
-  }
-
-  if (normalizedDegrees < 225) {
     return Direction.DOWN;
   }
 
-  if (normalizedDegrees < 315) {
-    return Direction.RIGHT;
+  if (normalizedDegrees < 225) {
+    return Direction.LEFT;
   }
 
-  return Direction.UP;
+  if (normalizedDegrees < 315) {
+    return Direction.UP;
+  }
+
+  return Direction.RIGHT;
 }
 
 export function directionToDegrees(direction: Direction): int {
