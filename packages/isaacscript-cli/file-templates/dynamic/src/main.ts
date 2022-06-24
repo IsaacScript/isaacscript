9f505487c629b778cd1a509ecbbce47488654baa
James <5511220+Zamiell@users.noreply.github.com>
2022-06-24 14:38:47 -0400

    fix(cli): main.ts template

--- packages/isaacscript-cli/file-templates/dynamic/src/main.ts
+++ packages/isaacscript-cli/file-templates/dynamic/src/main.ts
@@ -1,19 +1,22 @@
 import { ModCallback } from "isaac-typescript-definitions";
+import { log } from "isaacscript-common";
 
 const MOD_NAME = "MOD-NAME-TO-REPLACE";
 
-export function main(): void {
+main();
+
+function main(): void {
   // Instantiate a new mod object, which grants the ability to add callback functions that
   // correspond to in-game events.
   const mod = RegisterMod(MOD_NAME, 1);
 
-  // Set a callback function that corresponds to when a new run is started.
+  // Register a callback function that corresponds to when a new run is started.
   mod.AddCallback(ModCallback.POST_GAME_STARTED, postGameStarted);
 
-  // Print an initialization message to the "log.txt" file.
-  Isaac.DebugString(`${MOD_NAME} initialized.`);
+  // Print an message to the "log.txt" file.
+  log(`${MOD_NAME} initialized.`);
 }
 
 function postGameStarted() {
-  Isaac.DebugString("Callback triggered: POST_GAME_STARTED");
+  log("Callback triggered: POST_GAME_STARTED");
 }
