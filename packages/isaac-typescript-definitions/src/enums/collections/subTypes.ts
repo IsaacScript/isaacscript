38193172e2ebfa1fb42445ff9c5eca8a2197ede9
James <5511220+Zamiell@users.noreply.github.com>
2022-06-24 22:53:16 -0400

    refactor(defs): moving DiceFloorSubType to subTypes file

--- packages/isaac-typescript-definitions/src/enums/collections/subTypes.ts
+++ packages/isaac-typescript-definitions/src/enums/collections/subTypes.ts
@@ -1488,6 +1488,30 @@ export enum HeavenLightDoorSubType {
   MOONLIGHT = 1,
 }
 
+/** For `EntityType.EFFECT` (1000), `EffectVariant.DICE_FLOOR` (76) */
+export enum DiceFloorSubType {
+  /** Has the same effect as using a D4. */
+  ONE_PIP = 0,
+
+  /** Has the same effect as using a D20. */
+  TWO_PIP = 1,
+
+  /**
+   * Rerolls all pickups and trinkets on the floor, including items inside of a shop, excluding
+   * collectibles.
+   */
+  THREE_PIP = 2,
+
+  /** Rerolls all collectibles on the floor. */
+  FOUR_PIP = 3,
+
+  /** Has the same effect as using a Forget Me Now. */
+  FIVE_PIP = 4,
+
+  /** Has the combined effect of a 1-pip, 3-pip and 4-pip dice room. */
+  SIX_PIP = 5,
+}
+
 /** For `EntityType.EFFECT` (1000), `EffectVariant.TALL_LADDER` (156) */
 export enum LadderSubType {
   TALL_LADDER = 0,
