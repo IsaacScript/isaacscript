3178cf30a25319717da2103ef8c0b4405e222384
James <5511220+Zamiell@users.noreply.github.com>
2022-06-28 19:35:44 -0400

    chore(common): test new build algorithm

--- publish.sh
+++ publish.sh
@@ -31,7 +31,9 @@ VERSION_BUMP="$2"
 yarn config set version-tag-prefix "$PACKAGE_NAME-"
 yarn config set version-git-message "chore(release): $PACKAGE_NAME-%s"
 
-if [ "$VERSION_BUMP" == "major" ] || [ "$VERSION_BUMP" == "minor" ] || [ "$VERSION_BUMP" == "patch" ]; then
+if [ "$VERSION_BUMP" == "dev"] then
+  yarn version --prerelease --preid=dev
+elif [ "$VERSION_BUMP" == "major" ] || [ "$VERSION_BUMP" == "minor" ] || [ "$VERSION_BUMP" == "patch" ]; then
   yarn version --new-version $VERSION_BUMP --tag
 else
   yarn version --set-version $VERSION_BUMP
