463d8f7b407d9e412e76933f0dd41d2466f1cc97
James <5511220+Zamiell@users.noreply.github.com>
2022-06-30 21:36:11 -0400

    chore: rolling back deps

--- update.sh
+++ update.sh
@@ -12,13 +12,14 @@ PACKAGE_JSON="$DIR/package.json"
 OLD_HASH=$(md5sum "$PACKAGE_JSON")
 # Old versions:
 # - @mdx-js/react - Stuck until Docusaurus upgrades.
+# - @types/update-notifier - Stuck until TypeScript supports ESM.
 # - chalk - Stuck until TypeScript supports ESM.
 # - react - Stuck until Docusaurus upgrades.
 # - react-dom - Stuck until Docusaurus upgrades.
 # - unified - Does not work with the current Markdown linting setup because it requires a "module"
 #   type in "package.json", and Docusaurus does not support this yet.
 # - update-notifier - Stuck until TypeScript supports ESM.
-npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON" --reject "@mdx-js/react,chalk,react,react-dom,unified,update-notifier"
+npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON" --reject "@mdx-js/react,@types/update-notifier,chalk,react,react-dom,unified,update-notifier"
 NEW_HASH=$(md5sum "$PACKAGE_JSON")
 if [[ $OLD_HASH != $NEW_HASH ]]; then
   yarn install
