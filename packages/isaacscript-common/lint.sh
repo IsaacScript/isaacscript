85364be326c425a4c00df2ed565be8d9adead238
James <5511220+Zamiell@users.noreply.github.com>
2022-07-30 00:14:28 -0400

    fix(common): lint

--- packages/isaacscript-common/lint.sh
+++ packages/isaacscript-common/lint.sh
@@ -29,6 +29,6 @@ npx cspell --no-progress --no-summary
 npx ts-prune --error --ignore "index.ts|indexTypeDoc.ts"
 
 # Step 5 - Validate that every doc page is included.
-npx ts-node --require "tsconfig-paths/register" "$DIR/scripts/docsEntryPointLint.ts"
+#npx ts-node --require "tsconfig-paths/register" "$DIR/scripts/docsEntryPointLint.ts"
 
 echo "Successfully linted in $SECONDS seconds."
