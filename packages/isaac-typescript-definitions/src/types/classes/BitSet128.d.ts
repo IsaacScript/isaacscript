5fbd525bd93f09c7e6a7b274d7a043970f7aa116
James <5511220+Zamiell@users.noreply.github.com>
2022-07-31 22:37:30 -0400

    feat(defs): add BitSet128 missing fields

--- packages/isaac-typescript-definitions/src/types/classes/BitSet128.d.ts
+++ packages/isaac-typescript-definitions/src/types/classes/BitSet128.d.ts
@@ -1,10 +1,25 @@
-declare function BitSet128(this: void, l?: int, h?: int): BitSet128;
+/* eslint-disable isaacscript/member-ordering */
 
 /**
  * A class used to represent a 128-bit number. This is used because Lua only has 64-bit numbers and
  * C++ uses 128-bit numbers for certain things.
+ *
+ * @param lowBits The 64-bit number that represents the lower half of the number.
+ * @param highBits The 64-bit number that represents the upper half of the number.
  */
+declare function BitSet128(
+  this: void,
+  lowBits?: int,
+  highBits?: int,
+): BitSet128;
+
 declare interface BitSet128 extends IsaacAPIClass {
+  /** The 64-bit number that represents the lower half of the number. */
+  l: int;
+
+  /** The 64-bit number that represents the upper half of the number. */
+  h: int;
+
   /**
    * These methods are used to transpile:
    *
