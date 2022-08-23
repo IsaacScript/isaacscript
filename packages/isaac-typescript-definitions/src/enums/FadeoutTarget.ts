9c8ebee518d000382af2ef2afcf269e26c315404
James <5511220+Zamiell@users.noreply.github.com>
2022-08-22 20:48:14 -0400

    chore(defs): better docs for FadeoutTarget

--- packages/isaac-typescript-definitions/src/enums/FadeoutTarget.ts
+++ packages/isaac-typescript-definitions/src/enums/FadeoutTarget.ts
@@ -5,8 +5,14 @@ export enum FadeoutTarget {
    */
   NONE = 0,
 
+  /** The screen that has the three rectangles for the three different save files. */
   FILE_SELECT = 1,
+
+  /** The screen that lists "New Run", "Continue", "Challenges", and so on. */
   MAIN_MENU = 2,
+
+  /** The screen that shows "The Binding of Isaac: Repentance". */
   TITLE_SCREEN = 3,
+
   RESTART_RUN = 4,
 }
