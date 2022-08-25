6d84ff710fc404d1864f1c48aee9be43e6972975
James <5511220+Zamiell@users.noreply.github.com>
2022-08-25 18:31:18 -0400

    fix(common): initArray export

--- packages/isaacscript-common/src/index.ts
+++ packages/isaacscript-common/src/index.ts
@@ -84,6 +84,7 @@ export * from "./functions/globals";
 export * from "./functions/gridEntities";
 export * from "./functions/gridEntitiesSpecific";
 export * from "./functions/hex";
+export * from "./functions/initArray";
 export * from "./functions/input";
 export * from "./functions/isaacAPIClass";
 export * from "./functions/itemPool";
