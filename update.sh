1d51a7e4c23dae82b86f77ee09b387100ff4e675
James <5511220+Zamiell@users.noreply.github.com>
2022-08-25 20:56:01 -0400

    chore: downgrade deps

--- update.sh
+++ update.sh
@@ -16,10 +16,11 @@ OLD_HASH=$(md5sum "$PACKAGE_JSON")
 # - chalk - Stuck until TypeScript supports ESM.
 # - react - Stuck until Docusaurus upgrades.
 # - react-dom - Stuck until Docusaurus upgrades.
+# - typescript - Does not work with TSTL.
 # - unified - Does not work with the current Markdown linting setup because it requires a "module"
 #   type in "package.json", and Docusaurus does not support this yet.
 # - update-notifier - Stuck until TypeScript supports ESM.
-npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON" --reject "@mdx-js/react,@types/update-notifier,chalk,react,react-dom,unified,update-notifier"
+npx npm-check-updates --upgrade --packageFile "$PACKAGE_JSON" --reject "@mdx-js/react,@types/update-notifier,chalk,react,react-dom,typescript,unified,update-notifier"
 NEW_HASH=$(md5sum "$PACKAGE_JSON")
 if [[ $OLD_HASH != $NEW_HASH ]]; then
   yarn install
