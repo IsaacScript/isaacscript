7d3c203a9c21a8e81527a57e7073712baa3cfb67
James <5511220+Zamiell@users.noreply.github.com>
2022-08-30 16:04:50 -0400

    fix(defs): missing file

---
+++ packages/isaac-typescript-definitions/src/enums/CollectibleSpriteLayer.ts
@@ -0,0 +1,18 @@
+/** Corresponds to "005.100_collectible.anm2". */
+export enum CollectibleSpriteLayer {
+  /** The pedestal. */
+  BODY = 0,
+
+  /** The bouncing collectible. */
+  HEAD = 1,
+
+  SPARKLE = 2,
+
+  /** The shadow below the pedestal. */
+  SHADOW = 3,
+
+  /** The shadow on top of the pedestal. */
+  ITEM_SHADOW = 4,
+
+  ALTAR = 5,
+}
