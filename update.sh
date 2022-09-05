acc387cfbce95c7f7e5a04e622620411f24541d3
James <5511220+Zamiell@users.noreply.github.com>
2022-09-04 23:51:26 -0400

    chore: update deps

--- update.sh
+++ update.sh
@@ -18,7 +18,6 @@ OLD_HASH=$(md5sum "$PACKAGE_JSON")
 # - react - Stuck until Docusaurus upgrades.
 # - react-dom - Stuck until Docusaurus upgrades.
 # - typedoc - The latest version is bugged.
-# - typescript - Does not work with TSTL.
 # - unified - Does not work with the current Markdown linting setup because it requires a "module"
 #   type in "package.json", and Docusaurus does not support this yet.
 # - update-notifier - Stuck until TypeScript supports ESM.
