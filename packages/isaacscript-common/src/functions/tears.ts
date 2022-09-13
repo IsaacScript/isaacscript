be386023a4d57e8a78e581a283d6873f832c441f
James <5511220+Zamiell@users.noreply.github.com>
2022-09-13 18:03:01 -0400

    fix(common): tear helper functions frame

--- packages/isaacscript-common/src/functions/tears.ts
+++ packages/isaacscript-common/src/functions/tears.ts
@@ -37,16 +37,16 @@ export function getTearsStat(fireDelay: float): float {
  * is determined by looking at the parent.
  *
  * For the special case of Incubus and Blood Babies, the spawner entity of the tear is always the
- * player. The parent of the tear is the player on frames 0 and 1 and it is the familiar on frames 2
- * and onwards. For this reason, you can only use this function in the `POST_TEAR_INIT_VERY_LATE`
- * callback or on frame 2+.
+ * player. The parent of the tear is the player on frame 0 and it is the familiar on frame 1 and
+ * onwards. For this reason, you can only use this function in the `POST_TEAR_INIT_VERY_LATE`
+ * callback or on frame 1+.
  *
- * If this function is called on frame 0 or frame 1, it will throw a run-time error.
+ * If this function is called on frame 0, it will throw a run-time error.
  */
 export function isTearFromFamiliar(tear: EntityTear): boolean {
-  if (tear.FrameCount === 0 || tear.FrameCount === 1) {
+  if (tear.FrameCount === 0) {
     error(
-      `Failed to check if the given tear was from a player since the tear's frame count was equal to ${tear.FrameCount}. (The "isTearFromFamiliar" function must only be used in the "POST_TEAR_INIT_VERY_LATE" callback or on frame 2 and onwards.)`,
+      'Failed to check if the given tear was from a player since the tear\'s frame count was equal to 0. (The "isTearFromFamiliar" function must only be used in the "POST_TEAR_INIT_VERY_LATE" callback or on frame 1 and onwards.)',
     );
   }
 
@@ -64,16 +64,16 @@ export function isTearFromFamiliar(tear: EntityTear): boolean {
  * is determined by looking at the parent.
  *
  * For the special case of Incubus and Blood Babies, the spawner entity of the tear is always the
- * player. The parent of the tear is the player on frames 0 and 1 and it is the familiar on frames 2
- * and onwards. For this reason, you can only use this function in the `POST_TEAR_INIT_VERY_LATE`
- * callback or on frame 2+.
+ * player. The parent of the tear is the player on frame 0 and it is the familiar on frame 1 and
+ * onwards. For this reason, you can only use this function in the `POST_TEAR_INIT_VERY_LATE`
+ * callback or on frame 1+.
  *
- * If this function is called on frame 0 or frame 1, it will throw a run-time error.
+ * If this function is called on frame 0, it will throw a run-time error.
  */
 export function isTearFromPlayer(tear: EntityTear): boolean {
-  if (tear.FrameCount === 0 || tear.FrameCount === 1) {
+  if (tear.FrameCount === 0) {
     error(
-      `Failed to check if the given tear was from a player since the tear's frame count was equal to ${tear.FrameCount}. (The "isTearFromPlayer" function must only be used in the "POST_TEAR_INIT_VERY_LATE" callback or on frame 2 and onwards.)`,
+      'Failed to check if the given tear was from a player since the tear\'s frame count was equal to 0. (The "isTearFromPlayer" function must only be used in the "POST_TEAR_INIT_VERY_LATE" callback or on frame 1 and onwards.)',
     );
   }
 
