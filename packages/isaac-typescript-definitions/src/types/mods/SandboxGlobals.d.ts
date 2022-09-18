d335c3c401669b912230a32285c8131a40f0f03d
James <5511220+Zamiell@users.noreply.github.com>
2022-09-18 15:39:27 -0400

    fix(defs): update racing+ sandbox vars

--- packages/isaac-typescript-definitions/src/types/mods/SandboxGlobals.d.ts
+++ packages/isaac-typescript-definitions/src/types/mods/SandboxGlobals.d.ts
@@ -4,13 +4,7 @@
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
  * it will be undefined.
  */
-declare const sandboxGetTraceback: (() => string) | undefined;
-
-/**
- * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
- * it will be undefined.
- */
-declare const sandboxTraceback: (() => void) | undefined;
+declare const SandboxGetDate: (() => string) | undefined;
 
 /**
  * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
@@ -18,4 +12,24 @@ declare const sandboxTraceback: (() => void) | undefined;
  *
  * @param levels The amount of levels to look backwards in the call stack.
  */
-declare const getParentFunctionDescription: ((levels: int) => void) | undefined;
+declare const SandboxGetParentFunctionDescription:
+  | ((levels: int) => void)
+  | undefined;
+
+/**
+ * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
+ * it will be undefined.
+ */
+declare const SandboxGetTime: (() => float) | undefined;
+
+/**
+ * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
+ * it will be undefined.
+ */
+declare const SandboxGetTraceback: (() => string) | undefined;
+
+/**
+ * This is a global function provided by the Racing+ client sandbox. If the sandbox is not present,
+ * it will be undefined.
+ */
+declare const SandboxTraceback: (() => void) | undefined;
