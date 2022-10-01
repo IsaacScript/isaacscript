dc0c252e8da5b3cf0ef44a088b7764ce79086c96
James <5511220+Zamiell@users.noreply.github.com>
2022-10-01 16:33:45 -0400

    chore: revert esm dep

--- update.sh
+++ update.sh
@@ -17,7 +17,7 @@ OLD_HASH=$(md5sum "$PACKAGE_JSON")
 # - jest - Stuck until `ts-jest` is updated.
 # - react - Stuck until Docusaurus upgrades.
 # - react-dom - Stuck until Docusaurus upgrades.
-# - typedoc - The latest version is bugged.
+# - sort-package-json - Stuck until TypeScript supports ESM.
 # - unified - Does not work with the current Markdown linting setup because it requires a "module"
 #   type in "package.json", and Docusaurus does not support this yet.
 # - update-notifier - Stuck until TypeScript supports ESM.
