c5ec9f208772cb6f1097ece743f68ca72ba59cf5
James <5511220+Zamiell@users.noreply.github.com>
2022-11-15 14:50:48 -0500

    fix(common): disable lint for isc lua

--- packages/isaacscript-cli/src/plugins/constants.ts
+++ packages/isaacscript-cli/src/plugins/constants.ts
@@ -4,9 +4,16 @@
  * ```lua
  * local __TS__Unpack = table.unpack or unpack
  * ```
+ *
+ * `redundant-parameter` is turned off to expressions like:
+ *
+ * ```lua
+ * debug.traceback = function(thread, message, level)
+ * ```
  */
 export const LUA_LANGUAGE_SERVER_DISABLES = `
 ---@diagnostic disable: deprecated
+---@diagnostic disable: redundant-parameter
 `
   .trim()
   .concat("\n\n");
