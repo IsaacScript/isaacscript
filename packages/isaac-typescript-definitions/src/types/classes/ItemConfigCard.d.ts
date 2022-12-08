841476d45abddfc064a18f18657dbc751c3f51cb
James <5511220+Zamiell@users.noreply.github.com>
2022-12-08 12:40:12 -0500

    fix(defs): build

--- packages/isaac-typescript-definitions/src/types/classes/ItemConfigCard.d.ts
+++ packages/isaac-typescript-definitions/src/types/classes/ItemConfigCard.d.ts
@@ -1,24 +1,26 @@
 import { ItemConfigCardType } from "../../enums/ItemConfigCardType";
 
-declare interface ItemConfigCard extends IsaacAPIClass {
-  IsAvailable(): boolean;
-  IsCard(): boolean;
-  IsRune(): boolean;
+declare global {
+  interface ItemConfigCard extends IsaacAPIClass {
+    IsAvailable(): boolean;
+    IsCard(): boolean;
+    IsRune(): boolean;
 
-  AchievementID: int;
-  AnnouncerDelay: int;
-  AnnouncerVoice: int;
-  CardType: ItemConfigCardType;
-  Description: string;
-  GreedModeAllowed: boolean;
-  HudAnim: string;
-  ID: int;
-  MimicCharge: int;
-  Name: string;
+    AchievementID: int;
+    AnnouncerDelay: int;
+    AnnouncerVoice: int;
+    CardType: ItemConfigCardType;
+    Description: string;
+    GreedModeAllowed: boolean;
+    HudAnim: string;
+    ID: int;
+    MimicCharge: int;
+    Name: string;
 
-  /**
-   * Corresponds to the "pickup" value in "pocketitems.xml". This determines the visual look of the
-   * card when it is dropped on the ground.
-   */
-  PickupSubtype: int;
+    /**
+     * Corresponds to the "pickup" value in "pocketitems.xml". This determines the visual look of
+     * the card when it is dropped on the ground.
+     */
+    PickupSubtype: int;
+  }
 }
