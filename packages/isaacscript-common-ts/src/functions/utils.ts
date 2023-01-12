d285cd6124136c288027fcebf1c584840849f3fd
James <5511220+Zamiell@users.noreply.github.com>
2023-01-12 14:40:21 -0500

    feat(common-ts): parseIntSafe

--- packages/isaacscript-common-ts/src/functions/utils.ts
+++ packages/isaacscript-common-ts/src/functions/utils.ts
@@ -43,3 +43,34 @@ export function iRange(start: number, end?: number, increment = 1): number[] {
   const exclusiveEnd = end + 1;
   return eRange(start, exclusiveEnd, increment);
 }
+
+/**
+ * This is a more reliable version of `parseInt`. By default, `parseInt('1a')` will return "1",
+ * which is unexpected. This returns either an integer or NaN.
+ */
+export function parseIntSafe(input: string): number {
+  if (typeof input !== "string") {
+    return NaN;
+  }
+
+  // Remove all leading and trailing whitespace.
+  let trimmedInput = input.trim();
+
+  const isNegativeNumber = trimmedInput.startsWith("-");
+  if (isNegativeNumber) {
+    // Remove the leading minus sign before we match the regular expression.
+    trimmedInput = trimmedInput.substring(1);
+  }
+
+  if (/^\d+$/.exec(trimmedInput) === null) {
+    // "\d" matches any digit (same as "[0-9]").
+    return NaN;
+  }
+
+  if (isNegativeNumber) {
+    // Add the leading minus sign back.
+    trimmedInput = `-${trimmedInput}`;
+  }
+
+  return Number.parseInt(trimmedInput, 10);
+}
