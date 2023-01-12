06abfd7d188ea5a474e01cb6d55da2f111e8dff4
James <5511220+Zamiell@users.noreply.github.com>
2023-01-12 18:16:16 -0500

    chore: update deps

--- packages/isaac-typescript-definitions/src/types/classes/QueueItemData.d.ts
+++ packages/isaac-typescript-definitions/src/types/classes/QueueItemData.d.ts
@@ -1,5 +1,9 @@
 declare interface QueueItemData extends IsaacAPIClass {
   Charge: int;
-  Item?: ItemConfigItem;
+
+  // This must be marked explicitly as `ItemConfigItem | undefined` instead of using the optional
+  // property since end-users should be able to assign undefined to it in order to clear the queue.
+  Item: ItemConfigItem | undefined;
+
   Touched: boolean;
 }
