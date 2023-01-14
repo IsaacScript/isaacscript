80b07c12acbe64bbf8bfeac68ae717f10363e6e0
James <5511220+Zamiell@users.noreply.github.com>
2023-01-14 14:05:47 -0500

    chore: test local eslint plugin

--- lint.sh
+++ lint.sh
@@ -11,7 +11,7 @@ SECONDS=0
 cd "$DIR"
 
 # Checking "package.json" files
-npx ts-node --require "tsconfig-paths/register" --project "$DIR/tools/tsconfig.json" "$DIR/tools/packageJSONLint.ts"
+#npx ts-node --require "tsconfig-paths/register" --project "$DIR/tools/tsconfig.json" "$DIR/tools/packageJSONLint.ts"
 
 if [ "$1" != "json" ]; then
   echo "Linting scripts..."
