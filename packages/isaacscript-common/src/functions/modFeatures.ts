85fd96ecb5da02c329f1fcae62fb8082df89223d
James <5511220+Zamiell@users.noreply.github.com>
2023-01-17 04:07:05 -0500

    chore(common): update docs for initModFeatures

--- packages/isaacscript-common/src/functions/modFeatures.ts
+++ packages/isaacscript-common/src/functions/modFeatures.ts
@@ -4,6 +4,17 @@ import { ModUpgraded } from "../types/ModUpgraded";
 /**
  * Helper function to instantiate an array of mod features all at once. Use this function if your
  * mod uses the pattern of expressing mod features as `ModFeature` classes.
+ *
+ * For example:
+ *
+ * ```ts
+ * const features = [
+ *   MyFeature1,
+ *   MyFeature2,
+ *   MyFeature3,
+ * ] as const;
+ * initModFeatures(mod, modFeatures);
+ * ```
  */
 export function initModFeatures(
   mod: ModUpgraded,
