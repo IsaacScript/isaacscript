19bc0e878117c20d3946be520ff2dde76b4d5900
James <5511220+Zamiell@users.noreply.github.com>
2023-01-18 18:16:37 -0500

    chore(common): docs for TSTLClass

--- packages/isaacscript-common/src/interfaces/TSTLClassMetatable.ts
+++ packages/isaacscript-common/src/interfaces/TSTLClassMetatable.ts
@@ -1,3 +1,7 @@
+/**
+ * This is a type representing the metatable of a user-created class from TypeScript code.
+ * (TypeScriptToLua transpiles TypeScript classes to a Lua table with a specific kind of metatable.)
+ */
 export interface TSTLClassMetatable {
   ____constructor: () => void;
   __index: unknown;
--- packages/isaacscript-common/src/types/TSTLClass.ts
+++ packages/isaacscript-common/src/types/TSTLClass.ts
@@ -1,5 +1,6 @@
 /**
- * This is a type representing a user-created class from TypeScript code.
+ * This is a type representing a user-created class from TypeScript code. (TypeScriptToLua
+ * transpiles TypeScript classes to a Lua table with a specific kind of metatable.)
  *
  * This type is used by the save data manager to when copying, serializing, and deserializing.
  */
