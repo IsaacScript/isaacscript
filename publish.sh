15991f9b76760d2b81e8903ba1fb01752be4d2ed
James <5511220+Zamiell@users.noreply.github.com>
2023-01-26 13:40:59 -0500

    chore: update publish.sh

--- publish.sh
+++ publish.sh
@@ -60,6 +60,8 @@ npm publish --access=public --tag=$NPM_TAG
 
 sleep 1
 bash "$DIR/update.sh"
+npx syncpack fix-mismatches --prod --dev
+bash "$DIR/packages/isaacscript-lint/update.sh"
 
 set +e
 if npx git-dirty; then
