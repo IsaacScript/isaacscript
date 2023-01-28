5d44728bb2650f3e64322f9c220cc0d677552e54
James <5511220+Zamiell@users.noreply.github.com>
2023-01-28 00:15:56 -0500

    refactor(common-ts): remove node dep

--- packages/isaacscript-common-ts/src/index.ts
+++ packages/isaacscript-common-ts/src/index.ts
@@ -1,6 +1,5 @@
 export * from "./functions/array";
 export * from "./functions/enums";
-export * from "./functions/file";
 export * from "./functions/random";
 export * from "./functions/string";
 export * from "./functions/utils";
