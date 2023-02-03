7d81d599e81a536a1c338db390c6d5062b2bf98f
James <5511220+Zamiell@users.noreply.github.com>
2023-02-03 03:45:26 -0500

    refactor(config): split out into new file

--- packages/eslint-config-isaacscript/base-eslint.js
+++ packages/eslint-config-isaacscript/base-eslint.js
@@ -13,119 +13,272 @@ module.exports = {
      * Airbnb changes the default options for some reason, which makes the rule less strict.
      */
     "array-callback-return": ["warn", {}],
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/consistent-return
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/best-practices.js
+     *
+     * We prefer the "noImplicitReturns" compiler flag over the "consistent-return" ESLint rule,
+     * since it is type-aware:
+     * https://github.com/typescript-eslint/typescript-eslint/issues/5254
+     */
+    "consistent-return": ["off"],
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/curly
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/best-practices.js
+     *
+     * Always requiring curly braces can partially ward against Apple-style if statement bugs:
+     * https://www.imperialviolet.org/2014/02/22/applebug.html
+     *
+     * Additionally, this rule needs to be set to "all" to work properly with
+     * `eslint-prettier-config`:
+     * https://github.com/prettier/eslint-config-prettier#curly
+     */
+    curly: ["warn", "all"],
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/default-case
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/best-practices.js
+     *
+     * This rule is generally bad to have on in TypeScript projects:
+     * https://github.com/typescript-eslint/typescript-eslint/issues/5254
+     */
+    "default-case": "off",
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/no-console
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js
+     *
+     * Command-line programs commonly write to standard out and standard error.
+     */
+    "no-console": "off",
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/no-constant-binary-expression
+     *
+     * Not defined in parent configs.
+     *
+     * This rule was released in 2022 and is known to catch many subtle bugs:
+     * https://github.com/eslint/eslint.org/pull/240/files
+     */
+    "no-constant-binary-expression": "warn",
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/no-continue
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+     *
+     * Proper use of continues can reduce indentation for long blocks of code.
+     */
+    "no-continue": "off",
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/no-plusplus
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+     *
+     * Airbnb disallows these because it can lead to errors with minified code. When using Prettier,
+     * it adds semi-colons everywhere, so we don't have to worry about this.
+     */
+    "no-plusplus": "off",
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/no-restricted-syntax
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+     *
+     * - We move the selector for "for..of" loops, since they are commonly used.
+     * - We add a selector for "empty" invocations of the "array.push()" method.
+     */
+    "no-restricted-syntax": [
+      "warn",
+      {
+        selector: "ForInStatement",
+        message:
+          "for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.",
+      },
+      {
+        selector: "LabeledStatement",
+        message:
+          "Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.",
+      },
+      {
+        selector: "WithStatement",
+        message:
+          "`with` is disallowed in strict mode because it makes code impossible to predict and optimize.",
+      },
+      {
+        selector:
+          "CallExpression[callee.property.name='push'][arguments.length=0]",
+        message: "push must always be called with at least one argument.",
+      },
+    ],
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/no-unused-expressions
+     *
+     * Not defined in parent configs.
+     *
+     * An unused expression which has no effect on the state of the program indicates a logic error.
+     */
+    "no-unused-expressions": "warn",
+
+    /**
+     * Documentation:
+     * https://eslint.org/docs/latest/rules/prefer-destructuring
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/es6.js
+     *
+     * Array destructuring can result in non-intuitive code.
+     */
+    "prefer-destructuring": [
+      "warn",
+      {
+        VariableDeclarator: {
+          array: false,
+          object: true,
+        },
+        AssignmentExpression: {
+          array: false,
+          object: false,
+        },
+      },
+    ],
+
+    // "constructor-super" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "for-direction" is enabled here:
+    // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js
+
+    // "getter-return" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-async-promise-executor"
+
+    // "no-await-in-loop"
+
+    // "no-class-assign"
+
+    // "no-compare-neg-zero"
+
+    // "no-cond-assign"
+
+    // "no-const-assign" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-constant-binary-expression"
+
+    // "no-constant-condition"
+
+    // "no-constructor-return"
+
+    // "no-control-regex"
+
+    // "no-debugger"
+
+    // "no-dupe-args" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-dupe-class-members" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-dupe-else-if"
+
+    // "no-dupe-keys" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-duplicate-case"
+
+    // "no-duplicate-imports"
+
+    // "no-empty-character-class"
+
+    // "no-empty-pattern"
+
+    // "no-ex-assign"
+
+    // "no-fallthrough"
+
+    // "no-func-assign" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-import-assign" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-inner-declarations"
+
+    // "no-invalid-regexp"
+
+    // "no-irregular-whitespace"
+
+    // "no-loss-of-precision"
+
+    // "no-misleading-character-class"
+
+    // "no-new-native-nonconstructor"
+
+    // "no-new-symbol" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-obj-calls" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-promise-executor-return"
+
+    // "no-prototype-builtins"
+
+    // "no-self-assign"
+
+    // "no-self-compare"
+
+    // "no-setter-return"
+
+    // "no-sparse-arrays"
+
+    // "no-template-curly-in-string"
+
+    // "no-redeclare" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-setter-return" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-this-before-super" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-undef" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-unreachable" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "no-unsafe-negation" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+
+    // "valid-typeof" is disabled due to conflicting with the TypeScript compiler:
+    // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
   },
-
-  // "constructor-super" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "for-direction" is enabled here:
-  // https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/errors.js
-
-  // "getter-return" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-async-promise-executor"
-
-  // "no-await-in-loop"
-
-  // "no-class-assign"
-
-  // "no-compare-neg-zero"
-
-  // "no-cond-assign"
-
-  // "no-const-assign" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-constant-binary-expression"
-
-  // "no-constant-condition"
-
-  // "no-constructor-return"
-
-  // "no-control-regex"
-
-  // "no-debugger"
-
-  // "no-dupe-args" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-dupe-class-members" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-dupe-else-if"
-
-  // "no-dupe-keys" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-duplicate-case"
-
-  // "no-duplicate-imports"
-
-  // "no-empty-character-class"
-
-  // "no-empty-pattern"
-
-  // "no-ex-assign"
-
-  // "no-fallthrough"
-
-  // "no-func-assign" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-import-assign" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-inner-declarations"
-
-  // "no-invalid-regexp"
-
-  // "no-irregular-whitespace"
-
-  // "no-loss-of-precision"
-
-  // "no-misleading-character-class"
-
-  // "no-new-native-nonconstructor"
-
-  // "no-new-symbol" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-obj-calls" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-promise-executor-return"
-
-  // "no-prototype-builtins"
-
-  // "no-self-assign"
-
-  // "no-self-compare"
-
-  // "no-setter-return"
-
-  // "no-sparse-arrays"
-
-  // "no-template-curly-in-string"
-
-  // "no-redeclare" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-setter-return" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-this-before-super" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-undef" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-unreachable" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "no-unsafe-negation" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
-
-  // "valid-typeof" is disabled due to conflicting with the TypeScript compiler:
-  // https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
 };
---
+++ packages/eslint-config-isaacscript/base-typescript-eslint.js
@@ -0,0 +1,221 @@
+// This is a shared configuration file for ESLint:
+// https://eslint.org/docs/latest/user-guide/configuring
+// This config only contains modifications to the built-in rules from the "@typescript-eslint"
+// plugin.
+module.exports = {
+  rules: {
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/array-type
+     *
+     * Not defined in the parent configs.
+     *
+     * Prefer the "string[]" syntax over "Array<string>".
+     */
+    "@typescript-eslint/array-type": [
+      "warn",
+      {
+        default: "array-simple",
+      },
+    ],
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/explicit-module-boundary-types
+     *
+     * Not defined in the parent configs.
+     *
+     * Specifying explicit return types can help prevent bugs, but only require it on exported
+     * functions.
+     */
+    "@typescript-eslint/explicit-module-boundary-types": "warn",
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/lines-between-class-members
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/style.js
+     *
+     * Airbnb has "exceptAfterSingleLine" turned off by default. A list of single-line variable
+     * declarations at the top of a class is common in TypeScript.
+     */
+    "@typescript-eslint/lines-between-class-members": [
+      "warn",
+      "always",
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/naming-convention
+     *
+     * Defined at:
+     * https://github.com/iamturns/eslint-config-airbnb-typescript/blob/master/lib/shared.js
+     *
+     * Modify the Airbnb config to allow for a leading underscore, which signifies that it is
+     * temporarily not being used.
+     */
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      // Allow camelCase variables (23.2), PascalCase variables (23.8), and UPPER_CASE variables
+      // (23.10).
+      {
+        selector: "variable",
+        format: ["camelCase", "PascalCase", "UPPER_CASE"],
+        leadingUnderscore: "allow",
+      },
+      // Allow camelCase functions (23.2), and PascalCase functions (23.8).
+      {
+        selector: "function",
+        format: ["camelCase", "PascalCase"],
+        leadingUnderscore: "allow",
+      },
+      // Airbnb recommends PascalCase for classes (23.3), and although Airbnb does not make
+      // TypeScript recommendations, we are assuming this rule would similarly apply to anything
+      // "type like", including interfaces, type aliases, and enums.
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/no-unnecessary-boolean-literal-compare
+     *
+     * Not defined in the parent configs.
+     *
+     * This prevents useless code after refactoring variables to pure booleans.
+     */
+    "@typescript-eslint/no-unnecessary-boolean-literal-compare": "warn",
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/no-unused-vars
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/variables.js
+     *
+     * We want to lint unused arguments (the default is "after-used"). We also want to ignore
+     * arguments/variables that start with an underscore. This matches the behavior of the
+     * TypeScript compiler flag "--noUnusedLocals".
+     */
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        args: "all",
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/no-use-before-define
+     *
+     * Defined at:
+     * https://github.com/airbnb/javascript/blob/master/packages/eslint-config-airbnb-base/rules/variables.js
+     *
+     * This allows code to be structured in a more logical order.
+     */
+    "@typescript-eslint/no-use-before-define": "off",
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/prefer-optional-chain
+     *
+     * Defined at:
+     * https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/strict.ts
+     *
+     * This can modify the type of boolean declarations to "boolean | undefined", which is undesired
+     * in some circumstances:
+     * https://github.com/typescript-eslint/typescript-eslint/issues/5269
+     */
+    "@typescript-eslint/prefer-optional-chain": "off",
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/quotes
+     *
+     * Defined at:
+     * https://github.com/prettier/eslint-config-prettier/blob/main/%40typescript-eslint.js
+     *
+     * In order to forbid unnecessary backticks, we must re-enable the "@typescript-eslint/quotes"
+     * rule as specified in the eslint-config-prettier documentation:
+     * https://github.com/prettier/eslint-config-prettier#enforce-backticks
+     */
+    "@typescript-eslint/quotes": [
+      "warn",
+      "double",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: false,
+      },
+    ],
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/restrict-template-expressions
+     *
+     * Defined at:
+     * https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-requiring-type-checking.ts
+     *
+     * This rule disallows booleans and nulls in template expressions. However, a common use-case of
+     * template strings is to coerce everything to a string.
+     */
+    "@typescript-eslint/restrict-template-expressions": "off",
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/strict-boolean-expressions
+     *
+     * Not defined in the parent configs.
+     *
+     * This rule prevents bugs when refactoring a boolean to a number.
+     */
+    "@typescript-eslint/strict-boolean-expressions": [
+      "warn",
+      {
+        allowString: false,
+        allowNumber: false,
+        allowNullableObject: false,
+        allowNullableBoolean: false,
+        allowNullableString: false,
+        allowNullableNumber: false,
+        allowAny: false,
+      },
+    ],
+
+    /**
+     * Documentation:
+     * https://typescript-eslint.io/rules/switch-exhaustiveness-check/
+     *
+     * Not defined in the parent configs.
+     *
+     * This rule ensures type-safety with switch statements, which can be especially helpful when
+     * values are added or removed from an enum.
+     */
+    "@typescript-eslint/switch-exhaustiveness-check": "warn",
+  },
+
+  overrides: [
+    // Disable some TypeScript-specific rules in JavaScript files.
+    {
+      files: ["*.js", "*.cjs", "*.mjs", "*.jsx"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+        "@typescript-eslint/no-unsafe-argument": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/strict-boolean-expressions": "off",
+        "isaacscript/no-object-any": "off",
+      },
+    },
+  ],
+};
