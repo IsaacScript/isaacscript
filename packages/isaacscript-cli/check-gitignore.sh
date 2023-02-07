ebf4d00f6a02d8fdd393c7d8c22c166334faa6c2
James <5511220+Zamiell@users.noreply.github.com>
2023-02-07 17:39:41 -0500

    fix(cli): script

--- packages/isaacscript-cli/check-gitignore.sh
+++ packages/isaacscript-cli/check-gitignore.sh
@@ -6,17 +6,16 @@ set -e # Exit on any errors
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-LOCAL_GITIGNORE="/tmp/gitignore-template-local"
+LOCAL_GITIGNORE="$DIR/file-templates/dynamic/Node.gitignore"
 REMOTE_GITIGNORE="/tmp/gitignore-template-remote"
 URL="https://raw.githubusercontent.com/github/gitignore/master/Node.gitignore"
 
-cp "$DIR/file-templates/dynamic/Node.gitignore" > "$LOCAL_GITIGNORE"
 curl "$URL" --output "$REMOTE_GITIGNORE" --silent
 set +e
 cmp --silent "$LOCAL_GITIGNORE" "$REMOTE_GITIGNORE"
 UP_TO_DATE=$?
 set -e
-rm -f "$LOCAL_GITIGNORE" "$REMOTE_GITIGNORE"
+rm -f "$REMOTE_GITIGNORE"
 
 if [[ "$UP_TO_DATE" -ne "0" ]]; then
   echo "There is an update for the gitignore template from GitHub:"
