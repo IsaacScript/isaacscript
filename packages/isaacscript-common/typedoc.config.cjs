d0e2ca00c6d606ef6bfabf721de153bd4bcf71be
James <5511220+Zamiell@users.noreply.github.com>
2023-02-09 23:50:39 -0500

    fix(common): lint

--- packages/isaacscript-common/typedoc.config.cjs
+++ packages/isaacscript-common/typedoc.config.cjs
@@ -41,6 +41,7 @@ module.exports = {
     "ImmutableObject",
     "IsaacAPIClassTypeToSerializedType",
     "IsaacAPIClassTypeToType",
+    "ModUpgradedWithFeatures",
     "ReadonlyMapConstructor",
     "ReadonlySetConstructor",
     "Serializable",
