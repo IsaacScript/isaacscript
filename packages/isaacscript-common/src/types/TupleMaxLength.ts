ec07dafafaef26dfdf95454256b9d190cdb9b790
James <5511220+Zamiell@users.noreply.github.com>
2023-02-21 18:25:55 -0500

    chore: updating dependencies

--- packages/isaacscript-common/src/types/TupleMaxLength.ts
+++ packages/isaacscript-common/src/types/TupleMaxLength.ts
@@ -3,7 +3,7 @@ import { NaturalNumbersLessThan } from "./NaturalNumbersLessThan";
 /**
  * Helper type that validates that a tuple does not have a length greater than N - 1.
  *
- * For example, `TupleMaxLength<4>` will allow tuples of size 0, 1, 2, or 3.
+ * For example, `TupleMaxLength<string, 4>` will allow string tuples of size 0, 1, 2, or 3.
  */
 export type TupleMaxLength<T, MaxLength extends number> = (
   | T[]
