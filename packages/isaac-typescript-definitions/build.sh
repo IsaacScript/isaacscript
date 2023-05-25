ffef1800d0c49ff48341a2778a0641b86718ec6c
James <5511220+Zamiell@users.noreply.github.com>
2023-05-25 10:33:17 -0400

    chore: more verbose build.sh

--- packages/isaac-typescript-definitions/build.sh
+++ packages/isaac-typescript-definitions/build.sh
@@ -6,6 +6,8 @@ set -euo pipefail # Exit on errors and undefined variables.
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+echo "Building: $DIR"
+
 # Get the name of the repository:
 # https://stackoverflow.com/questions/23162299/how-to-get-the-last-part-of-dirname-in-bash/23162553
 REPO_NAME="$(basename "$DIR")"
