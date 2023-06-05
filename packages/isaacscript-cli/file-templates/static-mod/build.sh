1f26e38128e79827a7c8f791fbbf9f6757e81087
James <5511220+Zamiell@users.noreply.github.com>
2023-06-05 12:44:08 -0400

    fix(cli): build template

--- packages/isaacscript-cli/file-templates/static-mod/build.sh
+++ packages/isaacscript-cli/file-templates/static-mod/build.sh
@@ -6,8 +6,6 @@ set -euo pipefail # Exit on errors and undefined variables.
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-echo "Building: $DIR"
-
 SECONDS=0
 
 # Convert the TypeScript to a single Lua file (based on the settings in "tsconfig.json").
