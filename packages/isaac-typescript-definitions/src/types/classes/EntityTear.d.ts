01d9b6bae84256c093925edbfac3813a72a58a65
James <5511220+Zamiell@users.noreply.github.com>
2023-07-01 11:10:51 -0400

    chore(defs): update comment

--- packages/isaac-typescript-definitions/src/types/classes/EntityTear.d.ts
+++ packages/isaac-typescript-definitions/src/types/classes/EntityTear.d.ts
@@ -42,8 +42,8 @@ declare global {
      * - In each run, the game keeps track of how many tears have been fired by the player in total.
      * - TearIndex represents this tear counter.
      * - It is 0-indexed, meaning that the first tear fired by the player on a run will have a
-     *   TearIndex of 0, the second tear fired by the player on a run will have a TearIndex of 1,
-     *   and so on.
+     *   `TearIndex` of 0, the second tear fired by the player on a run will have a `TearIndex` of
+     *   1, and so on.
      */
     readonly TearIndex: int;
 
