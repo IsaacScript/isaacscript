6d768edbb5a48eddd6c0487a64607eaf6c0dddad
James <5511220+Zamiell@users.noreply.github.com>
2023-08-28 12:59:42 -0400

    fix(plugin): readme generation

--- packages/eslint-plugin-isaacscript/src/configs/recommended.ts
+++ packages/eslint-plugin-isaacscript/src/configs/recommended.ts
@@ -34,6 +34,7 @@ export const recommended = {
     "isaacscript/no-void-return-type": "error",
     "isaacscript/prefer-plusplus": "error",
     "isaacscript/prefer-postfix-plusplus": "error",
+    "isaacscript/require-break": "error",
     "isaacscript/require-capital-const-assertions": "error",
     "isaacscript/require-capital-read-only": "error",
     "isaacscript/require-unannotated-const-assertions": "error",
