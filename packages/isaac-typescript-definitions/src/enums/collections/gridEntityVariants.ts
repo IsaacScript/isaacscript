6e6a0558dcc01ec7578337fc4b9d3b0586d76e18
James <5511220+Zamiell@users.noreply.github.com>
2023-09-05 14:24:44 -0400

    refactor(defs): proper poop names

--- packages/isaac-typescript-definitions/src/enums/collections/gridEntityVariants.ts
+++ packages/isaac-typescript-definitions/src/enums/collections/gridEntityVariants.ts
@@ -97,10 +97,10 @@ export enum PoopGridEntityVariant {
   RAINBOW = 4,
   BLACK = 5,
   WHITE = 6,
-  GIGA_TOP_LEFT = 7,
-  GIGA_TOP_RIGHT = 8,
-  GIGA_BOTTOM_LEFT = 9,
-  GIGA_BOTTOM_RIGHT = 10,
+  GIANT_TOP_LEFT = 7,
+  GIANT_TOP_RIGHT = 8,
+  GIANT_BOTTOM_LEFT = 9,
+  GIANT_BOTTOM_RIGHT = 10,
   CHARMING = 11,
 }
 
