fde6db4d904c58ceff2941f145b1754d89e2788d
James <5511220+Zamiell@users.noreply.github.com>
2023-09-21 23:09:44 -0400

    docs(defs): document ButtonAction relation to InputHook

--- packages/isaac-typescript-definitions/src/enums/ButtonAction.ts
+++ packages/isaac-typescript-definitions/src/enums/ButtonAction.ts
@@ -1,36 +1,90 @@
 export enum ButtonAction {
+  /** This only fires on `InputHook.GET_ACTION_VALUE` (2). */
   LEFT = 0,
+
+  /** This only fires on `InputHook.GET_ACTION_VALUE` (2). */
   RIGHT = 1,
+
+  /** This only fires on `InputHook.GET_ACTION_VALUE` (2). */
   UP = 2,
+
+  /** This only fires on `InputHook.GET_ACTION_VALUE` (2). */
   DOWN = 3,
+
+  /** This fires on `InputHook.IS_ACTION_PRESSED` (0) and `InputHook.GET_ACTION_VALUE` (2). */
   SHOOT_LEFT = 4,
+
+  /** This fires on `InputHook.IS_ACTION_PRESSED` (0) and `InputHook.GET_ACTION_VALUE` (2). */
   SHOOT_RIGHT = 5,
+
+  /** This fires on `InputHook.IS_ACTION_PRESSED` (0) and `InputHook.GET_ACTION_VALUE` (2). */
   SHOOT_UP = 6,
+
+  /** This fires on `InputHook.IS_ACTION_PRESSED` (0) and `InputHook.GET_ACTION_VALUE` (2). */
   SHOOT_DOWN = 7,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   BOMB = 8,
+
+  /** This fires on `InputHook.IS_ACTION_PRESSED` (0) and `InputHook.IS_ACTION_TRIGGERED` (1). */
   ITEM = 9,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   PILL_CARD = 10,
 
   /**
    * This is also used for switching cards, switching The Forgotten, holding Esau in place, and so
    * on.
+   *
+   * This fires on `InputHook.IS_ACTION_PRESSED` (0) and `InputHook.IS_ACTION_TRIGGERED` (1).
    */
   DROP = 11,
 
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   PAUSE = 12,
+
+  /** This only fires on `InputHook.IS_ACTION_PRESSED` (0). */
   MAP = 13,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   MENU_CONFIRM = 14,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   MENU_BACK = 15,
+
+  /** This only fires on `InputHook.IS_ACTION_PRESSED` (0). */
   RESTART = 16,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   FULLSCREEN = 17,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   MUTE = 18,
+
+  // Does not seem to trigger any input hooks while on keyboard.
   JOIN_MULTIPLAYER = 19,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   MENU_LEFT = 20,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   MENU_RIGHT = 21,
+
+  // Does not seem to trigger any input hooks while on keyboard.
   MENU_UP = 22,
+
+  // Does not seem to trigger any input hooks while on keyboard.
   MENU_DOWN = 23,
+
+  // Does not seem to trigger any input hooks while on keyboard.
   MENU_LT = 24,
+
+  // Does not seem to trigger any input hooks while on keyboard.
   MENU_RT = 25,
+
+  // Does not seem to trigger any input hooks while on keyboard.
   MENU_TAB = 26,
+
+  /** This only fires on `InputHook.IS_ACTION_TRIGGERED` (1). */
   CONSOLE = 28,
 }
