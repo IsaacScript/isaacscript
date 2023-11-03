8e00f28bdb3f20c784b447dae57b20c0fe90263e
James <5511220+Zamiell@users.noreply.github.com>
2023-11-02 22:55:48 -0400

    fix: lint

--- packages/docs/.eslintrc.cjs
+++ packages/docs/.eslintrc.cjs
@@ -26,7 +26,27 @@ const config = {
     "import/no-default-export": "off", // React uses default exports.
     "n/file-extension-in-import": "off", // Docusaurus does not yet use ESM.
     "no-alert": "off",
+
+    // This rule has to be told which "package.json" file that the dependencies are located in. (The
+    // "package.json" file for the "docs" package does not contain any of the actual dependencies.)
+    "import/no-extraneous-dependencies": [
+      "error",
+      {
+        packageDir: REPO_ROOT,
+      },
+    ],
   },
+
+  overrides: [
+    // The "isaacscript-common-node" dependency is used in scripts and should never appear in a
+    // "package.json" file (if it is only used in script files).
+    {
+      files: ["**/scripts/*.{ts,cts,mts}"],
+      rules: {
+        "import/no-extraneous-dependencies": "off",
+      },
+    },
+  ],
 };
 
 module.exports = config;
