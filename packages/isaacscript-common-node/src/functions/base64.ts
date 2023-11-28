06900361a65d788b7cbef3d3b554663de817b220
James <5511220+Zamiell@users.noreply.github.com>
2023-11-28 16:59:08 -0500

    feat(common-node): encodeBase64

--- packages/isaacscript-common-node/src/functions/base64.ts
+++ packages/isaacscript-common-node/src/functions/base64.ts
@@ -3,3 +3,9 @@ export function decodeBase64(base64String: string): string {
   const buffer = Buffer.from(base64String, "base64");
   return buffer.toString("utf8");
 }
+
+/** Helper function to encode a string into a base64-encoded string. */
+export function encodeBase64(string: string): string {
+  const buffer = Buffer.from(string);
+  return buffer.toString("base64");
+}
