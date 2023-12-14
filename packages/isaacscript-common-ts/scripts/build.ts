7ba2ad2c2ecc53750a5573289f819e7a47f3f684
James <5511220+Zamiell@users.noreply.github.com>
2023-12-14 14:51:44 -0500

    chore: output CJS for common-ts

--- packages/isaacscript-common-ts/scripts/build.ts
+++ packages/isaacscript-common-ts/scripts/build.ts
@@ -1,5 +1,6 @@
 import { $s, buildScript } from "isaacscript-common-node";
 
 await buildScript(() => {
-  $s`tsc`;
+  // We use the `unbuild` library to output both ESM and CJS.
+  $s`unbuild`;
 });
