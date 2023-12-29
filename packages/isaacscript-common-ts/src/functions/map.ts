cb5ebbfdaeddb6eefdd39ed4f1a5ea5e76bc4ec6
Zamiell <5511220+Zamiell@users.noreply.github.com>
2023-12-28 23:05:46 -0500

    feat(common-ts): objectToMap

--- packages/isaacscript-common-ts/src/functions/map.ts
+++ packages/isaacscript-common-ts/src/functions/map.ts
@@ -16,3 +16,29 @@ export function mapFind<K, V>(
 
   return undefined;
 }
+
+/**
+ * Helper function to convert an object to a map.
+ *
+ * This is useful when you need to construct a type safe object with the `satisfies` operator, but
+ * then later on you need to query it in a way where you expect the return value to be T or
+ * undefined. In this situation, by converting the object to a map, you can avoid unsafe type
+ * assertions.
+ *
+ * Note that the map values will be inserted in a random order, due to how `pairs` works under the
+ * hood.
+ *
+ * Also see the `objectToReadonlyMap` function.
+ */
+// eslint-disable-next-line isaacscript/no-mutable-return
+export function objectToMap<K extends string | number | symbol, V>(
+  object: Record<K, V>,
+): Map<K, V> {
+  const map = new Map<K, V>();
+
+  for (const [key, value] of Object.entries(object)) {
+    map.set(key as K, value as V);
+  }
+
+  return map;
+}
