7d084700dda7336315add968c197e8fd5a08fc4e
Syntax Sculptor <syntaxsculptor@proton.me>
2024-01-03 21:43:20 -0800

    feat: Temporary MultiShotParams interface

--- packages/isaac-typescript-definitions/src/index.ts
+++ packages/isaac-typescript-definitions/src/index.ts
@@ -96,3 +96,4 @@ export * from "./enums/mods/EncyclopediaItemPoolType";
 export * from "./enums/mods/ModConfigMenuOptionType";
 export * from "./enums/mods/RepentogonEnums";
 export * from "./enums/mods/StageAPIEnums";
+export * from "./types/mods/repentogon/MultiShotParams";
---
+++ packages/isaac-typescript-definitions/src/types/mods/repentogon/MultiShotParams.d.ts
@@ -0,0 +1,5 @@
+export interface RepentogonMultiShotParams {
+  // Will fill this whole interface out later. For now this is here so
+  // `AddCallbackParametersRepentogon` can be finished.
+  temporaryValue: boolean;
+}
