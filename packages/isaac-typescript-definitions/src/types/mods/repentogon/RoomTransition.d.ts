dc6031acc9a1730fbb141096082268b0d0731cb5
Syntax Sculptor <syntaxsculptor@proton.me>
2024-01-05 01:41:27 -0800

    Create RoomTransition.d.ts

---
+++ packages/isaac-typescript-definitions/src/types/mods/repentogon/RoomTransition.d.ts
@@ -0,0 +1,17 @@
+import { RoomTransitionAnim } from "../../../enums/RoomTransitionAnim";
+import { BossID } from "../../../enums/collections/roomSubTypes";
+
+declare global {
+  /**
+   * This class is for REPENTOGON, an exe-hack which expands the modding API.
+   *
+   * @noSelf
+   * @see https://repentogon.com/index.html
+   */
+  namespace RoomTransition {
+    function GetTransitionMode(): RoomTransitionAnim;
+    function GetVersusScreenSprite(): Sprite;
+    function IsRenderingBossIntro(): boolean;
+    function StartBossIntro(boss1: BossID, boss2: BossID): void;
+  }
+}
