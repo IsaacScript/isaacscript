df6bc5c0db9efafe5346c69b7d18e887973379e8
James <5511220+Zamiell@users.noreply.github.com>
2024-03-12 22:18:07 -0400

    Update EntityConfig.d.ts

--- packages/isaac-typescript-definitions/src/types/repentogon/entity-config/EntityConfig.d.ts
+++ packages/isaac-typescript-definitions/src/types/repentogon/entity-config/EntityConfig.d.ts
@@ -13,19 +13,19 @@ declare global {
    */
   namespace EntityConfig {
     /**
-     * Returns the EntityConfigBaby from the provided BabySubType.
+     * Returns the `EntityConfigBaby` from the provided `BabySubType`.
      *
      * Returns undefined if there is no co-op baby with the given ID.
      */
     function GetBaby(babySubType: BabySubType): EntityConfigBaby | undefined;
 
     /**
-     * Returns the EntityConfigEntity from the provided EntityType.
+     * Returns the `EntityConfigEntity` from the provided `EntityType`.
      *
      * Providing the variant and subtype is optional. If a non-existent variant/subtype is
      * requested, the base version of that entity is returned instead.
      *
-     * Returns undefined if there is no entity from the provided EntityType.
+     * Returns undefined if there is no entity from the provided `EntityType`.
      *
      * @param entityType
      * @param variant Default is -1.
@@ -37,16 +37,16 @@ declare global {
       subType?: int,
     ): EntityConfigEntity | undefined;
 
-    /** Returns the highest BabySubType currently assigned to a valid co-op baby. */
+    /** Returns the highest `BabySubType` currently assigned to a valid co-op baby. */
     function GetMaxBabyID(): BabySubType;
 
-    /** Returns the highest PlayerType assigned to a valid player. */
+    /** Returns the highest `PlayerType` assigned to a valid player. */
     function GetMaxPlayerType(): PlayerType;
 
     /**
-     * Returns the EntityConfigPlayer from the provided Player.
+     * Returns the `EntityConfigPlayer` from the provided Player.
      *
-     * Returns undefined if there is no player with the given PlayerType.
+     * Returns undefined if there is no player with the given `PlayerType`.
      */
     function GetPlayer(playerType: PlayerType): EntityConfigPlayer | undefined;
   }
