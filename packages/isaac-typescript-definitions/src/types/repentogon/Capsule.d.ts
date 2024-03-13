24849aa74c537a6364f2cb251d43a3a54f2c6d6e
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-03-12 20:38:18 -0700

    feat: Capsule (#79)

---
+++ packages/isaac-typescript-definitions/src/types/repentogon/Capsule.d.ts
@@ -0,0 +1,43 @@
+/**
+ * Constructs a new Capsule object.
+ *
+ * Capsules are used by the game for collision detection. All entities have a collision capsule
+ * whose shape is defined by their XML markup. Additionally, you can use a null layer frame's size
+ * and position in a sprite's animation to generate a null capsule.
+ *
+ * Null layers are added in the sprite's .anm2 file by setting the type to "Null" when adding a
+ * layer. The size and relative position of the null layer is dependant on the position and scale of
+ * the frame.
+ *
+ * You can use `Entity.GetNullCapsule` to generate a capsule of the current frame in a specified
+ * null layer.
+ */
+export function Capsule(
+  this: void,
+  position: Vector,
+  sizeMultiplier: Vector,
+  rotation: number,
+  size: number,
+): Capsule;
+
+export function Capsule(
+  position: Vector,
+  targetPosition: Vector,
+  size: number,
+): Capsule;
+
+declare interface Capsule extends IsaacAPIClass {
+  Collide: (capsule: Capsule, point: Vector) => boolean;
+
+  GetDirection: () => Vector;
+
+  GetF1: () => number;
+
+  GetF2: () => number;
+
+  GetPosition: () => Vector;
+
+  GetVec2: () => Vector;
+
+  GetVec3: () => Vector;
+}
