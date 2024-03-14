0ce879381502d614419ace720e58bd09bc61181d
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-03-14 07:55:06 -0700

    ColorParams (#82)
    * feat: ColorParams
    
    * Update ColorParams.d.ts
    
    ---------
    
    Co-authored-by: James <5511220+Zamiell@users.noreply.github.com>

---
+++ packages/isaac-typescript-definitions-repentogon/src/types/ColorParams.d.ts
@@ -0,0 +1,50 @@
+export interface ColorParams extends IsaacAPIClass {
+  /** Returns the current color. */
+  GetColor: () => Color;
+
+  /** Returns the duration (in frames) over which the color change should last. */
+  GetDuration: () => int;
+
+  /** Returns whether the color fades out. */
+  GetFadeout: () => boolean;
+
+  /**
+   * Returns the remaining frames before the color change is complete. This decreases by 1 for each
+   * non-interpolation update, at 30 updates per second.
+   */
+  GetLifespan: () => int;
+
+  /** Returns the display priority for the color change. */
+  GetPriority: () => int;
+
+  /** Returns whether the color parameters are shared with the entity's child. */
+  GetShared: () => boolean;
+
+  /** Sets the color. */
+  SetColor: (color: Color) => void;
+
+  /** Sets the duration (in frames) for the color change. */
+  SetDuration: (duration: int) => void;
+
+  /** Sets whether or not the color fades out. */
+  SetFadeout: (isFading: boolean) => void;
+
+  /** Sets the remaining frames before the color change is complete. */
+  SetLifespan: (duration: int) => void;
+
+  /** Sets the display priority of the color change. */
+  SetPriority: (priority: int) => void;
+
+  /** Sets whether the `ColorParams` is shared with the entity's child. */
+  SetShared: (shared: boolean) => void;
+}
+
+export function ColorParams(
+  this: void,
+  color: Color,
+  priority: int,
+  duration1: int,
+  duration2: int,
+  fadeout: boolean,
+  shared: boolean,
+): ColorParams;
