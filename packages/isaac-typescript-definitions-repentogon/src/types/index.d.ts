4314086d4ec83955458278f3977d0d92c87ee232
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-15 17:24:22 -0700

    chore: Reordered index.d.ts alphabetically

--- packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
@@ -25,7 +25,7 @@
 /// <reference path="./classes/hud/PlayerHUD.d.ts" />
 /// <reference path="./classes/hud/PlayerHUDHeart.d.ts" />
 /// <reference path="./classes/hud/ScoreSheet.d.ts" />
-/// <reference path = "./classes/ImGui.d.ts"/>
+/// <reference path= "./classes/ImGui.d.ts"/>
 /// <reference path="./classes/ItemOverlay.d.ts" />
 /// <reference path="./classes/level-generator/LevelGeneratorRoom.d.ts" />
 /// <reference path="./classes/level-generator/LevelGeneratorEntry.d.ts" />
@@ -49,12 +49,12 @@
 /// <reference path="./classes/menus/TitleMenu.d.ts" />
 /// <reference path="./classes/MultiShotParams.d.ts" />
 /// <reference path="./classes/NightmareScene.d.ts" />
-/// <reference path="./classes/PersistentGameData.d.ts" />
-/// <reference path="./classes/PlayerManager.d.ts" />
-/// <reference path="./classes/PocketItem.d.ts" />
 /// <reference path="./classes/procedural-items/ProceduralEffect.d.ts"/>
 /// <reference path="./classes/procedural-items/ProceduralItem.d.ts"/>
 /// <reference path="./classes/procedural-items/ProceduralItemManager.d.ts"/>
+/// <reference path="./classes/PersistentGameData.d.ts" />
+/// <reference path="./classes/PlayerManager.d.ts" />
+/// <reference path="./classes/PocketItem.d.ts" />
 
 // ----------
 // Unofficial
