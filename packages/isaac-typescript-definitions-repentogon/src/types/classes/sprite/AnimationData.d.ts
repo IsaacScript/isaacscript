693fa8c2c28dd6a45a97c720ac59341c96559e5a
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-16 12:42:29 -0700

    feat: AnimationData

---
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/sprite/AnimationData.d.ts
@@ -0,0 +1,24 @@
+/**
+ * This class is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+declare interface AnimationData {
+  /**
+   * Returns an array of all of the animation layers. The array is ordered from bottom to top, not
+   * by layer ID.
+   */
+  GetAllLayers: () => AnimationLayer[];
+
+  /** Returns an `AnimationLayer` from the specified ID. Returns undefined if the ID is invalid. */
+  GetLayer: (layerID: int) => AnimationLayer | undefined;
+
+  /** Returns the number of frames in the animation. */
+  GetLength: () => int;
+
+  /** Returns the name of the animation. */
+  GetName: () => string;
+
+  /** Returns whether the animation is set to loop. */
+  IsLoopingAnimation: () => boolean;
+}
