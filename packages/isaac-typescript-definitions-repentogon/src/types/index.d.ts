3545f174b101748c0e369f5762f8c8e32978e65b
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-17 21:41:34 -0700

    feat: EntityFamiliar

--- packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
@@ -17,6 +17,7 @@
 /// <reference path="./classes/entity/EntityBomb.d.ts"/>
 /// <reference path="./classes/entity/EntityDelirium.d.ts"/>
 /// <reference path="./classes/entity/EntityEffect.d.ts"/>
+/// <reference path="./classes/entity/EntityFamiliar.d.ts"/>
 /// <reference path="./classes/entity-config/EntityConfig.d.ts" />
 /// <reference path="./classes/entity-config/EntityConfigBaby.d.ts" />
 /// <reference path="./classes/entity-config/EntityConfigEntity.d.ts" />
