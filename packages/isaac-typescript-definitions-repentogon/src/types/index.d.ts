d32f5b310142cc5ca5251ee503afc40ef54994cd
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-03-18 11:06:46 -0700

    feat: EntityPickup

--- packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
@@ -21,6 +21,7 @@
 /// <reference path="./classes/entity/EntityKnife.d.ts"/>
 /// <reference path="./classes/entity/EntityLaser.d.ts"/>
 /// <reference path="./classes/entity/EntityNPC.d.ts"/>
+/// <reference path="./classes/entity/EntityPickup.d.ts"/>
 /// <reference path="./classes/entity/EntityPlayer.d.ts"/>
 /// <reference path="./classes/entity-config/EntityConfig.d.ts" />
 /// <reference path="./classes/entity-config/EntityConfigBaby.d.ts" />
