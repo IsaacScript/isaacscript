b75df7144d165a10443ecaeac0b0c8246cab63f0
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-21 07:34:37 -0700

    Update EntityTear.d.ts

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/entity/EntityTear.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/entity/EntityTear.d.ts
@@ -3,18 +3,16 @@
  *
  * @see https://repentogon.com/index.html
  */
-declare global {
-  interface EntityTear extends Entity {
-    /**
-     * Returns the intensity value of the tear as a result of the Dead Eye Collectible. The
-     * intensity is between 0 and 1.
-     */
-    GetDeadEyeIntensity: () => float;
+declare interface EntityTear extends Entity {
+  /**
+   * Returns the intensity value of the tear as a result of the Dead Eye Collectible. The intensity
+   * is between 0 and 1.
+   */
+  GetDeadEyeIntensity: () => float;
 
-    /**
-     * Copies the tear and applies the Multidimensional Baby tear effect to it. Returns the copied
-     * tear.
-     */
-    MakeMultidimensionalCopy: () => EntityTear;
-  }
+  /**
+   * Copies the tear and applies the Multidimensional Baby tear effect to it. Returns the copied
+   * tear.
+   */
+  MakeMultidimensionalCopy: () => EntityTear;
 }
