1629fbc3c0eaf2c0c7de78b2e39b415d49f82222
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-22 08:58:19 -0700

    feat: MusicManager

---
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/MusicManager.d.ts
@@ -0,0 +1,19 @@
+import type { Music } from "isaac-typescript-definitions";
+
+declare global {
+  function MusicManager(this: void): MusicManager;
+
+  interface MusicManager extends IsaacAPIClass {
+    /** Returns the current pitch of the music. */
+    GetCurrentPitch: () => number;
+
+    /** Plays a jingle. */
+    PlayJingle: (jingle: Music) => void;
+
+    /** Sets the pitch of the music. */
+    SetCurrentPitch: (pitch: float) => void;
+
+    /** Stops the currently playing jingle. */
+    StopJingle: () => void;
+  }
+}
