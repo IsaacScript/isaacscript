e63afafb4f0ca28fb103b6858d3ca824830b4e20
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-22 09:37:49 -0700

    feat: RNG

--- packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
@@ -90,6 +90,7 @@
 /// <reference path="./classes/room-config/RoomConfigHolder.d.ts"/>
 /// <reference path="./classes/PosVel.d.ts"/>
 /// <reference path="./classes/RailManager.d.ts"/>
+/// <reference path="./classes/RNG.d.ts"/>
 /// <reference path="./classes/RoomTransition.d.ts"/>
 /// <reference path="./classes/sprite/AnimationData.d.ts"/>
 /// <reference path="./classes/sprite/AnimationFrame.d.ts"/>
