e2c749e01b9392acf7f112e76bca54b61d457e19
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-22 16:48:09 -0700

    feat: RoomConfigRoom

--- packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/index.d.ts
@@ -89,6 +89,7 @@
 /// <reference path="./classes/room-config/EntitiesSaveStateVector.d.ts"/>
 /// <reference path="./classes/room-config/GridEntitiesSaveStateVector.d.ts"/>
 /// <reference path="./classes/room-config/RoomConfigHolder.d.ts"/>
+/// <reference path="./classes/room-config/RoomConfigHolder.d.ts"/>
 /// <reference path="./classes/PosVel.d.ts"/>
 /// <reference path="./classes/RailManager.d.ts"/>
 /// <reference path="./classes/RNG.d.ts"/>
