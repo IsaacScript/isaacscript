347aafa2e61b1031412315e1c3bb6b486a4dc5a1
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-22 16:52:08 -0700

    feat: RoomDescriptor & RoomConfig

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/room-config/RoomConfigRoom.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/room-config/RoomConfigRoom.d.ts
@@ -1,8 +1,3 @@
-/**
- * This class is for REPENTOGON, an exe-hack which expands the modding API.
- *
- * @see https://repentogon.com/index.html
- */
 declare interface RoomConfigRoom extends IsaacAPIClass {
   Mode: int;
 }
