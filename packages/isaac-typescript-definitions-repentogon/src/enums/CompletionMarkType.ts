c3f92a6eb63c2399de44a1dc5f255e9c193c6c7c
Syntax Sculptor <syntaxsculptor@proton.me>
2024-03-22 15:40:26 -0700

    feat: Isaac definitions

---
+++ packages/isaac-typescript-definitions-repentogon/src/enums/CompletionMarkType.ts
@@ -0,0 +1,20 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+export enum CompletionMarkType {
+  MOMS_HEART = 0,
+  ISAAC = 1,
+  SATAN = 2,
+  BOSS_RUSH = 3,
+  BLUE_BABY = 4,
+  LAMB = 5,
+  MEGA_SATAN = 6,
+  ULTRA_GREED = 7,
+  HUSH = 9,
+  ULTRA_GREEDIER = 11,
+  DELIRIUM = 12,
+  MOTHER = 13,
+  BEAST = 14,
+}
---
+++ packages/isaac-typescript-definitions-repentogon/src/enums/DialogReturn.ts
@@ -0,0 +1,16 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+export enum DialogReturn {
+  PL = 1,
+  CANCEL = 2,
+  ABORT = 3,
+  RETRY = 4,
+  IGNORE = 5,
+  YES = 6,
+  NO = 7,
+  TRY_AGAIN = 10,
+  CONTINUE = 11,
+}
