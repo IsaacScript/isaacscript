8cd09ffffe75eb30af62107b9e443f951e304e76
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-04-30 14:20:13 -0400

    chore: upgrade some selective deps

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -19,7 +19,6 @@ import {
 } from "isaacscript-common-node";
 import { isEnumValue } from "isaacscript-common-ts";
 import path from "node:path";
-import { updateIsaacScriptMonorepo } from "./update.js";
 
 enum VersionBump {
   major = "major",
@@ -137,8 +136,8 @@ console.log(
 );
 
 // Finally, check for dependency updates to ensure that we keep the monorepo up to date.
-console.log("Checking for monorepo updates...");
-updateIsaacScriptMonorepo();
+/// console.log("Checking for monorepo updates...");
+/// updateIsaacScriptMonorepo();
 
 if (!isGitRepositoryClean(REPO_ROOT)) {
   const gitCommitMessage = "chore: updating dependencies";
