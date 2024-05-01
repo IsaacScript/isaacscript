405db8021f488296cbfc7a2957bddaabd99a5dac
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-05-01 14:10:46 -0700

    fix: PersistentGameData definitions

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/PersistentGameData.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/PersistentGameData.d.ts
@@ -6,53 +6,49 @@ import type {
 import type { Achievement } from "../../enums/Achievement";
 import type { EventCounter } from "../../enums/EventCounter";
 
-/**
- * It is not safe calling the methods of this class until the game fully loads. It's best to only
- * call them inside of callbacks.
- *
- * This class is for REPENTOGON, an exe-hack which expands the modding API.
- *
- * @see https://repentogon.com/index.html
- * @noSelf
- */
 declare global {
-  namespace PersistentGameData {
+  /**
+   * It is not safe calling the methods of this class until the game fully loads. It's best to only
+   * call them inside of callbacks.
+   *
+   * This class is for REPENTOGON, an exe-hack which expands the modding API.
+   *
+   * @see https://repentogon.com/index.html
+   */
+  interface PersistentGameData {
     /** Returns the number of times a specific entity has been killed, according to the bestiary. */
-    function GetBestiaryDeathCount(entityType: EntityType, variant: int): int;
+    GetBestiaryDeathCount: (entityType: EntityType, variant: int) => int;
 
     /**
      * Returns the number of times a specific entity has been encountered, according to the
      * bestiary.
      */
-    function GetBestiaryEncounterCount(
-      entityType: EntityType,
-      variant: int,
-    ): int;
+    GetBestiaryEncounterCount: (entityType: EntityType, variant: int) => int;
 
     /**
      * Returns the number of times a specific entity has killed a player, according to the bestiary.
      */
-    function GetBestiaryKillCount(entityType: EntityType, variant: int): int;
+    GetBestiaryKillCount: (entityType: EntityType, variant: int) => int;
 
     /** Returns the current value of the specified persistent event counter. */
-    function GetEventCounter(event: EventCounter): int;
+    GetEventCounter: (event: EventCounter) => int;
 
     /** Increases the value of a specified persistent event counter by the provided amount. */
-    function IncreaseEventCounter(event: EventCounter, count: int): void;
+    IncreaseEventCounter: (event: EventCounter, count: int) => void;
 
     /** Returns whether the provided `Challenge` has been completed. */
-    function IsChallengeComplete(challenge: Challenge): boolean;
+    IsChallengeComplete: (challenge: Challenge) => boolean;
 
     /** Returns whether the provided collectible is in the items page in the stats menu. */
-    function IsItemInCollection(collectible: CollectibleType): void;
+    IsItemInCollection: (collectible: CollectibleType) => void;
 
     /**
      * Tries to unlock the provided `Achievement`. Returns whether the achievement was unlocked
      * successfully.
      */
-    function TryUnlock(unlock: Achievement): boolean;
+    TryUnlock: (unlock: Achievement) => boolean;
 
     /** Returns whether the provided `Achievement` is unlocked. */
-    function Unlocked(unlock: Achievement): boolean;
+    Unlocked: (unlock: Achievement) => boolean;
   }
 }
