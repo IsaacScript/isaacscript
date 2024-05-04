d097d4f9fc1efd3fe4fb3e8c568cb758fbe4de1f
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-05-03 18:55:03 -0700

    fix: Color present constants

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/Color.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/Color.d.ts
@@ -37,7 +37,10 @@ declare interface Color extends IsaacAPIClass {
 
   /** Returns a string representation of the color. */
   Print: () => string;
+}
 
+/** @noSelf */
+declare namespace Color {
   // Unlike the vanilla API where constants like `Vector.Zero` can be overwritten, using these
   // constants is safe as Repentogon prevents these values from being changed by other mods.
 
@@ -45,122 +48,122 @@ declare interface Color extends IsaacAPIClass {
    * Used by enemies like Crackles and Coal Spiders. This color has a hardcoded special property;
    * gibs start orange and fade into grey.
    */
-  EmberFade: Color;
+  const EmberFade: Color;
 
   /** Used by lasers with the Almond Milk effect. */
-  LaserAlmond: Color;
+  const LaserAlmond: Color;
 
   /** Used by lasers with the Chocolate Milk effect. */
-  LaserChocolate: Color;
+  const LaserChocolate: Color;
 
   /** Used by lasers with the Lump of Coal effect. */
-  LaserCoal: Color;
+  const LaserCoal: Color;
 
   /** Used by lasers with the Fire Mind effect. */
-  LaserFireMind: Color;
+  const LaserFireMind: Color;
 
   /** Used by homing lasers. */
-  LaserHoming: Color;
+  const LaserHoming: Color;
 
   /** Used by lasers with the Ipecac effect. */
-  LaserIpecac: Color;
+  const LaserIpecac: Color;
 
   /** Used by Mother's mega laser. */
-  LaserMother: Color;
+  const LaserMother: Color;
 
   /** Used by lasers with the Number One effect. */
-  LaserNumberOne: Color;
+  const LaserNumberOne: Color;
 
   /** Used by lasers with the Soy Milk effect. */
-  LaserSoy: Color;
+  const LaserSoy: Color;
 
   /** Used by poisonous lasers. */
-  LaserPoison: Color;
+  const LaserPoison: Color;
 
   /** Used by puke projectiles fired by The Cage. */
-  ProjectileCageBlue: Color;
+  const ProjectileCageBlue: Color;
 
   /** Used by clustered projectiles fired in Corpse by enemies such as Mother. */
-  ProjectileCorpseClusterDark: Color;
+  const ProjectileCorpseClusterDark: Color;
 
   /** Used by clustered projectiles fired in Corpse by enemies such as Mother. */
-  ProjectileCorpseClusterLight: Color;
+  const ProjectileCorpseClusterLight: Color;
 
   /**
    * Used by projectiles fired in Corpse by enemies such as Mother. Also used for the green laser
    * fired by Chimera.
    */
-  ProjectileCorpseGreen: Color;
+  const ProjectileCorpseGreen: Color;
 
   /** Used by pink-ish white-ish projectiles fired in Corpse by enemies such as Mother. */
-  ProjectileCorpsePink: Color;
+  const ProjectileCorpsePink: Color;
 
   /** Used by white-ish grey-ish projectiles fired in Corpse by enemies such as Mother. */
-  ProjectileCorpseWhite: Color;
+  const ProjectileCorpseWhite: Color;
 
   /** Used by yellow projectiles fired in Corpse by enemies such as The Scourge. */
-  ProjectileCorpseYellow: Color;
+  const ProjectileCorpseYellow: Color;
 
   /** Used by projectiles which spawn fire waves fired by enemies like Crackle. */
-  ProjectileFireWave: Color;
+  const ProjectileFireWave: Color;
 
   /** Used by homing projectiles fired by enemies such as Psychic Maw. */
-  ProjectileHoming: Color;
+  const ProjectileHoming: Color;
 
   /** Used by the blue projectiles fired by Hush. */
-  ProjectileHushBlue: Color;
+  const ProjectileHushBlue: Color;
 
   /** Used by the green projectiles fired by Hush. */
-  ProjectileHushGreen: Color;
+  const ProjectileHushGreen: Color;
 
   /** Used by the yellow projectiles fired by Hush. */
-  ProjectileHushYellow: Color;
+  const ProjectileHushYellow: Color;
 
   /** Used by explosive projectiles fired by enemies such as Gurgles. */
-  ProjectileIpecac: Color;
+  const ProjectileIpecac: Color;
 
   /** Used by black projectiles fired by Mega Satan. */
-  ProjectileMegaSatanBlack: Color;
+  const ProjectileMegaSatanBlack: Color;
 
   /** Used by white projectiles fired by Mega Satan. */
-  ProjectileMegaSatanWhite: Color;
+  const ProjectileMegaSatanWhite: Color;
 
   /** Used by soy projectiles fired by enemies like Soy Creep. */
-  ProjectileSoy: Color;
+  const ProjectileSoy: Color;
 
   /** Used by tar projectiles fired by enemies like Clot. */
-  ProjectileTar: Color;
+  const ProjectileTar: Color;
 
   /** Used for tears fired by players with Almond Milk. */
-  TearAlmond: Color;
+  const TearAlmond: Color;
 
   /** Used for tears fired by players with Chocolate Milk. */
-  TearChocolate: Color;
+  const TearChocolate: Color;
 
   /** Used for tears fired by players with Lump of Coal. */
-  TearCoal: Color;
+  const TearCoal: Color;
 
   /** Used for tears fired by players with Common Cold. */
-  TearCommonCold: Color;
+  const TearCommonCold: Color;
 
   /** Used for homing tears fired by players with Spoon Bender. */
-  TearHoming: Color;
+  const TearHoming: Color;
 
   /** Used for explosive tears fired by players with Ipecac. */
-  TearIpecac: Color;
+  const TearIpecac: Color;
 
   /** Used for tears fired by players with Number One. */
-  TearNumberOne: Color;
+  const TearNumberOne: Color;
 
   /** Used for poison tears fired by players with Scorpio. */
-  TearScorpio: Color;
+  const TearScorpio: Color;
 
   /** Used for poison tears fired by players with Serpents Kiss. */
-  TearSerpentsKiss: Color;
+  const TearSerpentsKiss: Color;
 
   /** Used for tears fired by players with Soy Milk. */
-  TearSoy: Color;
+  const TearSoy: Color;
 
   /** Used for tears fired by familiars such as Little Gish. */
-  TearTar: Color;
+  const TearTar: Color;
 }
