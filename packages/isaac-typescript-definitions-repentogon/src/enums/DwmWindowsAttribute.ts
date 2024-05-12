6cb17aa35e321f41e21c6b52481d37c01bff1a8e
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-05-12 08:23:44 -0700

    fix: missing doc comments

--- packages/isaac-typescript-definitions-repentogon/src/enums/DwmWindowsAttribute.ts
+++ packages/isaac-typescript-definitions-repentogon/src/enums/DwmWindowsAttribute.ts
@@ -1,3 +1,8 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
 export enum DwmWindowAttribute {
   NC_RENDERING_ENABLED = 1,
   NC_RENDERING_POLICY = 2,
--- packages/isaac-typescript-definitions-repentogon/src/enums/WindowIcon.ts
+++ packages/isaac-typescript-definitions-repentogon/src/enums/WindowIcon.ts
@@ -1,3 +1,8 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
 export enum WindowIcon {
   NORMAL = 0,
   TAINTED = 1,
