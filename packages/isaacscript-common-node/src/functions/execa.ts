d470078c15ea88861c23707398898473681a2b10
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-05-13 09:42:21 -0400

    refactor(common-node): more explicit errors

--- packages/isaacscript-common-node/src/functions/execa.ts
+++ packages/isaacscript-common-node/src/functions/execa.ts
@@ -30,7 +30,7 @@ export async function $(
 
   try {
     returnBase = await dollarSignFuncWithOptions(templates, ...expressions);
-  } catch (error) {
+  } catch (error: unknown) {
     const execaSyncError = error as ExecaSyncError;
     process.exit(execaSyncError.exitCode);
   }
@@ -93,7 +93,7 @@ export function $s(
 
   try {
     returnBase = dollarSignFuncWithOptions.sync(templates, ...expressions);
-  } catch (error) {
+  } catch (error: unknown) {
     const execaSyncError = error as ExecaSyncError;
     process.exit(execaSyncError.exitCode);
   }
