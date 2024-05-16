d2fb84e5d7c227d43afec8950b7289cfb9abde21
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-05-16 11:50:33 -0400

    fix(rep): build

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/hud/PlayerHUDHeart.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/hud/PlayerHUDHeart.d.ts
@@ -1,22 +1,24 @@
 import type { HeartFlashType } from "../../../enums/HeartFlashType";
 
-/**
- * This class is for REPENTOGON, an exe-hack which expands the modding API.
- *
- * @see https://repentogon.com/index.html
- */
-declare interface PlayerHUDHeart extends IsaacAPIClass {
-  GetFlashType: () => HeartFlashType;
+declare global {
+  /**
+   * This class is for REPENTOGON, an exe-hack which expands the modding API.
+   *
+   * @see https://repentogon.com/index.html
+   */
+  interface PlayerHUDHeart extends IsaacAPIClass {
+    GetFlashType: () => HeartFlashType;
 
-  /** Returns the name of the animation currently playing on the heart. */
-  GetHeartAnim: () => string;
+    /** Returns the name of the animation currently playing on the heart. */
+    GetHeartAnim: () => string;
 
-  /** Returns the name of the overlay animation currently playing on the heart. */
-  GetHeartOverlayAnim: () => string;
+    /** Returns the name of the overlay animation currently playing on the heart. */
+    GetHeartOverlayAnim: () => string;
 
-  /** Returns whether the golden heart overlay is visible on the heart. */
-  IsGoldenHeartOverlayVisible: () => boolean;
+    /** Returns whether the golden heart overlay is visible on the heart. */
+    IsGoldenHeartOverlayVisible: () => boolean;
 
-  /** Returns whether the heart is visible. */
-  IsVisible: () => boolean;
+    /** Returns whether the heart is visible. */
+    IsVisible: () => boolean;
+  }
 }
