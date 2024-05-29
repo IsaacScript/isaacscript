b69df3f82ba5834f0e95de87612902e37c577ae9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-05-29 12:26:24 -0400

    fix: globs

--- packages/eslint-config-isaacscript/configs/eslint-plugin-unicorn.d.ts
+++
@@ -1,7 +0,0 @@
-import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
-
-// https://github.com/sindresorhus/eslint-plugin-unicorn/issues/2324
-declare module "eslint-plugin-unicorn" {
-  const ESLintPlugin: FlatConfig.Plugin;
-  export default ESLintPlugin;
-}
