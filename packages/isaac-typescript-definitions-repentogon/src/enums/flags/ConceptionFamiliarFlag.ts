f3ef75ec5e5a1d45940cfe064c54cabb7b9c2a36
SyntaxSculptor <syntaxsculptor@proton.me>
2024-06-29 19:57:31 -0800

    Update ConceptionFamiliarFlag.ts

--- packages/isaac-typescript-definitions-repentogon/src/enums/flags/ConceptionFamiliarFlag.ts
+++ packages/isaac-typescript-definitions-repentogon/src/enums/flags/ConceptionFamiliarFlag.ts
@@ -9,7 +9,21 @@
  * @notExported
  * @rename ConceptionFamiliarFlag
  */
-const ConceptionFamiliarFlagInternal = {} as const;
+const ConceptionFamiliarFlagInternal = {
+  LEECH: 1 << 0,
+  DARK_BUM: 1 << 1,
+  DEMON_BABY: 1 << 2,
+  INCUBUS: 1 << 3,
+  LIL_BRIMSTONE: 1 << 4,
+  SUCCUBUS: 1 << 5,
+  GUARDIAN_ANGEL: 1 << 6,
+  HOLY_WATER: 1 << 7,
+  RELIC: 1 << 8,
+  SWORD_PROTECTOR: 1 << 9,
+  SERAPHIM: 1 << 10,
+  LIL_ABADDON: 1 << 11,
+  TWISTED_PAIR: 1 << 12,
+} as const;
 
 type ConceptionFamiliarFlagValue = BitFlag & {
   readonly __conceptionFamiliarFlagBrand: symbol;
