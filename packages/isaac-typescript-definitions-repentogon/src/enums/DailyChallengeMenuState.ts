bbfc3110331795e9e16ec387ecf9f62eed974a3d
SyntaxSculptor <syntaxsculptor@proton.me>
2024-06-30 13:22:03 -0800

    feat: DailyChallengeMenu defs

---
+++ packages/isaac-typescript-definitions-repentogon/src/enums/DailyChallengeMenuState.ts
@@ -0,0 +1,10 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+export enum DailyChallengeMenuState {
+  NOT_LOADED = 0,
+  LOADING = 1,
+  LOADED = 2,
+}
