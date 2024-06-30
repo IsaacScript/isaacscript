95f0db08155c233037938dbb2c81cc66ab40111a
SyntaxSculptor <syntaxsculptor@proton.me>
2024-06-29 20:30:05 -0800

    feat: New Callbacks

---
+++ packages/isaac-typescript-definitions-repentogon/src/enums/PillCardSlot.ts
@@ -0,0 +1,11 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+export enum PillCardSlot {
+  PRIMARY = 0,
+  SECONDARY = 1,
+  TERTIARY = 2,
+  QUATERNARY = 3,
+}
