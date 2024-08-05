5a6724d707d4a360161ea52603d126eb791313d9
syntax-sculptor <syntaxsculptor@proton.me>
2024-08-04 21:26:34 -0800

    feat: GenericPrompt

---
+++ packages/isaac-typescript-definitions-repentogon/src/enums/GenericPromptSelection.ts
@@ -0,0 +1,9 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+export enum GenericPromptSelection {
+  NO = 0,
+  YES = 1,
+}
---
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/GenericPrompt.d.ts
@@ -0,0 +1,47 @@
+import type { GenericPromptSelection } from "../../enums/GenericPromptSelection";
+import type { GenericPromptSubmittedSelection } from "../../enums/GenericPromptSubmittedSelection";
+
+/**
+ * Constructs a new `GenericPrompt` class.
+ *
+ * This class is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+declare function GenericPrompt(this: void): GenericPrompt;
+
+/**
+ * This class is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+declare interface GenericPrompt {
+  GetCurrentSelection: () => GenericPromptSelection;
+  GetSprite: () => Sprite;
+  GetSubmittedSelection: () => GenericPromptSubmittedSelection;
+
+  /** @param smallPrompt Optional. Default is false. */
+  Initialize: (smallPrompt?: boolean) => void;
+
+  IsActive: () => boolean;
+  Render: () => void;
+  SetImageToVictoryRun: () => void;
+
+  /**
+   * @param line1 Optional. Default is an empty string.
+   * @param line2 Optional. Default is an empty string.
+   * @param line3 Optional. Default is an empty string.
+   * @param line4 Optional. Default is an empty string.
+   * @param line5 Optional. Default is an empty string.
+   */
+  SetText: (
+    line1?: string,
+    line2?: string,
+    line3?: string,
+    line4?: string,
+    line5?: string,
+  ) => void;
+
+  Show: () => void;
+  Update: (processInput: boolean) => void;
+}
