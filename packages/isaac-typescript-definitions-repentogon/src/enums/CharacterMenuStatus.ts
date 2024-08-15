8a4037bb18d52ad958e94b0cf661079a7e9b1cd1
Syntax Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2024-08-15 21:51:26 +0000

    REPENTOGON Version 1.0.11c (#115)
    * feat: CharacterMenuStatus
    
    * docs: GenericPrompt example

---
+++ packages/isaac-typescript-definitions-repentogon/src/enums/CharacterMenuStatus.ts
@@ -0,0 +1,12 @@
+/**
+ * This enum is for REPENTOGON, an exe-hack which expands the modding API.
+ *
+ * @see https://repentogon.com/index.html
+ */
+export enum CharacterMenuStatus {
+  DEFAULT = 0,
+  SEED = 1,
+  RANDOM_CHARACTER_SELECTION = 2,
+  FADEOUT = 3,
+  CHARACTER_PAPER_SWAP = 4,
+}
