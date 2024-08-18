f685048427048c1da73a9dbfe8c7c4e21f9f322d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-08-18 14:03:27 -0400

    fix: lint

--- packages/isaacscript-common/src/types/HasFunction.ts
+++ packages/isaacscript-common/src/types/HasFunction.ts
@@ -1,6 +1,7 @@
 /** Helper type to check if an object or class has one or more functions/methods. */
 export type HasFunction<T> =
   Record<string, unknown> extends {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
     [K in keyof T as T[K] extends Function ? K : never]-?: 1;
   }
     ? never
