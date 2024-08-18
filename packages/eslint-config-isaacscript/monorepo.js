939a7edfdd3163e5463f263829b0ef72588b3bb9
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-08-18 08:04:30 -0400

    chore: move to TSESLint projectService

--- packages/eslint-config-isaacscript/monorepo.js
+++ packages/eslint-config-isaacscript/monorepo.js
@@ -1,24 +1,7 @@
-import path from "node:path";
 import tseslint from "typescript-eslint";
-
-const REPO_ROOT = path.join(import.meta.dirname, "..", "..");
 
 /** This config is meant to be used in the IsaacScript monorepo. */
 export const monorepo = tseslint.config(
-  {
-    languageOptions: {
-      // We need to add the `tsconfigRootDir` property, but we must also repeat the options from
-      // "base-typescript-eslint.js" or they will be deleted.
-      parserOptions: {
-        sourceType: "module",
-        project: true,
-        ecmaVersion: "latest",
-
-        tsconfigRootDir: REPO_ROOT,
-      },
-    },
-  },
-
   // The "isaacscript-common-node" dependency is used in scripts and should never appear in a
   // "package.json" file (if it is only used in script files). This has to be a monorepo disable
   // because in a normal project, "isaacscript-common-node" should be required in "devDependencies".
@@ -37,7 +20,7 @@ export const monorepo = tseslint.config(
       "import-x/no-relative-packages": "off",
 
       // ESLint configs import from "typescript-eslint", but this is installed at the monorepo root
-      // instead of in the packages' "package.json" file.
+      // instead of in the individual package "package.json" file.
       "import-x/no-extraneous-dependencies": "off",
     },
   },
