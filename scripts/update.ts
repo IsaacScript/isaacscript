0c93447038ad106e69a2aeb4354ed74eb4399781
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-27 14:49:33 -0400

    chore: update deps

--- scripts/update.ts
+++ scripts/update.ts
@@ -1,8 +1,7 @@
 // Packages held back:
-// - "typescript" - Stuck on "5.5.2" until TSTL upgrades:
-// https://github.com/TypeScriptToLua/TypeScriptToLua/issues/1580
 // - "typescript" - Stuck on "5.5.4" until TSESLint upgrades:
 // https://github.com/typescript-eslint/typescript-eslint/issues/9653
+// - "typescript-to-lua" - Stuck on "1.26.2" until TSESLint upgrades (same as previous).
 
 import { updatePackageJSONDependenciesMonorepo } from "complete-node";
 
