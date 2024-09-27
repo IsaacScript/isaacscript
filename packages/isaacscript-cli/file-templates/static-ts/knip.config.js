ce6664adbd0ab09d81293ec1c4936fcc7afef1a2
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-09-27 14:46:37 -0400

    fix(cli): knip config template

--- packages/isaacscript-cli/file-templates/static-ts/knip.config.js
+++ packages/isaacscript-cli/file-templates/static-ts/knip.config.js
@@ -6,13 +6,12 @@
 /** @type {import("knip").KnipConfig} */
 const config = {
   ignore: ["eslint.config.mjs", "prettier.config.mjs"],
-  ignoreDependencies: [
-    "complete-lint", // This is a linting meta-package.
-    "complete-node", // This is provided by "complete-lint".
-    "complete-tsconfig", // This is provided by "complete-lint".
-  ],
   ignoreBinaries: [
     "tsx", // This is provided by "complete-lint".
+  ],
+  ignoreDependencies: [
+    "complete-lint", // This is a linting meta-package.
+    "complete-tsconfig", // This is provided by "complete-lint".
   ],
 };
 
