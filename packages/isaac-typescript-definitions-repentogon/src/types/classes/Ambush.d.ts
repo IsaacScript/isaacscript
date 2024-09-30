01e394ac29ca7fc89adfd7cf1592c649a724a339
syntax-sculptor <syntaxsculptor@proton.me>
2024-09-29 20:55:17 -0800

    docs: ambush

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/Ambush.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/Ambush.d.ts
@@ -1,4 +1,6 @@
 /**
+ * The `Ambush` class is used to manage Challenge Room and Boss Rush functionality.
+ *
  * This class is for REPENTOGON, an exe-hack which expands the modding API.
  *
  * @see https://repentogon.com/
@@ -9,9 +11,17 @@ declare namespace Ambush {
   function GetCurrentWave(): int;
 
   /**
+   * Returns the maximum amount of Boss Challenge Room waves.
+   *
+   * The maximum amount of Boss Challenge Room waves can be modified through the
+   * `Ambush.SetMaxBossChallengeWaves` method.
+   */
+  function GetMaxBossChallengeWaves(): int;
+
+  /**
    * Returns the amount of waves that the boss rush has.
    *
-   * The maximum amount of boss rush waves can be modified through the `Ambush.SetMaxBossrushWaves`
+   * The maximum amount of Boss Rush waves can be modified through the `Ambush.SetMaxBossrushWaves`
    * method.
    */
   function GetMaxBossrushWaves(): int;
@@ -19,7 +29,7 @@ declare namespace Ambush {
   /**
    * Returns the maximum amount of challenge room waves.
    *
-   * The maximum amount of challenge room waves can be modified through the
+   * The maximum amount of Challenge Room waves can be modified through the
    * `Ambush.SetMaxChallengeWaves` method.
    */
   function GetMaxChallengeWaves(): int;
@@ -27,19 +37,41 @@ declare namespace Ambush {
   /**
    * Returns the `RoomConfig` of the next Challenge Room wave to spawn.
    *
-   * Calling this method outside of a Challenge Room will throw an error.
+   * Calling this method will error if the current room's type is not`RoomType.CHALLENGE`.
    */
   function GetNextWave(): RoomConfig;
 
-  /** Returns an array containing the `RoomConfig` of the next Challenge Room waves. */
+  /**
+   * Returns an array containing the `RoomConfig` of the next Challenge Room waves.
+   *
+   * Calling this method will error if the current room's type is not`RoomType.CHALLENGE`.
+   */
   function GetNextWaves(): RoomConfig[];
 
-  /** Sets the maximum amount of Boss Rush waves. This is currently capped at 25. */
+  /**
+   * Sets the maximum amount of Boss Challenge Room waves.
+   *
+   * The maximum amount of Boss Challenge Room waves will persist until the game restarts. If this
+   * behavior is not desired, use Isaacscript Common's save manager to keep track of the original
+   * number of waves and reset it when appropriate.
+   */
+  function SetMaxBossChallengeWaves(waves: int): void;
+
+  /**
+   * Sets the maximum amount of Boss Rush waves. This is currently capped at 25 waves.
+   *
+   * The maximum amount of Boss Rush waves will persist until the game restarts. If this behavior is
+   * is not desired, use Isaacscript Common's save manager to keep track of the original number of
+   * waves and reset it when appropriate.
+   */
   function SetMaxBossrushWaves(waves: int): void;
 
   /**
-   * Sets the maximum amount of Challenge Room waves. The new amount of waves will persist across
-   * runs until the game restarts.
+   * Sets the maximum amount of Challenge Room waves.
+   *
+   * The maximum amount of Challenge Room waves will persist until the game restarts. If this
+   * behavior is not desired, use Isaacscript Common's save manager to keep track of the original
+   * number of waves and reset it when appropriate.
    */
   function SetMaxChallengeWaves(waves: int): void;
 
@@ -48,9 +80,9 @@ declare namespace Ambush {
   /**
    * Spawns a Challenge Room wave associated with the current floor.
    *
-   * Calling this method will result in a crash if:
-   *  - The current floor is Blue Womb.
-   *  - The current difficulty is either Greed or Greedier.
+   * Calling this method crashes if there is no Challenge Rooms in the current floor's STB file.
+   * Under normal circumstances, the only floors to not have any Challenge rooms in their STB file
+   * is every Greed/Greedier Mode floor and Blue Womb.
    */
   function SpawnWave(): void;
 
