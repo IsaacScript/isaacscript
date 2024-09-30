5e2d22b32d41ca803aa66df92b86c99df3cc7e67
syntax-sculptor <syntaxsculptor@proton.me>
2024-09-30 11:44:04 -0800

    docs: GenericPrompt

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/GenericPrompt.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/GenericPrompt.d.ts
@@ -4,42 +4,6 @@ import type { GenericPromptSubmittedSelection } from "../../enums/GenericPromptS
 /**
  * Constructs a new `GenericPrompt` class.
  *
- * **Example**
- *
- * ```ts
- * const mod = RegisterMod("Foo", 1);
- *
- * const prompt = GenericPrompt();
- * prompt.Initialize();
- * prompt.SetText("Some test text");
- *
- * let wasPromptDisplayed = false;
- *
- * function postRender() {
- *   prompt.Render();
- * }
- *
- * mod.AddCallback(ModCallback.POST_RENDER, postRender);
- *
- * function postUpdate() {
- *   prompt.Update(true); // Process user inputs
- *
- *   if (wasPromptDisplayed && !prompt.IsActive()) {
- *     // Prompt was closed by user.
- *     const submittedSelection = prompt.GetSubmittedSelection();
- *     print(`User selected option: ${submittedSelection}`);
- *     wasPromptDisplayed = false;
- *   }
- *
- *   if (Input.IsButtonTriggered(Keyboard.MINUS, 0)) {
- *     prompt.Show();
- *     wasPromptDisplayed = true;
- *   }
- * }
- *
- * mod.AddCallback(ModCallback.POST_UPDATE, postUpdate);
- * ```
- *
  * This class is for REPENTOGON, an exe-hack which expands the modding API.
  *
  * @see https://repentogon.com/
@@ -53,33 +17,60 @@ declare global {
    * @see https://repentogon.com/
    */
   interface GenericPrompt {
+    /** Returns the selection the player is hovering over. */
     GetCurrentSelection: () => GenericPromptSelection;
+
+    /** Returns the sprite used by the prompt. */
     GetSprite: () => Sprite;
+
+    /** Returns the selection the player has submitted. */
     GetSubmittedSelection: () => GenericPromptSubmittedSelection;
 
-    /** @param smallPrompt Optional. Default is false. */
+    /**
+     * Initializes the generic prompt. This method should be called immediately after constructing
+     * the object.
+     *
+     * @param smallPrompt Optional. Default is false.
+     */
     Initialize: (smallPrompt?: boolean) => void;
 
+    /** Returns whether the prompt is currently active. */
     IsActive: () => boolean;
+
+    /**
+     * Renders the prompt on the screen. This method should be called in `ModCallback.POST_RENDER`.
+     */
     Render: () => void;
+
+    /** Sets the prompt's image to the victory run image. */
     SetImageToVictoryRun: () => void;
 
     /**
+     * Sets the paper's text.
+     *
+     * @param header1 Optional. Default is an empty string.
+     * @param header2 Optional. Default is an empty string.
      * @param line1 Optional. Default is an empty string.
      * @param line2 Optional. Default is an empty string.
      * @param line3 Optional. Default is an empty string.
-     * @param line4 Optional. Default is an empty string.
-     * @param line5 Optional. Default is an empty string.
      */
     SetText: (
+      header1?: string,
+      header?: string,
       line1?: string,
       line2?: string,
       line3?: string,
-      line4?: string,
-      line5?: string,
     ) => void;
 
+    /** Displays the prompt on-screen. */
     Show: () => void;
+
+    /**
+     * Updates the animation of the prompt paper and its functionality. This method should be called
+     * in `ModCallback.POST_UPDATE`.
+     *
+     * @param processInput If true, the game will process the player's input for selecting yes/no.
+     */
     Update: (processInput: boolean) => void;
   }
 }
