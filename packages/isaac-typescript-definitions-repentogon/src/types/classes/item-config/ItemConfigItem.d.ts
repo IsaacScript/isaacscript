2ea75f00c2ddbae2e63a80f4f701b90d7c0f9020
syntax-sculptor <syntaxsculptor@proton.me>
2024-09-30 14:17:49 -0800

    docs: ItemConfigItem

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/item-config/ItemConfigItem.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/item-config/ItemConfigItem.d.ts
@@ -1,6 +1,27 @@
 declare interface ItemConfigItem extends IsaacAPIClass {
+  /**
+   * Returns an array of the strings defined in the item's `customcache` attribute in `items.xml`.
+   *
+   * The tags in the array are always in lowercase regardless of its casing in `items.xml`.
+   */
   GetCustomCacheTags: () => string[];
+
+  /**
+   * Returns an array of the strings defined in the item's `customtags` attribute in `items.xml`.
+   *
+   * The tags in the array are always in lowercase regardless of its casing in `items.xml`.
+   */
   GetCustomTags: () => string[];
+
+  /**
+   * Returns whether the provided cache tag was specified in the item's `customcache` attribute in
+   * `items.xml`. The tag name is case insensitive.
+   */
   HasCustomCacheTag: (tagName: string) => boolean;
+
+  /**
+   * Returns whether the provided tag was specified in the item's `customtags` attribute in
+   * `items.xml`. The tag name is case insensitive.
+   */
   HasCustomTag: (tag: string) => boolean;
 }
