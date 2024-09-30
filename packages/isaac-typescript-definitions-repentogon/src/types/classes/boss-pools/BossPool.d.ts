3709e74d66f4b21214aef7b352e58ce321e0195b
syntax-sculptor <syntaxsculptor@proton.me>
2024-09-29 21:00:33 -0800

    docs: BossPool

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/boss-pools/BossPool.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/boss-pools/BossPool.d.ts
@@ -4,8 +4,18 @@
  * @see https://repentogon.com/
  */
 declare interface BossPool extends IsaacAPIClass {
+  /** Returns the ID of the Boss Pool's Double Trouble room. */
   GetDoubleTroubleRoomID: () => int;
+
+  /** Returns an array of the Boss Pool's entries. */
   GetEntries: () => BossPoolEntry[];
+
+  /** Returns the name of the Boss Pool. */
   GetName: () => string;
+
+  /** Returns the RNG object of the Boss Pool. The RNG object is used to select the floor's boss. */
   GetRNG: () => RNG;
+
+  /** Returns the Boss Pool's weight. */
+  GetWeight: () => float;
 }
