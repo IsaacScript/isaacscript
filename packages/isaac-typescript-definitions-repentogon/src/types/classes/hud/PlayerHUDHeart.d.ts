9c9fd0b1b989bc03a955ebfedf8ec3168ab9e1a1
syntax-sculptor <syntaxsculptor@proton.me>
2024-09-30 12:05:55 -0800

    docs: PlayerHUDHeart

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/hud/PlayerHUDHeart.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/hud/PlayerHUDHeart.d.ts
@@ -7,6 +7,7 @@ declare global {
    * @see https://repentogon.com/
    */
   interface PlayerHUDHeart extends IsaacAPIClass {
+    /** Returns the heart's flash type. */
     GetFlashType: () => HeartFlashType;
 
     /** Returns the name of the animation currently playing on the heart. */
@@ -15,7 +16,10 @@ declare global {
     /** Returns the name of the overlay animation currently playing on the heart. */
     GetHeartOverlayAnim: () => string;
 
+    /** Returns whether the Eternal Heart overlay is visible. */
     IsEternalHeartOverlayVisible: () => boolean;
+
+    /** Returns whether the heart is fading. */
     IsFadingHeart: () => boolean;
 
     /** Returns whether the golden heart overlay is visible on the heart. */
