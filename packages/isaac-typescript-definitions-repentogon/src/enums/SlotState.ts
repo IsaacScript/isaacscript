52aa52e64e3080ec526f0f20bfbb15377e229962
James <5511220+Zamiell@users.noreply.github.com>
2024-10-18 18:08:21 -0400

    Update SlotState.ts

--- packages/isaac-typescript-definitions-repentogon/src/enums/SlotState.ts
+++ packages/isaac-typescript-definitions-repentogon/src/enums/SlotState.ts
@@ -5,10 +5,13 @@
  */
 export enum SlotState {
   IDLE = 1,
+
   /** Only used by Shell Game and Hell Game. */
   IDLE_REWARD = 2,
+
   BOMBED = 3,
   PAYOUT = 4,
+
   /** Only used by Shell Game and Hell Game. */
   REWARD = 5,
 }
