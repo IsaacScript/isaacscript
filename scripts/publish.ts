863899eab4e11870678298accdfe5c83f38f99b1
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-10-25 06:13:57 -0400

    chore: update publish script

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,3 +1,4 @@
 import { monorepoPublish } from "complete-node";
 
-await monorepoPublish();
+// TODO: https://github.com/typescript-eslint/typescript-eslint/issues/10191
+await monorepoPublish(false);
