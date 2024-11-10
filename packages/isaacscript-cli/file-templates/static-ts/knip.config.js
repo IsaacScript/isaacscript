f4fb3dc579b5f857457802fd7cd4aae6174a2d34
Zamiell <5511220+Zamiell@users.noreply.github.com>
2024-11-10 13:11:56 -0500

    chore(cli): update knip config template

--- packages/isaacscript-cli/file-templates/static-ts/knip.config.js
+++ packages/isaacscript-cli/file-templates/static-ts/knip.config.js
@@ -14,7 +14,6 @@ const config = {
   ],
   ignoreDependencies: [
     "complete-lint", // This is a linting meta-package.
-    "complete-tsconfig", // This is provided by "complete-lint".
   ],
 };
 
