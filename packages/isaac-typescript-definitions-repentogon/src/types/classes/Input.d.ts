27fbeb42640a5084d60b249fda32e88925401119
Syntax-Sculptor <143585501+Syntax-Sculptor@users.noreply.github.com>
2025-11-11 11:45:15 -0800

    feat: new Input definitions

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/Input.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/Input.d.ts
@@ -3,5 +3,6 @@ import type { ControllerIndex } from "isaac-typescript-definitions";
 declare global {
   namespace Input {
     function GetDeviceNameByIdx(index: ControllerIndex): string | undefined;
+    function GetMouseWheel(): Vector;
   }
 }
