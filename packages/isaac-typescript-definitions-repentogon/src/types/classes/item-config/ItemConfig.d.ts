b93775b7b1d083023b7a9b764f5c5616db6a505d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2025-12-23 14:38:18 -0500

    fix

--- packages/isaac-typescript-definitions-repentogon/src/types/classes/item-config/ItemConfig.d.ts
+++ packages/isaac-typescript-definitions-repentogon/src/types/classes/item-config/ItemConfig.d.ts
@@ -17,7 +17,8 @@ declare global {
     /**
      * Returns whether the trinket is valid.
      *
-     * @see https://github.com/TeamREPENTOGON/REPENTOGON/blob/db50daa92ff366565c699bf09641e8c5b9b2449c/repentogon/LuaInterfaces/LuaItemConfig.cpp#L241-L246
+     * @see
+     * https://github.com/TeamREPENTOGON/REPENTOGON/blob/db50daa92ff366565c699bf09641e8c5b9b2449c/repentogon/LuaInterfaces/LuaItemConfig.cpp#L241-L246
      * @see https://repentogon.com/ItemConfig.html?h=ItemConfig#isvalidtrinket
      * @deprecated Not currently exposed to Lua (hook commented out upstream), despite being
      *             documented.
