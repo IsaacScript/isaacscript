6c5cb9941d89f71ad43ad7b73d70c6ccd7673f66
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-09-02 11:11:58 -0400

    feat(cli): internal packages exempt from cooldown

--- packages/isaacscript-cli/src/commands/update/update.ts
+++ packages/isaacscript-cli/src/commands/update/update.ts
@@ -2,6 +2,19 @@ import { Command } from "@commander-js/extra-typings";
 import { updatePackageJSONDependencies } from "complete-node";
 import { CWD } from "../../constants.js";
 
+const INTERNAL_PACKAGES = [
+  "eslint-config-isaacscript",
+  "eslint-plugin-isaacscript",
+  "isaac-lua-polyfill",
+  "isaac-typescript-definitions",
+  "isaac-typescript-definitions-repentogon",
+  "isaacscript-cli",
+  "isaacscript-common",
+  "isaacscript-lint",
+  "isaacscript-spell",
+  "isaacscript-tsconfig",
+] as const;
+
 export const updateCommand = new Command()
   // eslint-disable-next-line unicorn/no-unreadable-new-expression
   .command("update")
@@ -9,7 +22,12 @@ export const updateCommand = new Command()
   .allowExcessArguments(false) // By default, Commander.js will allow extra positional arguments.
   .helpOption("-h, --help", "Display the list of options for this command.")
   .action(async () => {
-    const hasNewDependencies = await updatePackageJSONDependencies(CWD);
+    const hasNewDependencies = await updatePackageJSONDependencies(
+      CWD,
+      undefined,
+      undefined,
+      INTERNAL_PACKAGES,
+    );
     const msg = hasNewDependencies
       ? "Successfully installed new Node.js dependencies."
       : "There were no new dependency updates from npm.";
