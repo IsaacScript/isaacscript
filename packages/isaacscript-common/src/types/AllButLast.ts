4a0d7e94e591e7426cd17b14a617b202aff37e2f
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-09-02 09:11:14 -0400

    fix(common): AllButLast symmetry

--- packages/isaacscript-common/src/types/AllButLast.ts
+++ packages/isaacscript-common/src/types/AllButLast.ts
@@ -1,4 +1,7 @@
 /** Helper type to create a new tuple containing all but the last element of another tuple. */
-export type AllButLast<T extends unknown[]> = T extends [...infer Head, unknown]
+export type AllButLast<T extends readonly unknown[]> = T extends readonly [
+  ...infer Head,
+  unknown,
+]
   ? Head
   : unknown[];
