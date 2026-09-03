54f7442e61d765f0f1f0c3761f4777c3d5e98c9e
Zamiell <5511220+Zamiell@users.noreply.github.com>
2026-09-02 23:48:33 -0400

    chore: update deps

--- scripts/lint-all.ts
+++ scripts/lint-all.ts
@@ -1,3 +1,4 @@
+import type { LintCommand } from "complete-node";
 import { $q, getMonorepoPackageNames, lintCommands } from "complete-node";
 import path from "node:path";
 
@@ -7,14 +8,16 @@ const REPO_ROOT = path.resolve(import.meta.dirname, "..");
 // for the monorepo itself. For that, use the "lint.ts" script.
 const lintPackages = await getMonorepoPackageNames(REPO_ROOT, "lint");
 
-const lintPackageCommands = lintPackages.map((packageName) => {
-  const packagePath = path.join(REPO_ROOT, "packages", packageName);
-  const $$ = $q({
-    cwd: packagePath,
-    all: true,
-  });
-  return [packageName, $$`bun run lint`] as const;
-});
+const lintPackageCommands: readonly LintCommand[] = lintPackages.map(
+  (packageName) => {
+    const packagePath = path.join(REPO_ROOT, "packages", packageName);
+    const $$ = $q({
+      cwd: packagePath,
+      all: true,
+    });
+    return [packageName, $$`bun run lint`];
+  },
+);
 
 await lintCommands(import.meta.dirname, lintPackageCommands, true);
 
